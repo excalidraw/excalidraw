af1b1147df1904bbb1ca8c99fe5a34b064f160c3
Christopher Chedeau <vjeuxx@gmail.com>
2020-01-03 21:37:28 -0800

    Add support for forward

--- src/zindex.test.ts
+++ src/zindex.test.ts
@@ -1,6 +1,11 @@
-import { moveOneLeft, moveAllLeft } from "./zindex";
+import { moveOneLeft, moveOneRight, moveAllLeft, moveAllRight } from "./zindex";
 
-function expectMove(fn, elems, indices, equal) {
+function expectMove<T>(
+  fn: (elements: T[], indicesToMove: number[]) => void,
+  elems: T[],
+  indices: number[],
+  equal: T[]
+) {
   fn(elems, indices);
   expect(elems).toEqual(equal);
 }
@@ -15,6 +20,18 @@ it("should moveOneLeft", () => {
     ["a", "b", "c", "d"]
   );
   expectMove(moveOneLeft, ["a", "b", "c", "d"], [1, 3], ["b", "a", "d", "c"]);
+});
+
+it("should moveOneRight", () => {
+  expectMove(moveOneRight, ["a", "b", "c", "d"], [1, 2], ["a", "d", "b", "c"]);
+  expectMove(moveOneRight, ["a", "b", "c", "d"], [3], ["a", "b", "c", "d"]);
+  expectMove(
+    moveOneRight,
+    ["a", "b", "c", "d"],
+    [0, 1, 2, 3],
+    ["a", "b", "c", "d"]
+  );
+  expectMove(moveOneRight, ["a", "b", "c", "d"], [0, 2], ["b", "a", "d", "c"]);
 });
 
 it("should moveAllLeft", () => {
@@ -47,5 +64,38 @@ it("should moveAllLeft", () => {
     ["a", "b", "c", "d", "e", "f", "g"],
     [4, 5, 6],
     ["e", "f", "g", "a", "b", "c", "d"]
+  );
+});
+
+it("should moveAllRight", () => {
+  expectMove(
+    moveAllRight,
+    ["a", "b", "c", "d", "e", "f", "g"],
+    [2, 5],
+    ["a", "b", "d", "e", "g", "c", "f"]
+  );
+  expectMove(
+    moveAllRight,
+    ["a", "b", "c", "d", "e", "f", "g"],
+    [5],
+    ["a", "b", "c", "d", "e", "g", "f"]
+  );
+  expectMove(
+    moveAllRight,
+    ["a", "b", "c", "d", "e", "f", "g"],
+    [0, 1, 2, 3, 4, 5, 6],
+    ["a", "b", "c", "d", "e", "f", "g"]
+  );
+  expectMove(
+    moveAllRight,
+    ["a", "b", "c", "d", "e", "f", "g"],
+    [0, 1, 2],
+    ["d", "e", "f", "g", "a", "b", "c"]
+  );
+  expectMove(
+    moveAllRight,
+    ["a", "b", "c", "d", "e", "f", "g"],
+    [4, 5, 6],
+    ["a", "b", "c", "d", "e", "f", "g"]
   );
 });
