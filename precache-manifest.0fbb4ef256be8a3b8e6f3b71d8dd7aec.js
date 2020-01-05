f99761af494b8786a8d07db244fa6be18d46d523
giovannigiordano <giordano.giovanni1993@gmail.com>
2020-01-05 13:10:25 +0000

    Deploying to master from  ee851a0c89436ceb3e5440d06e9b306d4ff5b7ac

---
+++ precache-manifest.0fbb4ef256be8a3b8e6f3b71d8dd7aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d184ca60da16d138f26273d1a723e84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44655b91a9acb4dab431",
+    "url": "/static/css/main.4b4142f8.chunk.css"
+  },
+  {
+    "revision": "873896e28eb81c54171d",
+    "url": "/static/js/2.3fb278bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3fb278bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44655b91a9acb4dab431",
+    "url": "/static/js/main.3def8d06.chunk.js"
+  },
+  {
+    "revision": "b44dee203db29ecae5db",
+    "url": "/static/js/runtime-main.b019aae8.js"
+  }
+]);
