1739540f003e719e4f37c0ef299aabd806eecd25
Timur Khazamov <bboytx@gmail.com>
2020-01-09 01:09:09 +0500

    Creating a text near the center of a shape should put it in the center (#270)
    * Snap to element center
    
    * Fixed typo
    
    * Added comment
    
    * Reduced threshold to 30
    
    * Skip snapping if alt key is pressed
    
    * Fixed creating text with shape tool

--- src/scene/comparisons.ts
+++ src/scene/comparisons.ts
@@ -1,5 +1,6 @@
 import { ExcalidrawElement } from "../element/types";
 import { hitTest } from "../element/collision";
+import { getElementAbsoluteCoords } from "../element";
 
 export const hasBackground = (elements: ExcalidrawElement[]) =>
   elements.some(
@@ -34,5 +35,22 @@ export function getElementAtPosition(
     }
   }
 
+  return hitElement;
+}
+
+export function getElementContainingPosition(
+  elements: ExcalidrawElement[],
+  x: number,
+  y: number
+) {
+  let hitElement = null;
+  // We need to to hit testing from front (end of the array) to back (beginning of the array)
+  for (let i = elements.length - 1; i >= 0; --i) {
+    const [x1, y1, x2, y2] = getElementAbsoluteCoords(elements[i]);
+    if (x1 < x && x < x2 && y1 < y && y < y2) {
+      hitElement = elements[i];
+      break;
+    }
+  }
   return hitElement;
 }
