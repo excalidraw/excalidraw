62970016b6fe640ce794972e0c309a3c5a5a9733
Christopher Chedeau <vjeux@fb.com>
2020-01-11 15:20:56 -0800

    Remove console.log

--- src/actions/manager.tsx
+++ src/actions/manager.tsx
@@ -42,11 +42,6 @@ export class ActionManager implements ActionsManagerInterface {
     appState: AppState,
     updater: UpdaterFn
   ) {
-    console.log(
-      Object.values(this.actions)
-        .filter(action => "contextItemLabel" in action)
-        .map(a => ({ name: a.name, label: a.contextItemLabel }))
-    );
     return Object.values(this.actions)
       .filter(action => "contextItemLabel" in action)
       .sort(
