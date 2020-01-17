690ae53ef33bdbd5a85607d933bb76b16503cbf7
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2020-01-17 23:53:41 +0530

    Bug 389 (#428)
    * paste inside the viewport
    
    * Buttons in top left panel aren't horizontally centered

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -6,24 +6,37 @@ type StackProps = {
   children: React.ReactNode;
   gap?: number;
   align?: "start" | "center" | "end" | "baseline";
+  justifyContent?: "center" | "space-around" | "space-between";
 };
 
-function RowStack({ children, gap, align }: StackProps) {
+function RowStack({ children, gap, align, justifyContent }: StackProps) {
   return (
     <div
       className="Stack Stack_horizontal"
-      style={{ "--gap": gap, alignItems: align } as React.CSSProperties}
+      style={
+        {
+          "--gap": gap,
+          alignItems: align,
+          justifyContent
+        } as React.CSSProperties
+      }
     >
       {children}
     </div>
   );
 }
 
-function ColStack({ children, gap, align }: StackProps) {
+function ColStack({ children, gap, align, justifyContent }: StackProps) {
   return (
     <div
       className="Stack Stack_vertical"
-      style={{ "--gap": gap, justifyItems: align } as React.CSSProperties}
+      style={
+        {
+          "--gap": gap,
+          justifyItems: align,
+          justifyContent
+        } as React.CSSProperties
+      }
     >
       {children}
     </div>
