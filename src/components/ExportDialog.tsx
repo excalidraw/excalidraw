a118e59ac4d146d2b0d1d7cadb545d94c3d75631
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-01-28 23:50:25 +0200

    Wider export dialog

--- src/components/ExportDialog.tsx
+++ src/components/ExportDialog.tsx
@@ -257,7 +257,7 @@ export function ExportDialog({
       />
       {modalIsShown && (
         <Modal
-          maxWidth={640}
+          maxWidth={800}
           onCloseRequest={handleClose}
           labelledBy="export-title"
         >
