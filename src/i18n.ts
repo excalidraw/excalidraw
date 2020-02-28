5e7bae01d19830573c17018e0433c97e49e70095
Lipis <lipiridis@gmail.com>
2020-02-29 00:57:33 +0100

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,6 +10,7 @@ export const languages = [
   { lng: "pt", label: "Português", data: require("./locales/pt.json") },
   { lng: "ru", label: "Русский", data: require("./locales/ru.json") },
   { lng: "tr", label: "Türkçe", data: require("./locales/tr.json") },
+  { lng: "zh", label: "中文", data: require("./locales/zh.json") },
 ];
 
 let currentLanguage = languages[0];
