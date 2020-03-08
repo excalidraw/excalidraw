97b045e9032476dfe453c9ac89c35c3795fb9fe7
idlewinn <edwinlin1987@gmail.com>
2020-03-07 21:30:53 -0800

    enable collaboration, rooms, and mouse tracking

--- src/scene/export.ts
+++ src/scene/export.ts
@@ -49,6 +49,7 @@ export function exportToCanvas(
       scrollX: normalizeScroll(-minX + exportPadding),
       scrollY: normalizeScroll(-minY + exportPadding),
       zoom: 1,
+      remotePointerViewportCoords: {},
     },
     {
       renderScrollbars: false,
