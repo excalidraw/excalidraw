excalidraw/excalidraw

commit 67cfe22c69b76d0449bb019f002fff173576fabb

Fix perf regression (#880)

src/renderer/renderElement.ts
@@ -244,7 +244,9 @@ function generateElement(
   const zoom = sceneState ? sceneState.zoom : 1;
   const prevElementWithCanvas = elementWithCanvasCache.get(element);
   if (!prevElementWithCanvas || prevElementWithCanvas.canvasZoom !== zoom) {
-    return generateElementCanvas(element, zoom);
+    const elementWithCanvas = generateElementCanvas(element, zoom);
+    elementWithCanvasCache.set(element, elementWithCanvas);
+    return elementWithCanvas;
   }
   return prevElementWithCanvas;
 }

commit 788901a98497d4241edd516e9478a76ee63939a1

Fix a bug introduced by #880

src/renderer/renderElement.ts
@@ -136,6 +136,7 @@ function generateElement(
 ) {
   let shape = shapeCache.get(element) || null;
   if (!shape) {
+    elementWithCanvasCache.delete(element);
     switch (element.type) {
       case "rectangle":
         shape = generator.rectangle(0, 0, element.width, element.height, {