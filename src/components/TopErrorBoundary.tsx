e4c7efd48580e1e1044b16d147e31b88f273f787
dwelle <luzar.david@gmail.com>
2020-03-09 15:00:01 +0100

    include spaces in i18n

--- src/components/TopErrorBoundary.tsx
+++ src/components/TopErrorBoundary.tsx
@@ -94,13 +94,13 @@ export class TopErrorBoundary extends React.Component<
         <div className="ErrorSplash">
           <div className="ErrorSplash-messageContainer">
             <div className="ErrorSplash-paragraph bigger align-center">
-              {t("errorSplash.headingMain_pre")}{" "}
+              {t("errorSplash.headingMain_pre")}
               <button onClick={() => window.location.reload()}>
                 {t("errorSplash.headingMain_button")}
               </button>
             </div>
             <div className="ErrorSplash-paragraph align-center">
-              {t("errorSplash.clearCanvasMessage")}{" "}
+              {t("errorSplash.clearCanvasMessage")}
               <button
                 onClick={() => {
                   localStorage.clear();
@@ -113,8 +113,8 @@ export class TopErrorBoundary extends React.Component<
               <div className="smaller">
                 <span role="img" aria-label="warning">
                   ⚠️
-                </span>{" "}
-                {t("errorSplash.clearCanvasCaveat")}{" "}
+                </span>
+                {t("errorSplash.clearCanvasCaveat")}
                 <span role="img" aria-hidden="true">
                   ⚠️
                 </span>
@@ -122,10 +122,10 @@ export class TopErrorBoundary extends React.Component<
             </div>
             <div>
               <div className="ErrorSplash-paragraph">
-                {t("errorSplash.openIssueMessage_pre")}{" "}
+                {t("errorSplash.openIssueMessage_pre")}
                 <button onClick={this.createGithubIssue}>
                   {t("errorSplash.openIssueMessage_button")}
-                </button>{" "}
+                </button>
                 {t("errorSplash.openIssueMessage_post")}
               </div>
               <div className="ErrorSplash-paragraph">
