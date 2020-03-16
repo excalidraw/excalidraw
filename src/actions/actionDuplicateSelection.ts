f07726cd55dd2d3fa6d10540bfeda67df3183164
Faustino Kialungila <Faustino.kialungila@gmail.com>
2020-03-16 22:53:02 +0100

    CMD + D to Duplicate Selection (#982)
    * cmd+d to duplicate selection
    
    * use duplicateElement instead
    
    * use duplicateElement instead
    
    * Update actionDuplicateSelection.ts
    
    * select the new duplicated element
    
    * add locale
    
    * use event.key instead of event.code
    
    Co-authored-by: David Luzar <luzar.david@gmail.com>

---
+++ src/actions/actionDuplicateSelection.ts
@@ -0,0 +1,29 @@
+import { KEYS } from "../keys";
+import { register } from "./register";
+import { ExcalidrawElement } from "../element/types";
+import { duplicateElement } from "../element";
+
+export const actionDuplicateSelection = register({
+  name: "duplicateSelection",
+  perform: (elements, appState) => {
+    return {
+      appState,
+      elements: elements.reduce(
+        (acc: Array<ExcalidrawElement>, element: ExcalidrawElement) => {
+          if (appState.selectedElementIds[element.id]) {
+            const newElement = duplicateElement(element);
+            newElement.x = newElement.x + 10;
+            newElement.y = newElement.y + 10;
+            appState.selectedElementIds[newElement.id] = true;
+            delete appState.selectedElementIds[element.id];
+            return acc.concat([element, newElement]);
+          }
+          return acc.concat(element);
+        },
+        [],
+      ),
+    };
+  },
+  contextItemLabel: "labels.duplicateSelection",
+  keyTest: event => event[KEYS.CTRL_OR_CMD] && event.key === "d",
+});
