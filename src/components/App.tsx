94054473e4cf3d066f3942a788c6e17a8488df5d
idlewinn <edwinlin1987@gmail.com>
2020-03-21 14:35:44 -0700

    load scene from localStorage in collaboration prior to syncing with server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -519,13 +519,13 @@ export class App extends React.Component<any, AppState> {
       return;
     }
 
+    const scene = await loadScene(null);
+    this.syncActionResult(scene);
     const roomMatch = getCollaborationLinkData(window.location.href);
     if (roomMatch) {
       this.initializeSocketClient();
       return;
     }
-    const scene = await loadScene(null);
-    this.syncActionResult(scene);
 
     window.addEventListener("beforeunload", this.beforeUnload);
   }
