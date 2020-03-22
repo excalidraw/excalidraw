d7874a69f0f3f818642c0e29b52d2187b2f615f7
Lipis <lipiridis@gmail.com>
2020-03-22 16:13:42 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,6 +10,7 @@ export const languages = [
     label: "Bahasa Indonesia",
     data: require("./locales/id-ID.json"),
   },
+  { lng: "hu-HU", label: "Magyar", data: require("./locales/hu-HU.json") },  
   { lng: "no-No", label: "Norsk", data: require("./locales/no-NO.json") },
   { lng: "pl-PL", label: "Polski", data: require("./locales/pl-PL.json") },
   { lng: "pt-PT", label: "Português", data: require("./locales/pt-PT.json") },
