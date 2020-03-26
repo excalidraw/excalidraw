e42b449bab15a7366c25f0c8c68a2208bc06904c
dwelle <luzar.david@gmail.com>
2020-03-26 17:54:25 +0100

    fix comment breaking build jsx

--- src/components/LoadingMessage.tsx
+++ src/components/LoadingMessage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 export const LoadingMessage = () => {
+  // !! KEEP THIS IN SYNC WITH index.html !!
   return (
-    // !! KEEP THIS IN SYNC WITH index.html !!
     <div className="LoadingMessage">
       <span>Loading scene...</span>
     </div>
