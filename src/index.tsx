60a1e5522d13714ff6506cf9c5d372343ea21c0f
Kostas Bariotis <konmpar@gmail.com>
2020-03-31 09:37:51 +0100

    Add sentry integration (#1141)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 import { TopErrorBoundary } from "./components/TopErrorBoundary";
 import { IsMobileProvider } from "./is-mobile";
 import { App } from "./components/App";
 import "./styles.scss";
+
+const SentyEnvHostnameMap: { [key: string]: string } = {
+  "excalidraw.com": "production",
+  "now.sh": "staging",
+};
+
+const onlineEnv = Object.keys(SentyEnvHostnameMap).find(
+  (item) => window.location.hostname.indexOf(item) >= 0,
+);
+
+Sentry.init({
+  // Disable Sentry locally to avoid noise
+  dsn: onlineEnv
+    ? "https://7bfc596a5bf945eda6b660d3015a5460@sentry.io/5179260"
+    : undefined,
+  environment: onlineEnv ? SentyEnvHostnameMap[onlineEnv] : undefined,
+  release: process.env.REACT_APP_GIT_SHA,
+});
 
 // Block pinch-zooming on iOS outside of the content area
 document.addEventListener(
