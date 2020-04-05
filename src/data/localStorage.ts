ba05722ffe5789d62933e4ddd9e9d9ce10fe8764
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-04-05 23:29:46 +0530

    Wrap localStorage API Access in try-catch

--- src/data/localStorage.ts
+++ src/data/localStorage.ts
@@ -10,40 +10,49 @@ export function saveToLocalStorage(
   elements: readonly ExcalidrawElement[],
   appState: AppState,
 ) {
-  localStorage.setItem(
-    LOCAL_STORAGE_KEY,
-    JSON.stringify(elements.filter((element) => !element.isDeleted)),
-  );
-  localStorage.setItem(
-    LOCAL_STORAGE_KEY_STATE,
-    JSON.stringify(clearAppStateForLocalStorage(appState)),
-  );
+  try {
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify(elements.filter((element) => !element.isDeleted)),
+    );
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY_STATE,
+      JSON.stringify(clearAppStateForLocalStorage(appState)),
+    );
+  } catch (err) {
+    // Unable to access window.localStorage
+  }
 }
 
 export function restoreFromLocalStorage() {
-  const savedElements = localStorage.getItem(LOCAL_STORAGE_KEY);
-  const savedState = localStorage.getItem(LOCAL_STORAGE_KEY_STATE);
+  try {
+    const savedElements = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const savedState = localStorage.getItem(LOCAL_STORAGE_KEY_STATE);
 
-  let elements = [];
-  if (savedElements) {
-    try {
-      elements = JSON.parse(savedElements);
-    } catch {
-      // Do nothing because elements array is already empty
+    let elements = [];
+    if (savedElements) {
+      try {
+        elements = JSON.parse(savedElements);
+      } catch {
+        // Do nothing because elements array is already empty
+      }
     }
-  }
 
-  let appState = null;
-  if (savedState) {
-    try {
-      appState = JSON.parse(savedState) as AppState;
-      // If we're retrieving from local storage, we should not be collaborating
-      appState.isCollaborating = false;
-      appState.collaborators = new Map();
-    } catch {
-      // Do nothing because appState is already null
+    let appState = null;
+    if (savedState) {
+      try {
+        appState = JSON.parse(savedState) as AppState;
+        // If we're retrieving from local storage, we should not be collaborating
+        appState.isCollaborating = false;
+        appState.collaborators = new Map();
+      } catch {
+        // Do nothing because appState is already null
+      }
     }
-  }
 
-  return restore(elements, appState);
+    return restore(elements, appState);
+  } catch (err) {
+    // Unable to access window.localStorage
+    return [];
+  }
 }
