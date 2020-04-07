67805bc7a7a4afb70f20c3b751a9e33893a54470
Kostas Bariotis <konmpar@gmail.com>
2020-04-07 14:02:42 +0100

    Add collaborators names (#1223)
    * add random usernames
    
    * add username state
    
    * add username input
    
    * ability to set names
    
    * fix tests
    
    * set username oon mobile
    
    * remove auto generated names
    
    * remove commented code
    
    * always string
    
    * updaate snapshots
    
    * maintain username when clearing canvas
    
    * Update src/renderer/renderScene.ts
    
    Co-Authored-By: Lipis <lipiridis@gmail.com>
    
    * add border
    
    * fix styles
    
    Co-authored-by: Pete Hunt <petehunt@users.noreply.github.com>
    Co-authored-by: Faustino Kialungila <faustino.kialungila@gmail.com>
    Co-authored-by: Lipis <lipiridis@gmail.com>

--- src/appState.ts
+++ src/appState.ts
@@ -29,6 +29,7 @@ export function getDefaultAppState(): AppState {
     cursorButton: "up",
     scrolledOutside: false,
     name: `excalidraw-${getDateTime()}`,
+    username: "",
     isCollaborating: false,
     isResizing: false,
     isRotating: false,
