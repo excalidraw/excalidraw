6ae441a3e96d76aa70bfb2ca9982e3d59cb3eadb
mbj36 <mohitbajo36@gmail.com>
2020-04-16 02:41:44 +0530

    Fix #1364

--- src/components/RoomDialog.tsx
+++ src/components/RoomDialog.tsx
@@ -15,12 +15,14 @@ function RoomModal({
   onUsernameChange,
   onRoomCreate,
   onRoomDestroy,
+  onPressingEnter,
 }: {
   activeRoomLink: string;
   username: string;
   onUsernameChange: (username: string) => void;
   onRoomCreate: () => void;
   onRoomDestroy: () => void;
+  onPressingEnter: () => void;
 }) {
   const roomLinkInput = useRef<HTMLInputElement>(null);
 
@@ -85,6 +87,7 @@ function RoomModal({
               value={username || ""}
               className="RoomDialog-username TextInput"
               onChange={(event) => onUsernameChange(event.target.value)}
+              onKeyPress={(event) => event.key === "Enter" && onPressingEnter()}
             />
           </div>
           <p>{`🔒 ${t("roomDialog.desc_privacy")}`}</p>
@@ -173,6 +176,7 @@ export function RoomDialog({
             onUsernameChange={onUsernameChange}
             onRoomCreate={onRoomCreate}
             onRoomDestroy={onRoomDestroy}
+            onPressingEnter={handleClose}
           />
         </Dialog>
       )}
