4cd351ccfef61e288699423ddb85bd99c3e36161
dwelle <luzar.david@gmail.com>
2020-04-16 21:16:29 +0200

    fix bg color picker rendered behind shape menu

--- src/components/LayerUI.tsx
+++ src/components/LayerUI.tsx
@@ -125,7 +125,7 @@ export const LayerUI = React.memo(
           <div className="App-menu App-menu_top">
             <Stack.Col gap={4}>
               <Section heading="canvasActions">
-                <Island padding={4}>
+                <Island padding={4} style={{ zIndex: 1 }}>
                   <Stack.Col gap={4}>
                     <Stack.Row gap={1} justifyContent={"space-between"}>
                       {actionManager.renderAction("loadScene")}
