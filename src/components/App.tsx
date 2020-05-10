506f3be8215cd935d79731c65ec617156d5e7f64
kbariotis <konmpar@gmail.com>
2020-05-10 18:45:14 +0100

    finalize on pointer up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2058,6 +2058,7 @@ class App extends React.Component<any, AppState> {
         this.setState({
           draggingElement: element,
           editingElement: element,
+          multiElement: element,
         });
       }
     } else {
@@ -2341,6 +2342,15 @@ class App extends React.Component<any, AppState> {
       window.removeEventListener(EVENT.POINTER_MOVE, onPointerMove);
       window.removeEventListener(EVENT.POINTER_UP, onPointerUp);
 
+      if (draggingElement?.type === "draw") {
+        this.actionManager.executeAction(actionFinalize);
+        this.setState((prevState) => ({
+          selectedElementIds: {
+            ...prevState.selectedElementIds,
+            [draggingElement.id]: true,
+          },
+        }));
+      }
       if (isLinearElement(draggingElement)) {
         if (draggingElement!.points.length > 1) {
           history.resumeRecording();
