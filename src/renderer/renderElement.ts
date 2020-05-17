072ffa0bafffc60b04afb82becc52da7f86a93db
dwelle <luzar.david@gmail.com>
2020-05-17 15:54:52 +0200

    deduplicate logic in generateRoughOptions

--- src/renderer/renderElement.ts
+++ src/renderer/renderElement.ts
@@ -153,94 +153,49 @@ export function invalidateShapeForElement(element: ExcalidrawElement) {
 }
 
 export function generateRoughOptions(element: ExcalidrawElement): Options {
-  const strokeLineDash =
-    element.strokeStyle === "dashed"
-      ? DASHARRAY_DASHED
-      : element.strokeStyle === "dotted"
-      ? DASHARRAY_DOTTED
-      : undefined;
-  // for non-solid strokes, disable multiStroke because it tends to make
-  //  dashes/dots overlay each other
-  const disableMultiStroke = element.strokeStyle !== "solid";
-  // for non-solid strokes, increase the width a bit to make it visually
-  //  similar to solid strokes, because we're also disabling multiStroke
-  const strokeWidth =
-    element.strokeStyle !== "solid"
-      ? element.strokeWidth + 0.5
-      : element.strokeWidth;
-  // when increasing strokeWidth, we must explicitly set fillWeight and
-  //  hachureGap because if not specified, roughjs uses strokeWidth to
-  //  calculate them (and we don't want the fills to be modified)
-  const fillWeight = element.strokeWidth / 2;
-  const hachureGap = element.strokeWidth * 4;
+  const options: Options = {
+    seed: element.seed,
+    strokeLineDash:
+      element.strokeStyle === "dashed"
+        ? DASHARRAY_DASHED
+        : element.strokeStyle === "dotted"
+        ? DASHARRAY_DOTTED
+        : undefined,
+    // for non-solid strokes, disable multiStroke because it tends to make
+    //  dashes/dots overlay each other
+    disableMultiStroke: element.strokeStyle !== "solid",
+    // for non-solid strokes, increase the width a bit to make it visually
+    //  similar to solid strokes, because we're also disabling multiStroke
+    strokeWidth:
+      element.strokeStyle !== "solid"
+        ? element.strokeWidth + 0.5
+        : element.strokeWidth,
+    // when increasing strokeWidth, we must explicitly set fillWeight and
+    //  hachureGap because if not specified, roughjs uses strokeWidth to
+    //  calculate them (and we don't want the fills to be modified)
+    fillWeight: element.strokeWidth / 2,
+    hachureGap: element.strokeWidth * 4,
+    roughness: element.roughness,
+    stroke: element.strokeColor,
+  };
 
   switch (element.type) {
-    case "rectangle": {
-      return {
-        strokeWidth,
-        fillWeight,
-        hachureGap,
-        strokeLineDash,
-        disableMultiStroke,
-        stroke: element.strokeColor,
-        fill:
-          element.backgroundColor === "transparent"
-            ? undefined
-            : element.backgroundColor,
-        fillStyle: element.fillStyle,
-        roughness: element.roughness,
-        seed: element.seed,
-      };
-    }
-    case "diamond": {
-      return {
-        strokeWidth,
-        fillWeight,
-        hachureGap,
-        strokeLineDash,
-        disableMultiStroke,
-        stroke: element.strokeColor,
-        fill:
-          element.backgroundColor === "transparent"
-            ? undefined
-            : element.backgroundColor,
-        fillStyle: element.fillStyle,
-        roughness: element.roughness,
-        seed: element.seed,
-      };
-    }
+    case "rectangle":
+    case "diamond":
     case "ellipse": {
-      return {
-        strokeWidth,
-        fillWeight,
-        hachureGap,
-        strokeLineDash,
-        disableMultiStroke,
-        stroke: element.strokeColor,
-        fill:
-          element.backgroundColor === "transparent"
-            ? undefined
-            : element.backgroundColor,
-        fillStyle: element.fillStyle,
-        roughness: element.roughness,
-        seed: element.seed,
-        curveFitting: 1,
-      };
+      options.fillStyle = element.fillStyle;
+      options.fill =
+        element.backgroundColor === "transparent"
+          ? undefined
+          : element.backgroundColor;
+      if (element.type === "ellipse") {
+        options.curveFitting = 1;
+      }
+      return options;
     }
     case "line":
     case "draw":
     case "arrow": {
-      const options: Options = {
-        strokeWidth,
-        fillWeight,
-        hachureGap,
-        strokeLineDash,
-        disableMultiStroke,
-        stroke: element.strokeColor,
-        seed: element.seed,
-        roughness: element.roughness,
-      };
-
       // If shape is a line and is a closed shape,
       // fill the shape if a color is set.
       if (element.type === "line" || element.type === "draw") {
