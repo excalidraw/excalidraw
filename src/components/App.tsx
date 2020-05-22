214949ea230f91a94581ef200c7b91458f990fda
Aakansha Doshi <aakansha1216@gmail.com>
2020-05-23 02:12:19 +0530

    perf(app.tsx): dynamically import socket.io-client when needed to split bundle size

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import socketIOClient from "socket.io-client";
 import rough from "roughjs/bin/rough";
 import { RoughCanvas } from "roughjs/bin/canvas";
 import { simplify, Point } from "points-on-curve";
@@ -792,7 +791,9 @@ class App extends React.Component<any, AppState> {
     this.portal.close();
   };
 
-  private initializeSocketClient = (opts: { showLoadingState: boolean }) => {
+  private initializeSocketClient = async (opts: {
+    showLoadingState: boolean;
+  }) => {
     if (this.portal.socket) {
       return;
     }
@@ -912,6 +913,11 @@ class App extends React.Component<any, AppState> {
           initialize();
         }
       };
+
+      const socketIOClientLib: any = await import(
+        /* webpackChunkName: "socketIoClient.min" */ "socket.io-client"
+      );
+      const socketIOClient = socketIOClientLib.default;
 
       this.portal.open(
         socketIOClient(SOCKET_SERVER),
