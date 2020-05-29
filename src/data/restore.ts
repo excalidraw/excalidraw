ea67fae74e2dbbe02422e4f1ec436d40ad06e299
Aakansha Doshi <aakansha1216@gmail.com>
2020-05-29 19:59:39 +0530

    pass scrollx,scrolly and zoom default values

--- src/data/restore.ts
+++ src/data/restore.ts
@@ -91,7 +91,7 @@ export const restore = (
     savedState = {
       ...savedState,
       ...calculateScrollCenter(elements, {
-        appState: savedState,
+        appState: { scrollX: 0, scrollY: 0, zoom: 1, ...savedState },
         canvas: null,
       }),
     };
