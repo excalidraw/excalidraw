52cb8aacc736e3ef1da2ab3a17e10c28b0b5258e
Oliver Benns <oliverbenns@gmail.com>
2020-06-10 21:54:51 +0100

    Snap to user

--- src/actions/actionNavigate.tsx
+++ src/actions/actionNavigate.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Avatar } from "../components/Avatar";
 import { register } from "./register";
 import { getClientColors, getClientShortName } from "../clients";
+import { Collaborator, FlooredNumber } from "../types";
+import { normalizeScroll } from "../scene";
 
 export const actionGoToCollaborator = register({
   name: "goToCollaborator",
   perform: (_elements, appState, value) => {
+    const point = value as Collaborator["pointer"];
+    if (!point) {
+      return { appState, commitToHistory: false };
+    }
+
     return {
       appState: {
         ...appState,
-        // scrollX:
-        // openMenu: appState.openMenu === "canvas" ? null : "canvas",
+        scrollX: normalizeScroll(window.innerWidth / 2 - point.x),
+        scrollY: normalizeScroll(window.innerHeight / 2 - point.y),
       },
       commitToHistory: false,
     };
@@ -33,7 +40,10 @@ export const actionGoToCollaborator = register({
     const shortName = getClientShortName(collaborator.username || "?");
 
     return (
-      <Avatar color={background} onClick={() => {}}>
+      <Avatar
+        color={background}
+        onClick={() => updateData(collaborator.pointer)}
+      >
         {shortName}
       </Avatar>
     );
