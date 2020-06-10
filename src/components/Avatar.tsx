1a6862262137c1562747e16d7f9fc9f49004e727
Oliver Benns <oliverbenns@gmail.com>
2020-06-10 21:07:50 +0100

    Update avatar props

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,12 +5,21 @@ import React from "react";
 type AvatarProps = {
   children: React.ReactNode;
   className?: string;
-  name: string;
+  onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
   color: string;
 };
 
-export const Avatar = ({ children, className, color }: AvatarProps) => (
-  <div className={`Avatar ${className}`} style={{ background: color }}>
+export const Avatar = ({
+  children,
+  className,
+  color,
+  onClick,
+}: AvatarProps) => (
+  <div
+    className={`Avatar ${className}`}
+    style={{ background: color }}
+    onClick={onClick}
+  >
     {children}
   </div>
 );
