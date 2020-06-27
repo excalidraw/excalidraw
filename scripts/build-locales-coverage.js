8b158f2687d7b01efc4b4806adfc6a83417c49eb
kbariotis <konmpar@gmail.com>
2020-06-27 16:56:41 +0100

    add script to calculate percentage of translation

---
+++ scripts/build-locales-coverage.js
@@ -0,0 +1,35 @@
+const { readdirSync, writeFileSync } = require("fs");
+const files = readdirSync(`${__dirname}/../src/locales`);
+
+const flatten = (object) =>
+  Object.keys(object).reduce(
+    (initial, current) => ({ ...initial, ...object[current] }),
+    {},
+  );
+
+const locales = files.filter(
+  (file) =>
+    file !== "README.md" &&
+    file !== "build-percentages.js" &&
+    file !== "en.json" &&
+    file !== "percentages.json",
+);
+
+const percentages = {};
+
+for (let i = 0; i < locales.length; i++) {
+  const data = flatten(require(`${__dirname}/../src/locales/${locales[i]}`));
+
+  const allKeys = Object.keys(data);
+  const translatedKeys = allKeys.filter((item) => data[item] !== "");
+
+  const percentage = (100 * translatedKeys.length) / allKeys.length;
+
+  percentages[locales[i]] = parseInt(percentage);
+}
+
+writeFileSync(
+  `${__dirname}/../src/locales/percentages.json`,
+  JSON.stringify(percentages),
+  "utf8",
+);
