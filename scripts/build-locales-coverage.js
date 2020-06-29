17a8e3de31a6b7fffe1d5610892ca45a10503c5c
kbariotis <konmpar@gmail.com>
2020-06-29 21:59:44 +0100

    test

--- scripts/build-locales-coverage.js
+++ scripts/build-locales-coverage.js
@@ -1,32 +1,37 @@
 const { readdirSync, writeFileSync } = require("fs");
 const files = readdirSync(`${__dirname}/../src/locales`);
+try {
+  const flatten = (object) =>
+    Object.keys(object).reduce(
+      (initial, current) => ({ ...initial, ...object[current] }),
+      {},
+    );
 
-const flatten = (object) =>
-  Object.keys(object).reduce(
-    (initial, current) => ({ ...initial, ...object[current] }),
-    {},
+  const locales = files.filter(
+    (file) => file !== "README.md" && file !== "percentages.json",
   );
 
-const locales = files.filter(
-  (file) => file !== "README.md" && file !== "percentages.json",
-);
+  const percentages = {};
 
-const percentages = {};
+  for (let index = 0; index < locales.length; index++) {
+    const currentLocale = locales[index];
+    const data = flatten(
+      require(`${__dirname}/../src/locales/${currentLocale}`),
+    );
 
-for (let index = 0; index < locales.length; index++) {
-  const currentLocale = locales[index];
-  const data = flatten(require(`${__dirname}/../src/locales/${currentLocale}`));
+    const allKeys = Object.keys(data);
+    const translatedKeys = allKeys.filter((item) => data[item] !== "");
 
-  const allKeys = Object.keys(data);
-  const translatedKeys = allKeys.filter((item) => data[item] !== "");
+    const percentage = (100 * translatedKeys.length) / allKeys.length;
 
-  const percentage = (100 * translatedKeys.length) / allKeys.length;
+    percentages[currentLocale.replace(".json", "")] = parseInt(percentage);
+  }
 
-  percentages[currentLocale.replace(".json", "")] = parseInt(percentage);
+  writeFileSync(
+    `${__dirname}/../src/locales/percentages.json`,
+    JSON.stringify(percentages),
+    "utf8",
+  );
+} catch (e) {
+  console.log(e); // eslint-disable-line
 }
-
-writeFileSync(
-  `${__dirname}/../src/locales/percentages.json`,
-  JSON.stringify(percentages),
-  "utf8",
-);
