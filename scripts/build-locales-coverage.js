231a53a44e8986e82a8d1e772bc754e0cbbcd36a
Lipis <lipiridis@gmail.com>
2020-07-07 18:30:58 +0300

    New Crowdin updates (#1856)

--- scripts/build-locales-coverage.js
+++ scripts/build-locales-coverage.js
@@ -27,6 +27,6 @@ for (let index = 0; index < locales.length; index++) {
 
 writeFileSync(
   `${__dirname}/../src/locales/percentages.json`,
-  JSON.stringify(percentages, null, 2),
+  `${JSON.stringify(percentages, null, 2)}\n`,
   "utf8",
 );
