ed3b91ac219be525199420944dd3a1d4028c1e5a
tomfejer <tamas.fejer@prezi.com>
2020-08-15 11:19:07 +0200

    initial commit

--- src/shapes.tsx
+++ src/shapes.tsx
@@ -5,8 +5,15 @@ export const SHAPES = [
   {
     icon: (
       // fa-mouse-pointer
-      <svg viewBox="0 0 320 512" className="">
-        <path d="M302.189 329.126H196.105l55.831 135.993c3.889 9.428-.555 19.999-9.444 23.999l-49.165 21.427c-9.165 4-19.443-.571-23.332-9.714l-53.053-129.136-86.664 89.138C18.729 472.71 0 463.554 0 447.977V18.299C0 1.899 19.921-6.096 30.277 5.443l284.412 292.542c11.472 11.179 3.007 31.141-12.5 31.141z" />
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3zM13 13l6 6"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "selection",
@@ -14,9 +21,15 @@ export const SHAPES = [
   },
   {
     icon: (
-      // fa-square
-      <svg viewBox="0 0 448 512">
-        <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z" />
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M19 3H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "rectangle",
@@ -24,9 +37,15 @@ export const SHAPES = [
   },
   {
     icon: (
-      // custom
-      <svg viewBox="0 0 223.646 223.646">
-        <path d="M111.823 0L16.622 111.823 111.823 223.646 207.025 111.823z" />
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M12.5 2.5L6 12l6.5 9.5L19 12l-6.5-9.5z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "diamond",
@@ -34,19 +53,31 @@ export const SHAPES = [
   },
   {
     icon: (
-      // fa-circle
-      <svg viewBox="0 0 512 512">
-        <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z" />
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "ellipse",
-    key: "e",
+    key: ["e", "o"],
   },
   {
     icon: (
-      // fa-long-arrow-alt-right
-      <svg viewBox="0 0 448 512" className="rtl-mirror">
-        <path d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z" />
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M5 12h14M12 5l7 7-7 7"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "arrow",
@@ -54,15 +85,14 @@ export const SHAPES = [
   },
   {
     icon: (
-      // custom
-      <svg viewBox="0 0 6 6">
-        <line
-          x1="0"
-          y1="3"
-          x2="6"
-          y2="3"
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M5 12h14"
           stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
           strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     ),
@@ -71,12 +101,15 @@ export const SHAPES = [
   },
   {
     icon: (
-      // fa-pencil
-      <svg viewBox="0 0 512 512">
+      <svg width={24} height={24} fill="none">
         <path
-          fill="currentColor"
-          d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"
-        ></path>
+          d="M12 20h9M16.5 3.5a2.121 2.121 0 113 3L7 19l-4 1 1-4L16.5 3.5z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "draw",
@@ -84,9 +117,15 @@ export const SHAPES = [
   },
   {
     icon: (
-      // fa-font
-      <svg viewBox="0 0 448 512">
-        <path d="M432 416h-23.41L277.88 53.69A32 32 0 0 0 247.58 32h-47.16a32 32 0 0 0-30.3 21.69L39.41 416H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16h-19.58l23.3-64h152.56l23.3 64H304a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM176.85 272L224 142.51 271.15 272z" />
+      <svg width={24} height={24} fill="none">
+        <path
+          d="M4 7V4h16v3M9 20h6M12 4v16"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     ),
     value: "text",
