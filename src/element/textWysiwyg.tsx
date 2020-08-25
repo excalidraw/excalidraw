38d7bd4cb82ae7a581fb0c91171cd90a9e8fe1cf
Pedro Baracho <pedropbaracho@gmail.com>
2020-08-25 02:38:03 -0700

    Fix #2075 - Pinch zooming while typing text breaks the UI on Chrome Mac OS X (#2076)

--- src/element/textWysiwyg.tsx
+++ src/element/textWysiwyg.tsx
@@ -130,6 +130,7 @@ export const textWysiwyg = ({
   };
 
   const stopEvent = (event: Event) => {
+    event.preventDefault();
     event.stopPropagation();
   };
 
@@ -198,7 +199,10 @@ export const textWysiwyg = ({
   //  device keyboard is opened.
   window.addEventListener("resize", updateWysiwygStyle);
   window.addEventListener("pointerdown", onPointerDown);
-  window.addEventListener("wheel", stopEvent, true);
+  window.addEventListener("wheel", stopEvent, {
+    passive: false,
+    capture: true,
+  });
   document.body.appendChild(editable);
   editable.focus();
   editable.select();
