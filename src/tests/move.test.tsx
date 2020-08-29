bd1327682eece0a6155efb17280d5aada45f148a
Anton <anton.matrenin@introduct.tech>
2020-08-29 12:02:13 +0300

    Fix keypress rebinding

--- src/tests/move.test.tsx
+++ src/tests/move.test.tsx
@@ -4,6 +4,12 @@ import { render, fireEvent } from "./test-utils";
 import App from "../components/App";
 import * as Renderer from "../renderer/renderScene";
 import { reseed } from "../random";
+import { bindOrUnbindLinearElement } from "../element/binding";
+import {
+  ExcalidrawLinearElement,
+  NonDeleted,
+  ExcalidrawRectangleElement,
+} from "../element/types";
 
 // Unmount ReactDOM from root
 ReactDOM.unmountComponentAtNode(document.getElementById("root")!);
@@ -49,6 +55,78 @@ describe("move element", () => {
     expect([h.elements[0].x, h.elements[0].y]).toEqual([0, 40]);
 
     h.elements.forEach((element) => expect(element).toMatchSnapshot());
+  });
+
+  it("rectangles with binding arrow", () => {
+    const { getByToolName, container } = render(<App />);
+    const canvas = container.querySelector("canvas")!;
+
+    {
+      // create the first rectangle
+      const toolRect = getByToolName("rectangle");
+      fireEvent.click(toolRect);
+      fireEvent.pointerDown(canvas, { clientX: 0, clientY: 0 });
+      fireEvent.pointerMove(canvas, { clientX: 100, clientY: 1000 });
+      fireEvent.pointerUp(canvas);
+
+      // create the second rectangle
+      fireEvent.click(toolRect);
+      fireEvent.pointerDown(canvas, { clientX: 200, clientY: 0 });
+      fireEvent.pointerMove(canvas, { clientX: 300, clientY: 1000 });
+      fireEvent.pointerUp(canvas);
+
+      // create a line
+      const toolLine = getByToolName("line");
+      fireEvent.click(toolLine);
+      fireEvent.pointerDown(canvas, { clientX: 110, clientY: 50 });
+      fireEvent.pointerMove(canvas, { clientX: 190, clientY: 51 });
+      fireEvent.pointerUp(canvas);
+
+      // bind line to two rectangles
+      bindOrUnbindLinearElement(
+        h.elements[2] as NonDeleted<ExcalidrawLinearElement>,
+        h.elements[0] as ExcalidrawRectangleElement,
+        h.elements[1] as ExcalidrawRectangleElement,
+      );
+
+      // select the second rectangles
+      fireEvent.pointerDown(canvas, { clientX: 200, clientY: 0 });
+      fireEvent.pointerUp(canvas);
+
+      expect(renderScene).toHaveBeenCalledTimes(19);
+      expect(h.state.selectionElement).toBeNull();
+      expect(h.elements.length).toEqual(3);
+      expect(h.state.selectedElementIds[h.elements[1].id]).toBeTruthy();
+      expect([h.elements[0].x, h.elements[0].y]).toEqual([0, 0]);
+      expect([h.elements[1].x, h.elements[1].y]).toEqual([200, 0]);
+      expect([h.elements[2].x, h.elements[2].y]).toEqual([110, 50]);
+      expect([h.elements[2].width, h.elements[2].height]).toEqual([80, 1]);
+
+      renderScene.mockClear();
+    }
+
+    // Move selected rectangle
+    fireEvent.keyDown(canvas, { key: "ArrowRight", code: "ArrowRight" });
+    fireEvent.keyDown(canvas, { key: "ArrowDown", code: "ArrowDown" });
+    fireEvent.keyDown(canvas, { key: "ArrowDown", code: "ArrowDown" });
+
+    // Check that the arrow size has been changed according to moving the rectangle
+    expect(renderScene).toHaveBeenCalledTimes(3);
+    expect(h.state.selectionElement).toBeNull();
+    expect(h.elements.length).toEqual(3);
+    expect(h.state.selectedElementIds[h.elements[1].id]).toBeTruthy();
+    expect([h.elements[0].x, h.elements[0].y]).toEqual([0, 0]);
+    expect([h.elements[1].x, h.elements[1].y]).toEqual([201, 2]);
+    expect([Math.round(h.elements[2].x), Math.round(h.elements[2].y)]).toEqual([
+      110,
+      50,
+    ]);
+    expect([
+      Math.round(h.elements[2].width),
+      Math.round(h.elements[2].height),
+    ]).toEqual([81, 2]);
+
+    // h.elements.forEach((element) => expect(element).toMatchSnapshot());
   });
 });
 
