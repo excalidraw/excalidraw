4bd6b582aa96672c35ebcd1f9b2058b7204021cb
Guillaume Grossetie <ggrossetie@gmail.com>
2020-10-17 12:13:52 +0200

    Add two functions to convert an Excalidraw diagram definition to a SVG string or a PNG string
    Introduce a new package `@excalidraw/excalidraw-utils`.

---
+++ src/excalidraw-utils/webpack.prod.config.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "excalidraw-utils.js",
+    library: "ExcalidrawUtils",
+    libraryTarget: "umd",
+  },
+  entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx|mjs)$/,
+        exclude: /node_modules\/(?!(roughjs|socket.io-client|browser-nativefs)\/).*/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              configFile: path.resolve(__dirname, "tsconfig.prod.json"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
+};
