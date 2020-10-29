4dbc4c7912dbb06cea6be4cd563fea18b78963eb
Guillaume Grossetie <ggrossetie@gmail.com>
2020-10-29 20:11:07 +0100

    Add .min.js suffix

--- packages/utils/webpack.prod.config.js
+++ packages/utils/webpack.prod.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "excalidraw-utils.js",
+    filename: "excalidraw-utils.min.js",
     library: "ExcalidrawUtils",
     libraryTarget: "umd",
   },
