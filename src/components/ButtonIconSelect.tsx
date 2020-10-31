a77695df0aea2538fa89c42f954c046b1833d2d7
Noel Schnierer <noel.schnierer@gmail.com>
2020-10-31 14:06:49 +0100

    enhance fill color preview

--- src/components/ButtonIconSelect.tsx
+++ src/components/ButtonIconSelect.tsx
@@ -7,13 +7,15 @@ export const ButtonIconSelect = <T extends Object>({
   value,
   onChange,
   group,
+  className,
 }: {
   options: { value: T; text: string; icon: JSX.Element }[];
   value: T | null;
   onChange: (value: T) => void;
   group: string;
+  className?: string;
 }) => (
-  <div className="buttonList buttonListIcon">
+  <div className={clsx("buttonList buttonListIcon", className)}>
     {options.map((option) => (
       <label
         key={option.text}
