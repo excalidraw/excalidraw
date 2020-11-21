89cbc046b8c754929cead3c697112384afb26523
Aakansha Doshi <aakansha1216@gmail.com>
2020-11-21 21:10:10 +0530

    no need to mock localstorage api, looks like its already mocked in cra

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,6 @@
 import "@testing-library/jest-dom";
 import "jest-canvas-mock";
 
-import "./tests/helpers/mockLocalStorage";
-
 // ReactDOM is located inside index.tsx file
 // as a result, we need a place for it to render into
 const element = document.createElement("div");
