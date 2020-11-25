14bbcf95f99a46f7be8566da68a34f702ebdda1c
Jed Fox <git@jedfox.com>
2020-11-25 11:56:12 -0500

    Fix RTL layout issues in in zen mode and collaboration

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -403,9 +403,9 @@ export const users = createIcon(
   { width: 640, height: 512, mirror: true },
 );
 
+// not mirrored because it's inspired by a playback control, which is always RTL
 export const start = createIcon(
   "M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm115.7 272l-176 101c-15.8 8.8-35.7-2.5-35.7-21V152c0-18.4 19.8-29.8 35.7-21l176 107c16.4 9.2 16.4 32.9 0 42z",
-  { mirror: true },
 );
 
 export const stop = createIcon(
