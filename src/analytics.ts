ff3e65ddc9b8114e806991b7585ef3eba023f202
dwelle <luzar.david@gmail.com>
2020-12-02 14:13:58 +0100

    reference gtag via window to fix tests

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,13 +1,11 @@
-declare let gtag: Function;
-
 export const EVENT_ACTION = "action";
 export const EVENT_EXIT = "exit";
 export const EVENT_CHANGE = "change";
 export const EVENT_SHAPE = "shape";
 
-export const trackEvent = gtag
+export const trackEvent = window.gtag
   ? (name: string, category: string, label?: string, value?: number) => {
-      gtag("event", name, {
+      window.gtag("event", name, {
         event_category: category,
         event_label: label,
         value,
