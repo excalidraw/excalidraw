255cb052b8a145b910d045de4c7e884c965a5aea
dwelle <luzar.david@gmail.com>
2020-12-05 18:06:31 +0100

    debounce storage sizes getter & rewrite to hooks

--- src/components/StatsForNerds.tsx
+++ src/components/StatsForNerds.tsx
@@ -1,132 +1,140 @@
 import "./StatsForNerds.scss";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppState } from "../types";
-import {
-  ExcalidrawElement,
-  NonDeletedExcalidrawElement,
-} from "../element/types";
+import { NonDeletedExcalidrawElement } from "../element/types";
 import { getCommonBoundingBox } from "../element/bounds";
 import { getSelectedElements } from "../scene";
 import {
   getElementsStorageSize,
   getTotalStorageSize,
 } from "../excalidraw-app/data/localStorage";
-import { nFormatter } from "../utils";
+import { debounce, nFormatter } from "../utils";
 
-type StatsForNerdsProps = {
+type StorageSizes = { scene: number; total: number };
+
+const getStorageSizes = debounce((cb: (sizes: StorageSizes) => void) => {
+  cb({
+    scene: getElementsStorageSize(),
+    total: getTotalStorageSize(),
+  });
+}, 500);
+
+export const StatsForNerds = (props: {
   appState: AppState;
   elements: readonly NonDeletedExcalidrawElement[];
+}) => {
+  const [storageSizes, setStorageSizes] = useState<StorageSizes>({
+    scene: 0,
+    total: 0,
+  });
+
+  useEffect(() => {
+    getStorageSizes((sizes) => {
+      setStorageSizes(sizes);
+    });
+  });
+
+  const boundingBox = getCommonBoundingBox(props.elements);
+
+  const selectedElements = getSelectedElements(props.elements, props.appState);
+
+  const selectedBoundingBox = getCommonBoundingBox(selectedElements);
+  return (
+    <div className="StatsForNerds">
+      <h3>{"Stats for nerds"}</h3>
+      <table>
+        <tr>
+          <th colSpan={2}>{"Scene"}</th>
+        </tr>
+        <tr>
+          <td>{"Elements"}</td>
+          <td>{props.elements.length}</td>
+        </tr>
+        <tr>
+          <td>{"Width"}</td>
+          <td>{Math.round(boundingBox[2]) - Math.round(boundingBox[0])}</td>
+        </tr>
+        <tr>
+          <td>{"Height"}</td>
+          <td>{Math.round(boundingBox[3]) - Math.round(boundingBox[1])}</td>
+        </tr>
+        <tr>
+          <th colSpan={2}>{"Storage"}</th>
+        </tr>
+        <tr>
+          <td>{"Scene"}</td>
+          <td>{nFormatter(storageSizes.scene, 1)}</td>
+        </tr>
+        <tr>
+          <td>{"Total"}</td>
+          <td>{nFormatter(storageSizes.total, 1)}</td>
+        </tr>
+
+        {selectedElements.length === 1 && (
+          <>
+            <tr>
+              <th colSpan={2}>{"Element"}</th>
+            </tr>
+          </>
+        )}
+
+        {selectedElements.length > 1 && (
+          <>
+            <tr>
+              <th colSpan={2}>{"Selection"}</th>
+            </tr>
+            <tr>
+              <td>{"Elements"}</td>
+              <td>{selectedElements.length}</td>
+            </tr>
+          </>
+        )}
+        {selectedElements.length > 0 && (
+          <>
+            <tr>
+              <td>{"x"}</td>
+              <td>
+                {Math.round(
+                  selectedElements.length === 1
+                    ? selectedElements[0].x
+                    : selectedBoundingBox[0],
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>{"y"}</td>
+              <td>
+                {Math.round(
+                  selectedElements.length === 1
+                    ? selectedElements[0].y
+                    : selectedBoundingBox[1],
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>{"Width"}</td>
+              <td>
+                {Math.round(
+                  selectedElements.length === 1
+                    ? selectedElements[0].width
+                    : selectedBoundingBox[2] - selectedBoundingBox[0],
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>{"Height"}</td>
+              <td>
+                {Math.round(
+                  selectedElements.length === 1
+                    ? selectedElements[0].height
+                    : selectedBoundingBox[3] - selectedBoundingBox[1],
+                )}
+              </td>
+            </tr>
+          </>
+        )}
+      </table>
+    </div>
+  );
 };
-
-export class StatsForNerds extends React.Component<StatsForNerdsProps> {
-  public render() {
-    const boundingBox = getCommonBoundingBox(
-      this.props.elements as ExcalidrawElement[],
-    );
-
-    const selectedElements = getSelectedElements(
-      this.props.elements,
-      this.props.appState,
-    );
-
-    const selectedBoundingBox = getCommonBoundingBox(selectedElements);
-    return (
-      <div className="StatsForNerds">
-        <h3>{"Stats for nerds"}</h3>
-        <table>
-          <tr>
-            <th colSpan={2}>{"Scene"}</th>
-          </tr>
-          <tr>
-            <td>{"Elements"}</td>
-            <td>{this.props.elements.length}</td>
-          </tr>
-          <tr>
-            <td>{"Width"}</td>
-            <td>{Math.round(boundingBox[2]) - Math.round(boundingBox[0])}</td>
-          </tr>
-          <tr>
-            <td>{"Height"}</td>
-            <td>{Math.round(boundingBox[3]) - Math.round(boundingBox[1])}</td>
-          </tr>
-          <tr>
-            <th colSpan={2}>{"Storage"}</th>
-          </tr>
-          <tr>
-            <td>{"Scene"}</td>
-            <td>{nFormatter(getElementsStorageSize(), 1)}</td>
-          </tr>
-          <tr>
-            <td>{"Total"}</td>
-            <td>{nFormatter(getTotalStorageSize(), 1)}</td>
-          </tr>
-
-          {selectedElements.length === 1 && (
-            <>
-              <tr>
-                <th colSpan={2}>{"Element"}</th>
-              </tr>
-            </>
-          )}
-
-          {selectedElements.length > 1 && (
-            <>
-              <tr>
-                <th colSpan={2}>{"Selection"}</th>
-              </tr>
-              <tr>
-                <td>{"Elements"}</td>
-                <td>{selectedElements.length}</td>
-              </tr>
-            </>
-          )}
-          {selectedElements.length > 0 && (
-            <>
-              <tr>
-                <td>{"x"}</td>
-                <td>
-                  {Math.round(
-                    selectedElements.length === 1
-                      ? selectedElements[0].x
-                      : selectedBoundingBox[0],
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td>{"y"}</td>
-                <td>
-                  {Math.round(
-                    selectedElements.length === 1
-                      ? selectedElements[0].y
-                      : selectedBoundingBox[1],
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td>{"Width"}</td>
-                <td>
-                  {Math.round(
-                    selectedElements.length === 1
-                      ? selectedElements[0].width
-                      : selectedBoundingBox[2] - selectedBoundingBox[0],
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td>{"Height"}</td>
-                <td>
-                  {Math.round(
-                    selectedElements.length === 1
-                      ? selectedElements[0].height
-                      : selectedBoundingBox[3] - selectedBoundingBox[1],
-                  )}
-                </td>
-              </tr>
-            </>
-          )}
-        </table>
-      </div>
-    );
-  }
-}
