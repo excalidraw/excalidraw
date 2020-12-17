2df234ee9a7d4d2352df6081b8fc34754d9e710d
dwelle <luzar.david@gmail.com>
2020-12-17 18:57:43 +0100

    succeed if changelog already updated

--- scripts/changelog-check.js
+++ scripts/changelog-check.js
@@ -14,6 +14,10 @@ exec("git diff origin/master --cached --name-only", (error, stdout, stderr) => {
     process.exit(1);
   }
 
+  if (!stdout || stdout.includes("packages/excalidraw/CHANGELOG.MD")) {
+    process.exit(0);
+  }
+
   const changedFiles = stdout.trim().split("\n").map(normalizePath);
 
   const excalidrawPackageFiles = changedFiles.filter((filename) => {
