c085eaad47a6caaeae3ab47a9b3a1f60a3daceed
Aakansha Doshi <monstershome@gmail.com>
2020-12-20 16:28:46 +0530

    fix(appstate.ts): Remove unnecessary console.error as it was polluting Sentry (#2637)

--- src/appState.ts
+++ src/appState.ts
@@ -166,11 +166,6 @@ const _clearAppStateForStorage = <ExportType extends "export" | "browser">(
   const stateForExport = {} as { [K in ExportableKeys]?: typeof appState[K] };
   for (const key of Object.keys(appState) as (keyof typeof appState)[]) {
     const propConfig = APP_STATE_STORAGE_CONF[key];
-    if (!propConfig) {
-      console.error(
-        `_clearAppStateForStorage: appState key "${key}" config doesn't exist for "${exportType}" export type`,
-      );
-    }
     if (propConfig?.[exportType]) {
       // @ts-ignore see https://github.com/microsoft/TypeScript/issues/31445
       stateForExport[key] = appState[key];
