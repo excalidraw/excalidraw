6d7af33363310a9d88a3bf3aa06959300b8c2223
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-12-21 18:45:42 +0200

    refactor: Remove duplicate entry from en.json

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3651,7 +3651,7 @@ class App extends React.Component<ExcalidrawProps, AppState> {
           {
             checked: this.state.showStats,
             shortcutName: "stats",
-            label: t("labels.stats"),
+            label: t("stats.title"),
             action: this.toggleStats,
           },
         ],
