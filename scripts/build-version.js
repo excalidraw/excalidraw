4e413f33845ff03fcefc62eb08577199dfed95e9
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-01-10 20:27:30 +0200

    update

--- scripts/build-version.js
+++ scripts/build-version.js
@@ -5,17 +5,7 @@ const path = require("path");
 const versionFile = path.join("build", "version.json");
 const indexFile = path.join("build", "index.html");
 
-const zero = (digit) => `0${digit}`.slice(-2);
-
-const versionDate = (date) => {
-  const date_ = `${date.getFullYear()}-${zero(date.getMonth() + 1)}-${zero(
-    date.getDate(),
-  )}`;
-  const time = `${zero(date.getHours())}-${zero(date.getMinutes())}-${zero(
-    date.getSeconds(),
-  )}`;
-  return `${date_}-${time}`;
-};
+const versionDate = (date) => date.toISOString();
 
 const commitHash = () => {
   try {
