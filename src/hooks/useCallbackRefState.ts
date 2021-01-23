08bbfdef608ccdcde01b27191317921907f7c044
dwelle <luzar.david@gmail.com>
2021-01-23 17:26:35 +0100

    properly type refCallback

--- src/hooks/useCallbackRefState.ts
+++ src/hooks/useCallbackRefState.ts
@@ -2,6 +2,6 @@ import { useCallback, useState } from "react";
 
 export const useCallbackRefState = <T>() => {
   const [refValue, setRefValue] = useState<T | null>(null);
-  const refCallback = useCallback((value: T) => setRefValue(value), []);
+  const refCallback = useCallback((value: T | null) => setRefValue(value), []);
   return [refValue, refCallback] as const;
 };
