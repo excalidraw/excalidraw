4624ec2bd6ac9275b65fa158f0b019177ca6e283
Aakansha Doshi <aakansha1216@gmail.com>
2021-01-29 23:38:37 +0530

    fix: apply initialData appState for zenmode and grid stats and refactor check param for actions (#2871)
    * fix: pass default value for grid mode / zen mode so it sets the value from initialData appState
    
    fixes #2870
    
    * change checked from boolean to be a function which recieves appState and returns boolean
    
    * fix
    
    * use clsx
    
    Co-authored-by: dwelle <luzar.david@gmail.com>

--- src/actions/actionToggleGridMode.tsx
+++ src/actions/actionToggleGridMode.tsx
@@ -1,21 +1,20 @@
 import { CODES, KEYS } from "../keys";
 import { register } from "./register";
 import { GRID_SIZE } from "../constants";
+import { AppState } from "../types";
 
 export const actionToggleGridMode = register({
   name: "gridMode",
   perform(elements, appState) {
-    this.checked = !this.checked;
     return {
       appState: {
         ...appState,
-        gridSize: this.checked ? GRID_SIZE : null,
+        gridSize: this.checked!(appState) ? null : GRID_SIZE,
       },
       commitToHistory: false,
     };
   },
-  checked: false,
+  checked: (appState: AppState) => appState.gridSize !== null,
   contextItemLabel: "labels.gridMode",
-  // Wrong event code
   keyTest: (event) => event[KEYS.CTRL_OR_CMD] && event.code === CODES.QUOTE,
 });
