4d75a2c7950bb6bdeb1b4276073c1c7ea6588d92
Arun <arun4033622@gmail.com>
2021-02-14 19:28:33 +0530

    Hide scrollbars on mobile

--- src/renderer/renderScene.ts
+++ src/renderer/renderScene.ts
@@ -49,6 +49,7 @@ import {
 } from "../element/transformHandles";
 import { viewportCoordsToSceneCoords, supportsEmoji } from "../utils";
 import { UserIdleState } from "../excalidraw-app/collab/types";
+import { isMobile } from "../is-mobile";
 
 const hasEmojiSupport = supportsEmoji();
 
@@ -186,7 +187,7 @@ export const renderScene = (
   sceneState: SceneState,
   // extra options, currently passed by export helper
   {
-    renderScrollbars = true,
+    renderScrollbars = !isMobile(),
     renderSelection = true,
     // Whether to employ render optimizations to improve performance.
     // Should not be turned on for export operations and similar, because it
