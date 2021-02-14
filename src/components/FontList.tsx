aa780b8e80eeccdfaa973332c1dfdec844eb3bdf
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-02-14 13:55:41 +0200

    suggest

--- src/components/FontList.tsx
+++ src/components/FontList.tsx
@@ -28,20 +28,30 @@ export const FontList = ({
 }: {
   onChange: (value: typeof FONTS[number]) => void;
 }) => (
-  <select
-    className="FontList"
-    onChange={({ target }) => {
-      const meta = FONTS.find((f) => f.name === target.value)!;
-      if (meta.path) {
-        style.textContent = css(meta);
-      }
-      onChange(meta);
-    }}
-  >
-    {FONTS.map((font) => (
-      <option key={font.name} value={font.name}>
-        {font.name}
-      </option>
-    ))}
-  </select>
+  <>
+    <select
+      className="FontList"
+      onChange={({ target }) => {
+        const meta = FONTS.find((f) => f.name === target.value)!;
+        if (meta.path) {
+          style.textContent = css(meta);
+        }
+        onChange(meta);
+      }}
+    >
+      {FONTS.map((font) => (
+        <option key={font.name} value={font.name}>
+          {font.name}
+        </option>
+      ))}
+    </select>
+    <a
+      className="suggest-link"
+      href="https://github.com/excalidraw/excalidraw/issues/2945"
+      target="_blank"
+      rel="noreferrer"
+    >
+      {"Suggest yours on GitHub"}
+    </a>
+  </>
 );
