6f12ab6cfa20fcb86bc5f77f1dfc30ae0126f894
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-02-18 13:11:46 +0200

    Remove rock

--- src/components/FontList.tsx
+++ src/components/FontList.tsx
@@ -8,7 +8,6 @@ const FONTS = [
   { name: "Comic Relief", path: "/fonts/ComicRelief.ttf" },
   { name: "Fewriter", path: "/fonts/fewriter_memesbruh03.ttf" },
   { name: "GelPen", path: "/fonts/GelPenLight.ttf" },
-  { name: "RocknRoll One", path: "/fonts/RocknRollOne-Regular.ttf" },
   { name: "Swagger", path: "/fonts/swaggerlight.ttf" },
   { name: "Xarrovv", path: "/fonts/Xarrovv.otf" },
 ];
