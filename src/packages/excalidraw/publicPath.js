0ada71a3ce4ec07a9ea608652461c275487400fc
Aakansha Doshi <aakansha1216@gmail.com>
2021-02-20 23:38:13 +0530

    assign only when env not test

--- src/packages/excalidraw/publicPath.js
+++ src/packages/excalidraw/publicPath.js
@@ -1,5 +1,9 @@
-/* eslint-disable */
-/* global __webpack_public_path__:writable */
-__webpack_public_path__ =
-  window.EXCALIDRAW_ASSET_PATH ||
-  "https://unpkg.com/aakansha-excalidraw@0.4.0-asset/dist/";
+import { ENV } from "../../constants";
+
+if (process.env.NODE_ENV !== ENV.TEST) {
+  /* eslint-disable */
+  /* global __webpack_public_path__:writable */
+  __webpack_public_path__ =
+    window.EXCALIDRAW_ASSET_PATH ||
+    "https://unpkg.com/aakansha-excalidraw@0.4.0-asset/dist/";
+}
