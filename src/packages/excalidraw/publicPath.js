b5a7e265790c269c8f961de984e26298feacd6ca
Aakansha Doshi <aakansha1216@gmail.com>
2021-02-20 21:02:39 +0530

    add public path

--- src/packages/excalidraw/publicPath.js
+++ src/packages/excalidraw/publicPath.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 /* global __webpack_public_path__:writable */
-//@ts-ignore
 __webpack_public_path__ =
   window.EXCALIDRAW_ASSET_PATH ||
   "https://unpkg.com/aakansha-excalidraw@0.4.0-asset/dist/";
