73d0947d880dc255be300715f239b017c4a66e0e
Aakansha Doshi <aakansha1216@gmail.com>
2021-02-21 15:09:19 +0530

    Append content hash to excalidraw-assets so cache bursting happens when version update

--- src/packages/excalidraw/webpack.prod.config.js
+++ src/packages/excalidraw/webpack.prod.config.js
@@ -13,7 +13,7 @@ module.exports = {
     library: "Excalidraw",
     libraryTarget: "umd",
     filename: "[name].js",
-    chunkFilename: "excalidraw-assets/[name].js",
+    chunkFilename: "excalidraw-assets/[name]-[contenthash].js",
     publicPath: "",
   },
   resolve: {
