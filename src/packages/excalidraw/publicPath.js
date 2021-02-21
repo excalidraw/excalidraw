e97ed2d7e33c91f12aaac108819e581a975356a2
Aakansha Doshi <aakansha1216@gmail.com>
2021-02-21 20:07:43 +0530

    update

--- src/packages/excalidraw/publicPath.js
+++ src/packages/excalidraw/publicPath.js
@@ -5,5 +5,5 @@ if (process.env.NODE_ENV !== ENV.TEST) {
   /* global __webpack_public_path__:writable */
   __webpack_public_path__ =
     window.EXCALIDRAW_ASSET_PATH ||
-    `https://unpkg.com/@excalidraw/excalidraw@${pkg.version}/dist/`;
+    `https://unpkg.com/${pkg.name}@${pkg.version}/dist/`;
 }
