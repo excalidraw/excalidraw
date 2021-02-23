899f837aa877b77e9707ca5a28f6c97b94865f3a
Daniel J. Geiger <1852529+DanielJGeiger@users.noreply.github.com>
2021-02-22 22:24:00 -0600

    Satisfy webpack.

--- src/packages/utils/webpack.prod.config.js
+++ src/packages/utils/webpack.prod.config.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    fallback: { path: require.resolve("path-browserify") },
   },
   optimization: {
     runtimeChunk: false,
