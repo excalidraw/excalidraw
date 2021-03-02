8266836dbfe14edd9ba6ae39e8b0307d1617498b
dwelle <luzar.david@gmail.com>
2021-03-02 10:20:16 +0100

    fix: host workbox locally

--- src/service-worker.js
+++ src/service-worker.js
@@ -17,9 +17,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js",
-);
+importScripts("/workbox-sw.js");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
