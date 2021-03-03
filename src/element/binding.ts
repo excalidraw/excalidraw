5926ea4f96b48cd4a5d86e5f20b29ccf28ed8f8d
Aakansha Doshi <aakansha1216@gmail.com>
2021-03-03 16:16:43 +0530

    fix

--- src/element/binding.ts
+++ src/element/binding.ts
@@ -182,11 +182,9 @@ const bindLinearElement = (
     } as PointBinding,
   });
   mutateElement(hoveredElement, {
-    boundElementIds: [
-      ...Array.from(
-        new Set([...(hoveredElement.boundElementIds ?? []), linearElement.id]),
-      ),
-    ],
+    boundElementIds: Array.from(
+      new Set([...(hoveredElement.boundElementIds ?? []), linearElement.id]),
+    ),
   });
 };
 
