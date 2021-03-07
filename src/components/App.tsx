e559f831ce0e3c9d184523b2b6d3c2d9c4c2a741
Aakansha Doshi <aakansha1216@gmail.com>
2021-03-07 21:12:10 +0530

    fix: Position text editor absolute and fix the offsets so it doesn't remain fixed when container is scrolled (#3200)
    * fix: Position text editor absolute and fix the offsets so it doesn't remain fixed when container is scrolled
    
    * update changelog
    
    * Update src/packages/excalidraw/CHANGELOG.md

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1578,7 +1578,10 @@ class App extends React.Component<ExcalidrawProps, AppState> {
           },
           this.state,
         );
-        return [viewportX, viewportY];
+        return [
+          viewportX - this.state.offsetLeft,
+          viewportY - this.state.offsetTop,
+        ];
       },
       onChange: withBatchedUpdates((text) => {
         updateElement(text);
