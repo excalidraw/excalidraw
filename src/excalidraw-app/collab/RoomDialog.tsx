8ffe3ff6c8ca971708f246265e46f39fdf922695
Arun Kumar <arun4033622@gmail.com>
2021-03-10 23:47:33 +0530

    feat: Implement the Web Share API for the collaboration dialog

--- src/excalidraw-app/collab/RoomDialog.tsx
+++ src/excalidraw-app/collab/RoomDialog.tsx
@@ -1,7 +1,13 @@
 import React, { useRef } from "react";
 import { copyTextToSystemClipboard } from "../../clipboard";
 import { Dialog } from "../../components/Dialog";
-import { clipboard, start, stop } from "../../components/icons";
+import {
+  clipboard,
+  start,
+  stop,
+  share,
+  shareIOS,
+} from "../../components/icons";
 import { ToolButton } from "../../components/ToolButton";
 import { t } from "../../i18n";
 import "./RoomDialog.scss";
@@ -24,6 +30,8 @@ const RoomDialog = ({
   setErrorMessage: (message: string) => void;
 }) => {
   const roomLinkInput = useRef<HTMLInputElement>(null);
+  const navigator = window.navigator as any;
+  const isAppleBrowser = /Apple/.test(navigator.vendor);
 
   const copyRoomLink = async () => {
     try {
@@ -33,6 +41,18 @@ const RoomDialog = ({
     }
     if (roomLinkInput.current) {
       roomLinkInput.current.select();
+    }
+  };
+
+  const shareRoomLink = async () => {
+    try {
+      await navigator.share({
+        title: t("roomDialog.shareTitle"),
+        text: t("roomDialog.shareTitle"),
+        url: activeRoomLink,
+      });
+    } catch (error) {
+      setErrorMessage(error.message);
     }
   };
 
@@ -68,13 +88,23 @@ const RoomDialog = ({
             <p>{t("roomDialog.desc_inProgressIntro")}</p>
             <p>{t("roomDialog.desc_shareLink")}</p>
             <div className="RoomDialog-linkContainer">
-              <ToolButton
-                type="button"
-                icon={clipboard}
-                title={t("labels.copy")}
-                aria-label={t("labels.copy")}
-                onClick={copyRoomLink}
-              />
+              {navigator.share ? (
+                <ToolButton
+                  type="button"
+                  icon={isAppleBrowser ? shareIOS : share}
+                  title={t("labels.share")}
+                  aria-label={t("labels.share")}
+                  onClick={shareRoomLink}
+                />
+              ) : (
+                <ToolButton
+                  type="button"
+                  icon={clipboard}
+                  title={t("labels.copy")}
+                  aria-label={t("labels.copy")}
+                  onClick={copyRoomLink}
+                />
+              )}
               <input
                 value={activeRoomLink}
                 readOnly={true}
