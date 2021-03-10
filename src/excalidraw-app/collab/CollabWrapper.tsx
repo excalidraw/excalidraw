7e7ea7b1b7566bb802c84720c9f8c582c4cb5682
Lipis <lipiridis@gmail.com>
2021-03-10 17:45:37 +0200

    chore: Track collaboration (#3211)

--- src/excalidraw-app/collab/CollabWrapper.tsx
+++ src/excalidraw-app/collab/CollabWrapper.tsx
@@ -40,6 +40,7 @@ import { createInverseContext } from "../../createInverseContext";
 import { t } from "../../i18n";
 import { UserIdleState } from "./types";
 import { IDLE_THRESHOLD, ACTIVE_THRESHOLD } from "../../constants";
+import { trackEvent } from "../../analytics";
 
 interface CollabState {
   modalIsShown: boolean;
@@ -188,6 +189,7 @@ class CollabWrapper extends PureComponent<Props, CollabState> {
   };
 
   openPortal = async () => {
+    trackEvent("share", "room creation");
     return this.initializeSocketClient(null);
   };
 
@@ -196,6 +198,7 @@ class CollabWrapper extends PureComponent<Props, CollabState> {
     if (window.confirm(t("alerts.collabStopOverridePrompt"))) {
       window.history.pushState({}, APP_NAME, window.location.origin);
       this.destroySocketClient();
+      trackEvent("share", "room closed");
     }
   };
 
