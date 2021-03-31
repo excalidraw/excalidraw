b91911d39a76ede9082ab13133067ab6492a4dfe
Hampus Lavin <hampus.lavin@gmail.com>
2021-03-31 14:28:25 +0200

    test: Add unit tests for package/utils (#3357)
    * Update return type to reflect actual signature
    
    * add tests
    
    * Set getDimensions as optional
    
    * add newlines between specs
    
    * remove redundant assertion
    
    * move fixtures to separate files
    
    * Add spacing
    
    * Move tests, add cases
    
    * Add unit tests for package/utils exportToSvg
    
    * extract default object in test
    
    * Move test suite to new file

---
+++ src/tests/fixtures/elementFixture.ts
@@ -0,0 +1,37 @@
+import { ExcalidrawElement } from "../../element/types";
+
+const elementBase: Omit<ExcalidrawElement, "type"> = {
+  id: "vWrqOAfkind2qcm7LDAGZ",
+  x: 414,
+  y: 237,
+  width: 214,
+  height: 214,
+  angle: 0,
+  strokeColor: "#000000",
+  backgroundColor: "#15aabf",
+  fillStyle: "hachure",
+  strokeWidth: 1,
+  strokeStyle: "solid",
+  roughness: 1,
+  opacity: 100,
+  groupIds: [],
+  strokeSharpness: "sharp",
+  seed: 1041657908,
+  version: 120,
+  versionNonce: 1188004276,
+  isDeleted: false,
+  boundElementIds: null,
+};
+
+export const rectangleFixture: ExcalidrawElement = {
+  ...elementBase,
+  type: "rectangle",
+};
+export const ellipseFixture: ExcalidrawElement = {
+  ...elementBase,
+  type: "ellipse",
+};
+export const diamondFixture: ExcalidrawElement = {
+  ...elementBase,
+  type: "diamond",
+};
