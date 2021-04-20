32ac1e3010798f08d17d5da05a20526962cf352a
dwelle <luzar.david@gmail.com>
2021-04-20 23:13:22 +0200

    refactor: move getSyncableElements to CollabWrapper

--- src/packages/excalidraw/index.tsx
+++ src/packages/excalidraw/index.tsx
@@ -111,11 +111,7 @@ const forwardedRefComp = forwardRef<
   PublicExcalidrawProps
 >((props, ref) => <Excalidraw {...props} excalidrawRef={ref} />);
 export default React.memo(forwardedRefComp, areEqual);
-export {
-  getSceneVersion,
-  getSyncableElements,
-  getElementMap,
-} from "../../element";
+export { getSceneVersion, getElementMap } from "../../element";
 export { defaultLang, languages } from "../../i18n";
 export { restore, restoreAppState, restoreElements } from "../../data/restore";
 export {
