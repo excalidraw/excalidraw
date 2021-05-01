a9990e811f7f7ef3488fdac5efc2d282d918b291
Steve Ruiz <steveruizok@gmail.com>
2021-05-01 10:00:52 +0100

    Update renderElement.ts

--- src/renderer/renderElement.ts
+++ src/renderer/renderElement.ts
@@ -789,7 +789,7 @@ export function getFreeDrawSvgPath(element: ExcalidrawFreeDrawElement) {
 
   // Consider changing the options for simulated pressure vs real pressure
   const options = {
-    size: element.strokeWidth * 5,
+    size: element.strokeWidth * 6,
     thinning: 0.55,
     smoothing: 0.5,
     streamline: 0.4,
