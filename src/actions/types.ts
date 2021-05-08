78a803e2f3ef5d48ea02f99c0c8fbeb8419dc941
Arun Kumar <arun4033622@gmail.com>
2021-05-08 13:21:41 +0530

    feat: Add shortcut for dark mode
    * Create and move toggle into an action
    
    * Add shortcut on help dialog

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -98,7 +98,8 @@ export type ActionName =
   | "flipHorizontal"
   | "flipVertical"
   | "viewMode"
-  | "exportWithDarkMode";
+  | "exportWithDarkMode"
+  | "toggleTheme";
 
 export interface Action {
   name: ActionName;
