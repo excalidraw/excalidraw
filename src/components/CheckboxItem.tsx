34cce8d6dc2e0ff4b8a4d37d288de8c70b8b0868
dwelle <luzar.david@gmail.com>
2021-05-25 21:03:26 +0200

    add ARIA

--- src/components/CheckboxItem.tsx
+++ src/components/CheckboxItem.tsx
@@ -17,7 +17,9 @@ export const CheckboxItem: React.FC<{
         ) as HTMLButtonElement).focus();
       }}
     >
-      <button className="Checkbox-box">{checkIcon}</button>
+      <button className="Checkbox-box" role="checkbox" aria-checked={checked}>
+        {checkIcon}
+      </button>
       <div className="Checkbox-label">{children}</div>
     </div>
   );
