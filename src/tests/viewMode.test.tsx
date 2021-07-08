a08c94d91d9161c8afc514282338cb2db7e83934
connor-pph <connor.hanafee@gmail.com>
2021-07-08 00:29:27 +0000

    view-mode cursor fix + tests

--- src/tests/viewMode.test.tsx
+++ src/tests/viewMode.test.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {
-  render,
-  GlobalTestState
-} from "./test-utils";
+import { render, GlobalTestState } from "./test-utils";
 import ExcalidrawApp from "../excalidraw-app";
 import { KEYS } from "../keys";
 import { Keyboard, Pointer } from "./helpers/ui";
 import { CURSOR_TYPE } from "../constants";
 
 const { h } = window;
-const mouse = new Pointer("mouse");
+const pointerTypes = ["mouse", "touch", "pen"];
 
 describe("view mode", () => {
   beforeEach(async () => {
@@ -18,17 +14,23 @@ describe("view mode", () => {
   });
 
   it("after switching to view mode – cursor type should be pointer", async () => {
-    h.setState({viewModeEnabled: true});
-    expect(GlobalTestState.canvas.style._values.cursor).toBe(CURSOR_TYPE.POINTER);
+    h.setState({ viewModeEnabled: true });
+    expect(GlobalTestState.canvas.style._values.cursor).toBe(
+      CURSOR_TYPE.POINTER,
+    );
   });
 
   it("after switching to view mode, moving, clicking, and pressing space key – cursor type should be pointer", async () => {
-    h.setState({viewModeEnabled: true});
-    mouse.reset();
-    mouse.move(100, 100);
-    mouse.click();
-    Keyboard.keyPress(KEYS.SPACE);
-    expect(GlobalTestState.canvas.style._values.cursor).toBe(CURSOR_TYPE.POINTER);
+    h.setState({ viewModeEnabled: true });
+    pointerTypes.forEach((pointerType) => {
+      const pointer = new Pointer(pointerType);
+      pointer.reset();
+      pointer.move(100, 100);
+      pointer.click();
+      Keyboard.keyPress(KEYS.SPACE);
+      expect(GlobalTestState.canvas.style._values.cursor).toBe(
+        CURSOR_TYPE.POINTER,
+      );
+    });
   });
-
 });
