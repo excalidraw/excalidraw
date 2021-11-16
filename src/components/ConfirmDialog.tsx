80c7149caa0fcd1c8492e2f947bb1bec7e4a18ef
ad1992 <aakansha1216@gmail.com>
2021-11-16 17:16:41 +0530

    feat: create confirm dialog to use instead of window.confirm

---
+++ src/components/ConfirmDialog.tsx
@@ -0,0 +1,57 @@
+import { t } from "../i18n";
+import { Dialog } from "./Dialog";
+import { ToolButton } from "./ToolButton";
+
+import "./ConfirmDialog.scss";
+
+interface Props {
+  onConfirm: () => void;
+  onCancel: () => void;
+  title: string;
+  children: React.ReactNode;
+  confirmText?: string;
+  cancelText?: string;
+  className?: string;
+}
+const ConfirmDialog = (props: Props) => {
+  const {
+    onConfirm,
+    onCancel,
+    title,
+    children,
+    confirmText = t("buttons.confirm"),
+    cancelText = t("buttons.cancel"),
+    className = "",
+    ...rest
+  } = props;
+  return (
+    <Dialog
+      onCloseRequest={onCancel}
+      title={title}
+      small={true}
+      {...rest}
+      className={`confirm-dialog ${className}`}
+    >
+      {children}
+      <div className="confirm-dialog-buttons">
+        <ToolButton
+          type="button"
+          title={confirmText}
+          aria-label={confirmText}
+          label={confirmText}
+          onClick={onConfirm}
+          className="confirm-dialog--confirm"
+        />
+        <ToolButton
+          type="button"
+          title={cancelText}
+          aria-label={cancelText}
+          label={cancelText}
+          onClick={onCancel}
+          className="confirm-dialog--cancel"
+        />
+      </div>
+    </Dialog>
+  );
+};
+export default ConfirmDialog;
