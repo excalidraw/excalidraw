d85a5d46f77568b8b3f08d36a810d30696ac2874
dwelle <luzar.david@gmail.com>
2021-11-17 15:08:02 +0100

    show toast on adding to library via contextMenu

--- src/actions/actionAddToLibrary.ts
+++ src/actions/actionAddToLibrary.ts
@@ -3,24 +3,44 @@ import { getSelectedElements } from "../scene";
 import { getNonDeletedElements } from "../element";
 import { deepCopyElement } from "../element/newElement";
 import { randomId } from "../random";
+import { t } from "../i18n";
 
 export const actionAddToLibrary = register({
   name: "addToLibrary",
   perform: (elements, appState, _, app) => {
-    app.library.loadLibrary().then((items) => {
-      app.library.saveLibrary([
-        ...items,
-        {
-          id: randomId(),
-          status: "unpublished",
-          elements: getSelectedElements(
-            getNonDeletedElements(elements),
-            appState,
-          ).map(deepCopyElement),
-        },
-      ]);
-    });
-    return false;
+    return app.library
+      .loadLibrary()
+      .then((items) => {
+        return app.library.saveLibrary([
+          ...items,
+          {
+            id: randomId(),
+            status: "unpublished",
+            elements: getSelectedElements(
+              getNonDeletedElements(elements),
+              appState,
+            ).map(deepCopyElement),
+          },
+        ]);
+      })
+      .then(() => {
+        return {
+          commitToHistory: false,
+          appState: {
+            ...appState,
+            toastMessage: t("toast.addedToLibrary"),
+          },
+        };
+      })
+      .catch((error) => {
+        return {
+          commitToHistory: false,
+          appState: {
+            ...appState,
+            errorMessage: error.message,
+          },
+        };
+      });
   },
   contextItemLabel: "labels.addToLibrary",
 });
