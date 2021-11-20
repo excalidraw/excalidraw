10c2ab5812b064310f577549df1ebd49a2a19083
zsviczian <viczian.zsolt@gmail.com>
2021-11-20 14:54:40 +0100

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -248,6 +248,7 @@ export interface ExcalidrawProps {
   onLibraryChange?: (libraryItems: LibraryItems) => void | Promise<any>;
   autoFocus?: boolean;
   generateIdForFile?: (file: File) => string | Promise<string>;
+  onThemeChange?: (newTheme: string) => void;  
 }
 
 export type SceneData = {
