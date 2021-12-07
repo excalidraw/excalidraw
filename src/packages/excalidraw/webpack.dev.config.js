8ca147e0c6e4fc040c02b75339b9443676d0ff39
Martin Roed <msroed@gmail.com>
2021-12-07 13:39:21 +0100

    databutton color on activeelement in leftmenu

--- src/packages/excalidraw/webpack.dev.config.js
+++ src/packages/excalidraw/webpack.dev.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     library: "Excalidraw",
-    libraryTarget: "umd",
+    libraryTarget: "commonjs2",
     filename: "[name].js",
     chunkFilename: "excalidraw-assets-dev/[name]-[contenthash].js",
     publicPath: "",
