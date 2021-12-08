6111c07177c12ec799894e305cdd2fab24092307
ad1992 <aakansha1216@gmail.com>
2021-12-08 11:40:22 +0530

    build: use type:javascript/auto so font file assets aren't duplicated

--- src/packages/excalidraw/webpack.dev.config.js
+++ src/packages/excalidraw/webpack.dev.config.js
@@ -63,6 +63,7 @@ module.exports = {
             },
           },
         ],
+        type: "javascript/auto",
       },
     ],
   },
--- src/packages/excalidraw/webpack.prod.config.js
+++ src/packages/excalidraw/webpack.prod.config.js
@@ -81,6 +81,7 @@ module.exports = {
             },
           },
         ],
+        type: "javascript/auto",
       },
     ],
   },
