f7bb7a59996f376f0877897b229d8b2d4ac48ca5
Aakansha Doshi <aakansha1216@gmail.com>
2021-12-22 19:16:49 +0530

    fix: support dragging binded text in container selected in a group (#4462)
    * fix: support moving binded text when container selected via group
    
    * update coords of bounded text only when element doesn't belong to any group or element in group is selected
    
    * dnt drag binded text when nested group selected
    
    * Update src/element/dragElements.ts
    
    Co-authored-by: David Luzar <luzar.david@gmail.com>
    
    Co-authored-by: David Luzar <luzar.david@gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3574,6 +3574,7 @@ class App extends React.Component<AppProps, AppState> {
             lockDirection,
             dragDistanceX,
             dragDistanceY,
+            this.state,
           );
           this.maybeSuggestBindingForAll(selectedElements);
 
