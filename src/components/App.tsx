2681f0eb3bdb04c0898aa5a180aab739a2273c3b
David Luzar <luzar.david@gmail.com>
2021-12-27 14:18:11 +0100

    fix: prevent browser from scrolling when panning (#4489)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1765,6 +1765,7 @@ class App extends React.Component<AppProps, AppState> {
       if (event.key === KEYS.SPACE && gesture.pointers.size === 0) {
         isHoldingSpace = true;
         setCursor(this.canvas, CURSOR_TYPE.GRABBING);
+        event.preventDefault();
       }
 
       if (event.key === KEYS.G || event.key === KEYS.S) {
@@ -2731,6 +2732,7 @@ class App extends React.Component<AppProps, AppState> {
       return false;
     }
     isPanning = true;
+    event.preventDefault();
 
     let nextPastePrevented = false;
     const isLinux = /Linux/.test(window.navigator.platform);
