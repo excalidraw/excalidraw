ef04d04e6d0a1d5cc6548677e374f88b03b9188a
zsviczian <viczian.zsolt@gmail.com>
2022-01-16 09:42:15 +0100

    show fourth font on properties panel

--- src/types.ts
+++ src/types.ts
@@ -414,5 +414,5 @@ export type ExcalidrawImperativeAPI = {
   readyPromise: ResolvablePromise<ExcalidrawImperativeAPI>;
   ready: true;
   id: string;
-  setLocalFont: (url: string) => void;
+  setLocalFont: (url: string, showOnPanel: boolean) => void;
 };
