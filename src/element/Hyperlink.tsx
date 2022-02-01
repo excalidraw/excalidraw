f4520f670eed4e1bf18479b2ed87a606829e487c
dwelle <luzar.david@gmail.com>
2022-02-01 18:09:35 +0100

    touch hyperlink tooltip DOM only if needed

--- src/element/Hyperlink.tsx
+++ src/element/Hyperlink.tsx
@@ -39,6 +39,8 @@ EXTERNAL_LINK_IMG.src = `data:${MIME_TYPES.svg}, ${encodeURIComponent(
   `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#1971c2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>`,
 )}`;
 
+let IS_HYPERLINK_TOOLTIP_VISIBLE = false;
+
 export const Hyperlink = ({
   element,
   appState,
@@ -333,10 +335,14 @@ export const showHyperlinkTooltip = (
   tooltipDiv.classList.add("excalidraw-tooltip--visible");
   tooltipDiv.style.maxWidth = "20rem";
   tooltipDiv.innerHTML = element.link!;
+  IS_HYPERLINK_TOOLTIP_VISIBLE = true;
 };
 
 export const hideHyperlinkToolip = () => {
-  getTooltipDiv().classList.remove("excalidraw-tooltip--visible");
+  if (IS_HYPERLINK_TOOLTIP_VISIBLE) {
+    IS_HYPERLINK_TOOLTIP_VISIBLE = false;
+    getTooltipDiv().classList.remove("excalidraw-tooltip--visible");
+  }
 };
 
 export const shouldHideLinkPopup = (
