36909fa76ade5bfd1003028fe1fb5728b46b173e
Joe McKenney <joe@switchboard.cc>
2022-03-10 12:07:03 -0800

    wire collab button growth experience

--- src/components/CollabButton.tsx
+++ src/components/CollabButton.tsx
@@ -3,8 +3,46 @@ import { ToolButton } from "./ToolButton";
 import { t } from "../i18n";
 import { useIsMobile } from "../components/App";
 import { users } from "./icons";
+import { useSbState } from "@switchboardcc/sdk";
 
 import "./CollabButton.scss";
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+} from "@chakra-ui/react";
+
+const CollabButtonWrapper = (props: any) => {
+  const [state, setState] = useSbState("share-78e5c60");
+  if (!state || !state.active || state.finished) {
+    return props.children;
+  }
+  return (
+    <Popover isOpen={!state.finished}>
+      <PopoverTrigger>
+        <div>{props.children}</div>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton
+          onClick={() => {
+            if (!state.finished) {
+              setState({ ...state, finished: true });
+            }
+          }}
+        />
+        <PopoverBody>
+          Looking good! Let’s invite a coworker to help us make this drawing
+          even better. Click the Live collaboration button.
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+};
 
 const CollabButton = ({
   isCollaborating,
@@ -16,7 +54,7 @@ const CollabButton = ({
   onClick: () => void;
 }) => {
   return (
-    <>
+    <CollabButtonWrapper>
       <ToolButton
         className={clsx("CollabButton", {
           "is-collaborating": isCollaborating,
@@ -32,7 +70,7 @@ const CollabButton = ({
           <div className="CollabButton-collaborators">{collaboratorCount}</div>
         )}
       </ToolButton>
-    </>
+    </CollabButtonWrapper>
   );
 };
 
