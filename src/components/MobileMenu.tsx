e3a78fe5dfc4d32a09fc5564e3fa28574fdcfd18
zsviczian <viczian.zsolt@gmail.com>
2022-03-11 11:49:18 +0100

    Update MobileMenu.tsx

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -207,7 +207,8 @@ export const MobileMenu = ({
                             .map(([clientId, client]) => (
                               <React.Fragment key={clientId}>
                                 {actionManager.renderAction(
-                                  "goToCollaborator", {
+                                  "goToCollaborator",
+                                  {
                                     id: clientId,
                                   }
                                 )}
