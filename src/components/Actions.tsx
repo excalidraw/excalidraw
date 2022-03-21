8d60f22ff74814e5223951c7ba931627a2cbca51
zsviczian <viczian.zsolt@gmail.com>
2022-03-21 15:20:13 +0100

    Update Actions.tsx

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -196,7 +196,7 @@ export const ShapesSwitcher = ({
   setDeviceType: (obj: Partial<DeviceType>) => void;
 }) => {
   const penDetected = useDeviceType().penDetected;
-  app.setState({ toastMessage: penDetected ? "penOn" : "penOff" }); //debug iOS
+  setAppState({ toastMessage: penDetected ? "penOn" : "penOff" }); //debug iOS
   return (
     <>
       {SHAPES.map(({ value, icon, key }, index) => {
