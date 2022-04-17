4d13dbf625c9b008dd37421b627ec03ce8052f44
David Luzar <luzar.david@gmail.com>
2022-04-17 22:40:39 +0200

    feat: reconcile when saving to firebase (#4991)
    * naming tweaks
    
    * do not mark local element as duplicate when there's no remote counterpart
    
    * merge instead of overwrite elements when saving to firebase & reconcile local state
    
    * decouple syncing from persistence
    
    * fix ts
    
    * clarify doc
    
    * fix reconciliation not removing duplicates

--- src/excalidraw-app/app_constants.ts
+++ src/excalidraw-app/app_constants.ts
@@ -11,12 +11,12 @@ export const FILE_UPLOAD_MAX_BYTES = 3 * 1024 * 1024; // 3 MiB
 // 1 year (https://stackoverflow.com/a/25201898/927631)
 export const FILE_CACHE_MAX_AGE_SEC = 31536000;
 
-export const BROADCAST = {
+export const WS_EVENTS = {
   SERVER_VOLATILE: "server-volatile-broadcast",
   SERVER: "server-broadcast",
 };
 
-export enum SCENE {
+export enum WS_SCENE_EVENT_TYPES {
   INIT = "SCENE_INIT",
   UPDATE = "SCENE_UPDATE",
 }
