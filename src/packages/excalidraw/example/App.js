6fedc944ec25bdac4368b068912dfed290a68da1
Aakansha Doshi <aakansha1216@gmail.com>
2022-05-10 15:02:57 +0530

    make sure comment input never overflows the viewport

--- src/packages/excalidraw/example/App.js
+++ src/packages/excalidraw/example/App.js
@@ -32,6 +32,9 @@ const COMMENT_SVG = (
     <path d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z" />
   </svg>
 );
+const COMMENT_ICON_DIMENSION = 32;
+const COMMENT_INPUT_HEIGHT = 50;
+const COMMENT_INPUT_WIDTH = 150;
 
 const resolvablePromise = () => {
   let resolve;
@@ -203,8 +206,12 @@ export default function App() {
         { sceneX: commentIcons[id].x, sceneY: commentIcons[id].y },
         appstate,
       );
-      ele.style.left = `${x - 16 - appstate.offsetLeft}px`;
-      ele.style.top = `${y - 16 - appstate.offsetTop}px`;
+      ele.style.left = `${
+        x - COMMENT_ICON_DIMENSION / 2 - appstate.offsetLeft
+      }px`;
+      ele.style.top = `${
+        y - COMMENT_ICON_DIMENSION / 2 - appstate.offsetTop
+      }px`;
     });
   };
 
@@ -269,12 +276,12 @@ export default function App() {
           id={commentIcon.id}
           key={commentIcon.id}
           style={{
-            top: `${y - 16 - appState.offsetTop}px`,
-            left: `${x - 16 - appState.offsetLeft}px`,
+            top: `${y - COMMENT_ICON_DIMENSION / 2 - appState.offsetTop}px`,
+            left: `${x - COMMENT_ICON_DIMENSION / 2 - appState.offsetLeft}px`,
             position: "absolute",
             zIndex: 1,
-            width: "32px",
-            height: "32px",
+            width: `${COMMENT_ICON_DIMENSION}px`,
+            height: `${COMMENT_ICON_DIMENSION}px`,
           }}
           className="comment-icon"
           onPointerDown={(event) => {
@@ -333,15 +340,37 @@ export default function App() {
       { sceneX: comment.x, sceneY: comment.y },
       appState,
     );
+    let top = y - COMMENT_ICON_DIMENSION / 2 - appState.offsetTop;
+    let left = x - COMMENT_ICON_DIMENSION / 2 - appState.offsetLeft;
 
+    if (
+      top + COMMENT_INPUT_HEIGHT <
+      appState.offsetTop + COMMENT_INPUT_HEIGHT
+    ) {
+      top = COMMENT_ICON_DIMENSION / 2;
+    }
+    if (top + COMMENT_INPUT_HEIGHT > appState.height) {
+      top = appState.height - COMMENT_INPUT_HEIGHT - COMMENT_ICON_DIMENSION / 2;
+    }
+    if (
+      left + COMMENT_INPUT_WIDTH <
+      appState.offsetLeft + COMMENT_INPUT_WIDTH
+    ) {
+      left = COMMENT_ICON_DIMENSION / 2;
+    }
+    if (left + COMMENT_INPUT_WIDTH > appState.width) {
+      left = appState.width - COMMENT_INPUT_WIDTH - COMMENT_ICON_DIMENSION / 2;
+    }
     return (
       <textarea
         className="comment"
         style={{
-          top: `${y - 16 - appState.offsetTop}px`,
-          left: `${x - 16 - appState.offsetLeft}px`,
+          top: `${top}px`,
+          left: `${left}px`,
           position: "absolute",
           zIndex: 1,
+          height: `${COMMENT_INPUT_HEIGHT}px`,
+          width: `${COMMENT_INPUT_WIDTH}px`,
         }}
         ref={(ref) => {
           setTimeout(() => ref?.focus());
