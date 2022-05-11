4d4c9c94a95ecbe9f6ea9f1d2fb1a8e090d1cb99
Maielo <maielo.mv@gmail.com>
2022-05-11 23:19:30 +0200

    [feat] added global current used cookies in excal plus across domains

--- src/constants.ts
+++ src/constants.ts
@@ -193,3 +193,8 @@ export const VERTICAL_ALIGN = {
 };
 
 export const ELEMENT_READY_TO_ERASE_OPACITY = 20;
+
+export const COOKIES = {
+  AUTH_STATE_COOKIE: "excplus-auth",
+  AUTOREDIRECT_FROM_OSS_EDITOR: "excplus-auto-redirect-from-oss-editor",
+} as const;
