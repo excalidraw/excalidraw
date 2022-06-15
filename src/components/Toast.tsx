ba16516a6bc48eea52ce98a1c1aae40ed3fc6be1
Aakansha Doshi <aakansha1216@gmail.com>
2022-06-15 15:17:24 +0530

    feat: make toast closable and allow custom duration

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,34 +1,58 @@
 import { useCallback, useEffect, useRef } from "react";
 import { TOAST_TIMEOUT } from "../constants";
+import { close } from "./icons";
 import "./Toast.scss";
+import { ToolButton } from "./ToolButton";
 
 export const Toast = ({
   message,
   clearToast,
+  closable = true,
+  // pass null to prevent auto dismiss
+  duration,
 }: {
   message: string;
   clearToast: () => void;
+  closable?: boolean;
+  duration?: number | null;
 }) => {
   const timerRef = useRef<number>(0);
-
-  const scheduleTimeout = useCallback(
-    () =>
-      (timerRef.current = window.setTimeout(() => clearToast(), TOAST_TIMEOUT)),
-    [clearToast],
-  );
+  const toastDuration = duration === undefined ? TOAST_TIMEOUT : duration;
+  const scheduleTimeout = useCallback(() => {
+    if (!toastDuration) {
+      return;
+    }
+    timerRef.current = window.setTimeout(() => clearToast(), toastDuration);
+  }, [clearToast, toastDuration]);
 
   useEffect(() => {
+    if (!toastDuration) {
+      return;
+    }
     scheduleTimeout();
     return () => clearTimeout(timerRef.current);
-  }, [scheduleTimeout, message]);
+  }, [scheduleTimeout, message, toastDuration]);
 
+  const onMouseEnter = toastDuration
+    ? () => clearTimeout(timerRef?.current)
+    : undefined;
+  const onMouseLeave = toastDuration ? scheduleTimeout : undefined;
   return (
     <div
       className="Toast"
-      onMouseEnter={() => clearTimeout(timerRef?.current)}
-      onMouseLeave={scheduleTimeout}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       <p className="Toast__message">{message}</p>
+      {closable && (
+        <ToolButton
+          icon={close}
+          aria-label="close"
+          type="icon"
+          onClick={clearToast}
+          className="close"
+        />
+      )}
     </div>
   );
 };
