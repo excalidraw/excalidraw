8984d8f19a278184c829a8c42f8f6e2e2c4f93c5
pomdtr <achille.lacoin@gmail.com>
2022-06-29 10:23:48 +0000

    don't clean export options on load

--- src/data/blob.ts
+++ src/data/blob.ts
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { cleanAppStateForTextExport } from "../appState";
+import { cleanAppStateForImageExport } from "../appState";
 import { ALLOWED_IMAGE_MIME_TYPES, MIME_TYPES } from "../constants";
 import { clearElementsForExport } from "../element";
 import { ExcalidrawElement, FileId } from "../element/types";
@@ -143,7 +143,7 @@ export const loadSceneOrLibraryFromBlob = async (
             appState: {
               theme: localAppState?.theme,
               fileHandle: fileHandle || blob.handle || null,
-              ...cleanAppStateForTextExport(data.appState || {}),
+              ...cleanAppStateForImageExport(data.appState || {}),
               ...(localAppState
                 ? calculateScrollCenter(
                     data.elements || [],
