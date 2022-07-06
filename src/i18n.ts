5cbd8068e96ac7d5c466f4031271cce8b3662b7d
Aakansha Doshi <aakansha1216@gmail.com>
2022-07-06 15:21:05 +0530

    build: extract all i18n files into locales folder (#5419)

--- src/i18n.ts
+++ src/i18n.ts
@@ -86,7 +86,7 @@ export const setLanguage = async (lang: Language) => {
     currentLangData = {};
   } else {
     currentLangData = await import(
-      /* webpackChunkName: "i18n-[request]" */ `./locales/${currentLang.code}.json`
+      /* webpackChunkName: "locales/[request]" */ `./locales/${currentLang.code}.json`
     );
   }
 };
