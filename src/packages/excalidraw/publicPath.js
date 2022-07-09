ea88c818973ddad4981f1fc3d24e10ac5dbe8778
Daniel J. Geiger <1852529+DanielJGeiger@users.noreply.github.com>
2022-07-09 13:57:20 -0500

    Don't override a preset value of window.EXCALIDRAW_ASSET_PATH in the `@excalidraw/excalidraw` example app.

--- src/packages/excalidraw/publicPath.js
+++ src/packages/excalidraw/publicPath.js
@@ -2,7 +2,10 @@ import { ENV } from "../../constants";
 if (process.env.NODE_ENV !== ENV.TEST) {
   /* eslint-disable */
   /* global __webpack_public_path__:writable */
-  if (process.env.NODE_ENV === ENV.DEVELOPMENT) {
+  if (process.env.NODE_ENV === ENV.DEVELOPMENT && (
+    window.EXCALIDRAW_ASSET_PATH === undefined ||
+    window.EXCALIDRAW_ASSET_PATH === ""
+  )) {
     window.EXCALIDRAW_ASSET_PATH = "/";
   }
   __webpack_public_path__ =
