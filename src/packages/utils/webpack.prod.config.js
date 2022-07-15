e6898959bcd4fea2a2e21bb74f0e6975f699fb0b
Daniel J. Geiger <1852529+DanielJGeiger@users.noreply.github.com>
2022-07-14 22:34:38 -0500

    chore: Dedupe package dependencies and webpack configs.

--- src/packages/utils/webpack.prod.config.js
+++ src/packages/utils/webpack.prod.config.js
@@ -2,54 +2,17 @@ const webpack = require("webpack");
 const path = require("path");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { merge } = require("webpack-merge");
+const configCommon = require("../common.webpack.prod.config");
 
-module.exports = {
-  mode: "production",
+const config = {
   entry: { "excalidraw-utils.min": "./index.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
     library: "ExcalidrawUtils",
-    libraryTarget: "umd",
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".css", ".scss"],
   },
   optimization: {
     runtimeChunk: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /node_modules/,
-        use: ["style-loader", { loader: "css-loader" }, "sass-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js)$/,
-        use: [
-          {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true,
-              configFile: path.resolve(__dirname, "../tsconfig.prod.json"),
-            },
-          },
-          {
-            loader: "babel-loader",
-
-            options: {
-              presets: [
-                "@babel/preset-env",
-                ["@babel/preset-react", { runtime: "automatic" }],
-                "@babel/preset-typescript",
-              ],
-              plugins: [["@babel/plugin-transform-runtime"]],
-            },
-          },
-        ],
-      },
-    ],
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
@@ -58,3 +21,4 @@ module.exports = {
     ...(process.env.ANALYZER === "true" ? [new BundleAnalyzerPlugin()] : []),
   ],
 };
+module.exports = merge(configCommon, config);
