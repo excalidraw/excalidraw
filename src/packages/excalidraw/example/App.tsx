73c40f2fcbb765eddef9795a1acd9018eb09fe1e
Aakansha Doshi <aakansha1216@gmail.com>
2022-07-19 17:50:14 +0530

    fix the attributes in comment svg example

--- src/packages/excalidraw/example/App.tsx
+++ src/packages/excalidraw/example/App.tsx
@@ -75,9 +75,9 @@ const COMMENT_SVG = (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="feather feather-message-circle"
   >
     <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
