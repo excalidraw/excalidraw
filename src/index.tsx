f105c0794a9cfdd8032a77c9fd75d77ae892b517
Aakansha Doshi <aakansha1216@gmail.com>
2022-07-22 11:20:36 +0530

    chore: upgrade to React 18 (#5450)
    * chore: upgrade to React 18
    
    * better type
    
    * use React.FC to fix type
    
    Co-authored-by: dwelle <luzar.david@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import ExcalidrawApp from "./excalidraw-app";
 
 import "./excalidraw-app/pwa";
 import "./excalidraw-app/sentry";
 window.__EXCALIDRAW_SHA__ = process.env.REACT_APP_GIT_SHA;
-
-ReactDOM.render(<ExcalidrawApp />, document.getElementById("root"));
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <ExcalidrawApp />
+  </StrictMode>,
+);
