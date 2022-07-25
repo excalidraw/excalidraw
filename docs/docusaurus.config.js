8b317cbbf1871d309846e25631b26ed57202a520
Aakansha Doshi <aakansha1216@gmail.com>
2022-07-25 15:48:16 +0530

    get started docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -77,8 +77,8 @@ const config = {
             title: "Docs",
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: "Get Started",
+                to: "/docs/get-started",
               },
             ],
           },
