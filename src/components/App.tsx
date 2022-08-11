8e9d04ae18858d96e9f3c491b35059f4bc047612
Aakansha Doshi <aakansha1216@gmail.com>
2022-08-11 14:40:59 +0530

    only add polyfill for test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6214,27 +6214,29 @@ if (
     },
   });
 
-  // Taken from https://github.com/tc39/proposal-relative-indexing-method#polyfill so that it works in tests
-  /* eslint-disable */
-  Object.defineProperty(Array.prototype, "at", {
-    value: function (n: number) {
-      // ToInteger() abstract op
-      n = Math.trunc(n) || 0;
-      // Allow negative indexing from the end
-      if (n < 0) {
-        n += this.length;
-      }
-      // OOB access is guaranteed to return undefined
-      if (n < 0 || n >= this.length) {
-        return undefined;
-      }
-      // Otherwise, this is just normal property access
-      return this[n];
-    },
-    writable: true,
-    enumerable: false,
-    configurable: true,
-  });
+  if (process.env.NODE_ENV === ENV.TEST) {
+    // Taken from https://github.com/tc39/proposal-relative-indexing-method#polyfill so that it works in tests
+    /* eslint-disable */
+    Object.defineProperty(Array.prototype, "at", {
+      value: function (n: number) {
+        // ToInteger() abstract op
+        n = Math.trunc(n) || 0;
+        // Allow negative indexing from the end
+        if (n < 0) {
+          n += this.length;
+        }
+        // OOB access is guaranteed to return undefined
+        if (n < 0 || n >= this.length) {
+          return undefined;
+        }
+        // Otherwise, this is just normal property access
+        return this[n];
+      },
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    });
+  }
 }
 
 export default App;
