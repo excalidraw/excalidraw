03f0719b8df46e6f18f93ade03a77ff27ac91109
Daniel J. Geiger <1852529+DanielJGeiger@users.noreply.github.com>
2022-08-25 18:44:11 -0500

    Subtypes: Add more tests.

--- src/tests/subtypes.test.tsx
+++ src/tests/subtypes.test.tsx
@@ -3,6 +3,7 @@ import {
   CustomMethods,
   SubtypePrepFn,
   SubtypeTypes,
+  isValidSubtype,
   prepareSubtype,
   subtypeCollides,
 } from "../subtypes";
@@ -15,6 +16,18 @@ import { Theme } from "../element/types";
 import { createIcon, iconFillColor } from "../components/icons";
 import { SubtypeButton } from "../components/SubtypeButton";
 import { registerAuxLangData } from "../i18n";
+
+const getLangData = async (langCode: string): Promise<Object | undefined> => {
+  try {
+    const condData = await import(
+      /* webpackChunkName: "locales/[request]" */ `./helpers/locales/${langCode}.json`
+    );
+    if (condData) {
+      return condData;
+    }
+  } catch (e) {}
+  return undefined;
+};
 
 const testSubtypeIcon = ({ theme }: { theme: Theme }) =>
   createIcon(
@@ -51,6 +64,47 @@ const testSubtypeTypes: SubtypeTypes = {
   customShortcutMap: {},
 };
 
+const testSubtypeNonParent = "text" as const;
+
+const test2Subtype = "test2" as const;
+const test2Props = [{} as {}] as const;
+
+const test2SubtypeTypes: SubtypeTypes = {
+  subtype: test2Subtype,
+  parents: [{ subtype: test2Subtype, parentType: "text" }],
+  customData: test2Props,
+  customActions: [
+    {
+      subtype: test2Subtype,
+      actions: [test2Subtype],
+    },
+  ],
+  disabledActions: [],
+  customShortcutNames: [],
+  customShortcutMap: {},
+};
+
+const test3Subtype = "test3" as const;
+const test3Props = [{} as {}] as const;
+
+const test3SubtypeTypes: SubtypeTypes = {
+  subtype: test3Subtype,
+  parents: [
+    { subtype: test3Subtype, parentType: "text" },
+    { subtype: test3Subtype, parentType: "line" },
+  ],
+  customData: test3Props,
+  customActions: [
+    {
+      subtype: test3Subtype,
+      actions: [test3Subtype],
+    },
+  ],
+  disabledActions: [],
+  customShortcutNames: [],
+  customShortcutMap: {},
+};
+
 const cleanTestElementUpdate = function (updates) {
   const oldUpdates = {};
   for (const key in updates) {
@@ -69,23 +123,43 @@ const prepareTestSubtype = function (
 ) {
   const methods = {} as CustomMethods;
   methods.clean = cleanTestElementUpdate;
-  const getLangData = async (langCode: string): Promise<Object | undefined> => {
-    try {
-      const condData = await import(
-        /* webpackChunkName: "locales/[request]" */ `./helpers/locales/${langCode}.json`
-      );
-      if (condData) {
-        return condData;
-      }
-    } catch (e) {}
-    return undefined;
-  };
+
   addLangData(fallbackLangData, getLangData);
   registerAuxLangData(fallbackLangData, getLangData);
 
-  const testAction = SubtypeButton("test", "line", testSubtypeIcon);
+  const actions = [SubtypeButton("test", "line", testSubtypeIcon)];
+  actions.forEach((action) => addCustomAction(action));
 
-  const actions = [testAction];
+  return { actions, methods };
+} as SubtypePrepFn;
+
+const prepareTest2Subtype = function (
+  addCustomAction,
+  addLangData,
+  onSubtypeLoaded,
+) {
+  const methods = {} as CustomMethods;
+
+  addLangData(fallbackLangData, getLangData);
+  registerAuxLangData(fallbackLangData, getLangData);
+
+  const actions = [SubtypeButton("test2", "text", testSubtypeIcon)];
+  actions.forEach((action) => addCustomAction(action));
+
+  return { actions, methods };
+} as SubtypePrepFn;
+
+const prepareTest3Subtype = function (
+  addCustomAction,
+  addLangData,
+  onSubtypeLoaded,
+) {
+  const methods = {} as CustomMethods;
+
+  addLangData(fallbackLangData, getLangData);
+  registerAuxLangData(fallbackLangData, getLangData);
+
+  const actions = [SubtypeButton("test3", "text", testSubtypeIcon)];
   actions.forEach((action) => addCustomAction(action));
 
   return { actions, methods };
@@ -94,8 +168,31 @@ const prepareTestSubtype = function (
 const { h } = window;
 
 prepareSubtype(testSubtypeTypes, prepareTestSubtype);
+prepareSubtype(test2SubtypeTypes, prepareTest2Subtype);
+prepareSubtype(test3SubtypeTypes, prepareTest3Subtype);
 
 describe("subtypes", () => {
+  it("should correctly validate", async () => {
+    testSubtypeTypes.parents.forEach((p) => {
+      expect(isValidSubtype(p.subtype, p.parentType)).toBe(true);
+      expect(isValidSubtype(undefined, p.parentType)).toBe(false);
+    });
+    expect(isValidSubtype(testSubtype, testSubtypeNonParent)).toBe(false);
+    expect(isValidSubtype(testSubtype, undefined)).toBe(false);
+    expect(isValidSubtype(undefined, undefined)).toBe(false);
+  });
+  it("should collide with themselves", async () => {
+    expect(subtypeCollides(testSubtype, [testSubtype])).toBe(true);
+    expect(subtypeCollides(testSubtype, [testSubtype, test2Subtype])).toBe(
+      true,
+    );
+  });
+  it("should not collide without type overlap", async () => {
+    expect(subtypeCollides(testSubtype, [test2Subtype])).toBe(false);
+  });
+  it("should collide with type overlap", async () => {
+    expect(subtypeCollides(testSubtype, [test3Subtype])).toBe(true);
+  });
   it("should apply to ExcalidrawElements", async () => {
     await render(<ExcalidrawApp />, {
       localStorageData: {
@@ -135,8 +232,5 @@ describe("subtypes", () => {
         expect(el.roughness).toBe(1);
       }
     });
-  });
-  it("should collide with themselves", async () => {
-    expect(subtypeCollides(testSubtype, [testSubtype])).toBe(true);
   });
 });
