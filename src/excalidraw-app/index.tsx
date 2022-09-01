188116ac569fa17eedfec02aba9f21f187152821
Abdullah Adeel <64294045+mabdullahadeel@users.noreply.github.com>
2022-09-01 15:11:44 +0500

    fix: #5622 - prevent session theme reset during collaboration (#5640)
    * ✅ fixed #5622 - prevent session theme reset
    
     - ❌ prevent newly initialized state to override theme preferences.
     - 🔧 fix for #5622
    
    * refactor
    
    Co-authored-by: Aakansha Doshi <aakansha1216@gmail.com>

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -194,7 +194,13 @@ const initializeScene = async (opts: {
       scene: {
         ...scene,
         appState: {
-          ...restoreAppState(scene?.appState, excalidrawAPI.getAppState()),
+          ...restoreAppState(
+            {
+              ...scene?.appState,
+              theme: localDataState?.appState?.theme || scene?.appState?.theme,
+            },
+            excalidrawAPI.getAppState(),
+          ),
           // necessary if we're invoking from a hashchange handler which doesn't
           // go through App.initializeScene() that resets this flag
           isLoading: false,
