b3ddb3877950377a489a7f4bbb8655aa2c8430af
dwelle <luzar.david@gmail.com>
2022-09-13 20:50:53 +0200

    Revert "chore: Dedupe webpack configs. (#5449)"
    This reverts commit da4fa91ffc2bf9b1b74c9fec8164348273a9cad2.

--- src/packages/utils/webpack.prod.config.js
+++ src/packages/utils/webpack.prod.config.js
@@ -1,23 +1,60 @@
-global.__childdir = __dirname;
-global.__noenv = true;
 const webpack = require("webpack");
 const path = require("path");
-const { merge } = require("webpack-merge");
-const commonConfig = require("../common.webpack.prod.config");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-const config = {
+module.exports = {
+  mode: "production",
   entry: { "excalidraw-utils.min": "./index.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
     library: "ExcalidrawUtils",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".css", ".scss"],
   },
   optimization: {
     runtimeChunk: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", { loader: "css-loader" }, "sass-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js)$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              configFile: path.resolve(__dirname, "../tsconfig.prod.json"),
+            },
+          },
+          {
+            loader: "babel-loader",
+
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+                "@babel/preset-typescript",
+              ],
+              plugins: [["@babel/plugin-transform-runtime"]],
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1,
     }),
+    ...(process.env.ANALYZER === "true" ? [new BundleAnalyzerPlugin()] : []),
   ],
 };
-module.exports = merge(commonConfig, config);
