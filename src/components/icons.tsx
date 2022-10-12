3723e4a1cd9c2fed3eb524b6d0d15b54005217fd
barnabasmolnar <barnabas@excalidraw.com>
2022-10-13 01:38:56 +0200

    Changed some icons.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -54,124 +54,179 @@ export const createIcon = (
 };
 
 const newIconProps: Opts = {
-  width: 24,
-  height: 24,
-  strokeWidth: 1.2,
-  strokeLinecap: "round",
-  strokeLinejoin: "round",
+  width: 16,
+  height: 16,
   fill: "none",
-  stroke: "currentColor",
 };
 
 export const SelectionIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="3" y1="12" x2="6" y2="12" />
-    <line x1="12" y1="3" x2="12" y2="6" />
-    <line x1="7.8" y1="7.8" x2="5.6" y2="5.6" />
-    <line x1="16.2" y1="7.8" x2="18.4" y2="5.6" />
-    <line x1="7.8" y1="16.2" x2="5.6" y2="18.4" />
-    <path d="M12 12l9 3l-4 2l-2 4l-3 -9" />
+    <path
+      d="M2 8h2M8 2v2M5.2 5.2 3.733 3.733M10.8 5.2l1.467-1.467M5.2 10.8l-1.467 1.467M8 8l6 2-2.667 1.333L10 14 8 8Z"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const RectangleIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <rect x="4" y="4" width="16" height="16" rx="2" />,
+    <path
+      d="M12 2.667H4c-.736 0-1.333.597-1.333 1.333v8c0 .736.597 1.333 1.333 1.333h8c.736 0 1.333-.597 1.333-1.333V4c0-.736-.597-1.333-1.333-1.333Z"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const DiamondIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M10.5 20.4l-6.9 -6.9c-.781 -.781 -.781 -2.219 0 -3l6.9 -6.9c.781 -.781 2.219 -.781 3 0l6.9 6.9c.781 .781 .781 2.219 0 3l-6.9 6.9c-.781 .781 -2.219 .781 -3 0z" />
+    <path
+      d="M7 13.6 2.4 9c-.52-.52-.52-1.48 0-2L7 2.4c.52-.52 1.48-.52 2 0L13.6 7c.52.52.52 1.48 0 2L9 13.6c-.52.52-1.48.52-2 0v0Z"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const EllipseIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <circle cx="12" cy="12" r="9" />
+    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12Z"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   </>,
   newIconProps,
 );
 
 export const ArrowIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="5" y1="12" x2="19" y2="12" />
-    <line x1="15" y1="16" x2="19" y2="12" />
-    <line x1="15" y1="8" x2="19" y2="12" />
+    <path
+      d="M3.333 8h9.334M10 10.667 12.667 8M10 5.333 12.667 8"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const LineIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="5" y1="12" x2="19" y2="12" />
+    <path
+      d="M3.333 8h9.334"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const FreedrawIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
-    <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
+    <path
+      d="M2.667 13.333h2.666l7-7a1.886 1.886 0 0 0-2.666-2.666l-7 7v2.666ZM9 4.333 11.667 7"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const TextIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="4" y1="20" x2="7" y2="20" />
-    <line x1="14" y1="20" x2="21" y2="20" />
-    <line x1="6.9" y1="15" x2="13.8" y2="15" />
-    <line x1="10.2" y1="6.3" x2="16" y2="20" />
-    <polyline points="5 20 11 4 13 4 20 20" />
+    <path
+      d="M2.667 13.333h2M9.333 13.333H14M4.6 10h4.6M6.8 4.2l3.867 9.133M3.333 13.333l4-10.666h1.334l4.666 10.666"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const ImageIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="15" y1="8" x2="15.01" y2="8" />
-    <rect x="4" y="4" width="16" height="16" rx="3" />
-    <path d="M4 15l4 -4a3 5 0 0 1 3 0l5 5" />
-    <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2" />
+    <path
+      d="M10 5.333h.007M11.333 2.667H4.667a2 2 0 0 0-2 2v6.666a2 2 0 0 0 2 2h6.666a2 2 0 0 0 2-2V4.667a2 2 0 0 0-2-2Z"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="m2.667 10 2.666-2.667c.304-.292.65-.446 1-.446.351 0 .696.154 1 .446l3.334 3.334"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M9.333 9.333 10 8.667c.304-.293.649-.447 1-.447s.696.154 1 .447L13.333 10"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const EraserIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M19 19h-11l-4 -4a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9 9" />
-    <line x1="18" y1="12.3" x2="11.7" y2="6" />
+    <path
+      d="M12.667 12.667H5.333L2.667 10a.667.667 0 0 1 0-.94l6.666-6.667a.667.667 0 0 1 .94 0l3.334 3.334a.667.667 0 0 1 0 .94l-6 6M12 8.2 7.8 4"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const ZoomInIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="12" y1="5" x2="12" y2="19" />
-    <line x1="5" y1="12" x2="19" y2="12" />
+    <path
+      d="M8 3.333v9.334M3.333 8h9.334"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
 
 export const ZoomOutIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="5" y1="12" x2="19" y2="12" />
+    <path
+      d="M3.333 8h9.334"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
@@ -186,7 +241,7 @@ export const TrashIcon = createIcon(
       strokeLinejoin="round"
     />
   </>,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const DuplicateIcon = createIcon(
@@ -206,7 +261,7 @@ export const DuplicateIcon = createIcon(
       strokeLinejoin="round"
     />
   </>,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const MoonIcon = createIcon(
@@ -217,7 +272,7 @@ export const MoonIcon = createIcon(
     strokeLinecap="round"
     strokeLinejoin="round"
   />,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const SunIcon = createIcon(
@@ -228,7 +283,7 @@ export const SunIcon = createIcon(
     strokeLinecap="round"
     strokeLinejoin="round"
   />,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const HamburgerMenuIcon = createIcon(
@@ -239,15 +294,25 @@ export const HamburgerMenuIcon = createIcon(
     strokeLinecap="round"
     strokeLinejoin="round"
   />,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const ExportIcon = createIcon(
   <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-    <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3" />
-    ,
+    <path
+      d="M9.333 5.333V4A1.333 1.333 0 0 0 8 2.667H3.333A1.333 1.333 0 0 0 2 4v8a1.333 1.333 0 0 0 1.333 1.333H8A1.333 1.333 0 0 0 9.333 12v-1.333"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M4.667 8H14l-2-2m0 4 2-2"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </>,
   newIconProps,
 );
@@ -269,7 +334,7 @@ export const HelpIcon = createIcon(
       strokeLinejoin="round"
     />
   </>,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const LinkIcon = createIcon(
@@ -280,7 +345,7 @@ export const LinkIcon = createIcon(
     strokeLinecap="round"
     strokeLinejoin="round"
   />,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const checkIcon = createIcon(
@@ -313,7 +378,7 @@ export const load = createIcon(
     strokeLinecap="round"
     strokeLinejoin="round"
   />,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const clipboard = createIcon(
@@ -809,7 +874,7 @@ export const FillHachureIcon = createIcon(
       </clipPath>
     </defs>
   </>,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const FillCrossHatchIcon = createIcon(
@@ -830,7 +895,7 @@ export const FillCrossHatchIcon = createIcon(
       </clipPath>
     </defs>
   </>,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const FillSolidIcon = createIcon(
@@ -838,7 +903,7 @@ export const FillSolidIcon = createIcon(
     <rect x=".5" y=".5" width={15} height={15} rx="2.5" fill="currentColor" />
     <rect x=".5" y=".5" width={15} height={15} rx="2.5" stroke="currentColor" />
   </>,
-  { width: 16, height: 16, fill: "none" },
+  newIconProps,
 );
 
 export const StrokeWidthIcon = ({
