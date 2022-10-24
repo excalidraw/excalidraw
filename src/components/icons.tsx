69c4bd59eac1a8b56ca483aee3b68d48dac8dd1e
barnabasmolnar <barnabas@excalidraw.com>
2022-10-24 15:36:45 +0200

    Icon sizing on smaller screens.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1237,20 +1237,24 @@ export const CenterHorizontallyIcon = createIcon(
   newIconProps,
 );
 
-// export const users = createIcon(
-//   "M192 256c61.9 0 112-50.1 112-112S253.9 32 192 32 80 82.1 80 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C51.6 288 0 339.6 0 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zM480 256c53 0 96-43 96-96s-43-96-96-96-96 43-96 96 43 96 96 96zm48 32h-3.8c-13.9 4.8-28.6 8-44.2 8s-30.3-3.2-44.2-8H432c-20.4 0-39.2 5.9-55.7 15.4 24.4 26.3 39.7 61.2 39.7 99.8v38.4c0 2.2-.5 4.3-.6 6.4H592c26.5 0 48-21.5 48-48 0-61.9-50.1-112-112-112z",
-//   { width: 640, height: 512, mirror: true },
-// );
-
 export const users = createIcon(
-  <path
-    d="M6 7.333A2.667 2.667 0 1 0 6 2a2.667 2.667 0 0 0 0 5.333ZM2 14v-1.333A2.667 2.667 0 0 1 4.667 10h2.666A2.667 2.667 0 0 1 10 12.667V14M10.667 2.087a2.666 2.666 0 0 1 0 5.166M14 14v-1.333a2.667 2.667 0 0 0-2-2.567"
-    stroke="currentColor"
-    strokeWidth="1.2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  />,
-  { width: 16, height: 16, mirror: true, fill: "none" },
+  <>
+    <g
+      clipPath="url(#a)"
+      stroke="currentColor"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M7.5 9.167a3.333 3.333 0 1 0 0-6.667 3.333 3.333 0 0 0 0 6.667ZM2.5 17.5v-1.667A3.333 3.333 0 0 1 5.833 12.5h3.334a3.333 3.333 0 0 1 3.333 3.333V17.5M13.333 2.608a3.333 3.333 0 0 1 0 6.459M17.5 17.5v-1.667a3.333 3.333 0 0 0-2.5-3.208" />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h20v20H0z" />
+      </clipPath>
+    </defs>
+  </>,
+  newIconProps,
 );
 
 // not mirrored because it's inspired by a playback control, which is always RTL
