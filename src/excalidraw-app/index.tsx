0f9a7fc4b29b6006753f158598491c85466d6667
dwelle <luzar.david@gmail.com>
2022-11-01 11:25:22 +0100

    add ga for eplus redirect button

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -598,7 +598,7 @@ const ExcalidrawWrapper = () => {
       <div style={{ display: "flex", gap: ".5rem", alignItems: "center" }}>
         {isExcalidrawPlusSignedUser && (
           <a
-            href={`${process.env.REACT_APP_PLUS_APP}/#excalidraw-redirect`}
+            href={`${process.env.REACT_APP_PLUS_APP}?utm_source=excalidraw&utm_medium=app&utm_content=signedInUserRedirectButton#excalidraw-redirect`}
             target="_blank"
             rel="noreferrer"
             className="plus-button"
