5d011a7b6e07567dd076680a6e9ca72086a17535
David Luzar <luzar.david@gmail.com>
2022-11-25 10:19:38 +0100

    Update src/scene/Scene.ts

--- src/scene/Scene.ts
+++ src/scene/Scene.ts
@@ -152,7 +152,7 @@ class Scene {
   }
 
   insertElementAtIndex(element: ExcalidrawElement, index: number) {
-    if (Number.isFinite(index) && index < 0) {
+    if (!Number.isFinite(index) || index < 0) {
       throw new Error(
         "insertElementAtIndex can only be called with index >= 0",
       );
