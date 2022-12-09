50787330a8deb58d6662e983f9fab53486fd8f1a
Naor Peled <thehecticbyte@gmail.com>
2022-12-09 20:58:27 +0200

    wip

--- src/clipboard.test.ts
+++ src/clipboard.test.ts
@@ -1,184 +1,50 @@
-import * as clipboard from "./clipboard";
-import {
-  type ElementsClipboard,
-  parseClipboard,
-  transformClipboardElementsToText,
-} from "./clipboard";
+import { parseClipboard, transformClipboardElementsToText } from "./clipboard";
 
-// @ts-expect-error - we need this in order for the tests not to fail on document.execCommand not being defined
-jest.spyOn(clipboard, "copyTextViaExecCommand").mockResolvedValue(true);
+describe("clipboard", () => {
+  describe("parseClipboard", () => {
+    it("should parse valid json correctly", async () => {
+      let text = "123";
 
-describe("parseClipboard", () => {
-  it("should parse valid json correctly", async () => {
-    let text = "123";
+      let clipboardData = await parseClipboard({
+        // @ts-ignore
+        clipboardData: {
+          getData: () => text,
+        },
+      });
 
-    let clipboardData = await parseClipboard({
-      //@ts-ignore
-      clipboardData: {
-        getData: () => text,
-      },
+      expect(clipboardData.text).toBe(text);
+
+      text = "[123]";
+
+      clipboardData = await parseClipboard({
+        // @ts-ignore
+        clipboardData: {
+          getData: () => text,
+        },
+      });
+
+      expect(clipboardData.text).toBe(text);
     });
+  });
 
-    expect(clipboardData.text).toBe(text);
+  describe("transformClipboardElementsToText", () => {
+    it('should return a concatenated string of the text contents when all elements are of type "text"', () => {
+      const elements: Parameters<typeof transformClipboardElementsToText>[0] = [
+        // @ts-expect-error - we only care about the type and text properties in this test
+        {
+          type: "text",
+          text: "123\n456",
+        },
+        // @ts-expect-error - we only care about the type and text properties in this test
+        {
+          type: "text",
+          text: "789",
+        },
+      ];
 
-    text = "[123]";
+      const result = transformClipboardElementsToText(elements);
 
-    clipboardData = await parseClipboard({
-      //@ts-ignore
-      clipboardData: {
-        getData: () => text,
-      },
+      expect(result).toBe("123\n456\n789");
     });
-
-    expect(clipboardData.text).toBe(text);
-  });
-});
-
-describe("copyToClipboard", () => {
-  const copyTextToSystemClipboard = jest
-    .spyOn(clipboard, "copyTextToSystemClipboard")
-    .mockResolvedValue();
-  const transformClipboardElementsToText = jest.spyOn(
-    clipboard,
-    "transformClipboardElementsToText",
-  );
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-
-  it("should call transformClipboardElementsToText with correct arguments", async () => {
-    const elements: Parameters<typeof clipboard["copyToClipboard"]>[0] = [
-      {
-        id: "1",
-        type: "rectangle",
-        x: 0,
-        y: 0,
-        width: 100,
-        height: 100,
-        angle: 0,
-        strokeColor: "#000000",
-        backgroundColor: "#ffffff",
-        fillStyle: "hachure",
-        strokeWidth: 1,
-        strokeStyle: "solid",
-        roughness: 1,
-        opacity: 100,
-        seed: 0,
-        groupIds: [],
-        roundness: null,
-        version: 0,
-        isDeleted: false,
-        versionNonce: 0,
-        boundElements: [],
-        updated: 0,
-        link: null,
-        locked: false,
-      },
-    ];
-    // @ts-expect-error - we don't really need to pass an app state here to test this
-    const appState: Parameters<typeof clipboard["copyToClipboard"]>[1] = {};
-    const files: Parameters<typeof clipboard["copyToClipboard"]>[2] = null;
-
-    const input: Parameters<typeof clipboard["copyToClipboard"]> = [
-      elements,
-      appState,
-      files,
-    ];
-
-    await clipboard.copyToClipboard(...input);
-
-    expect(transformClipboardElementsToText).toHaveBeenCalledWith(elements);
-  });
-
-  it("should copy elements as text to clipboard", async () => {
-    const elements: Parameters<typeof clipboard["copyToClipboard"]>[0] = [
-      {
-        id: "1",
-        type: "rectangle",
-        x: 0,
-        y: 0,
-        width: 100,
-        height: 100,
-        angle: 0,
-        strokeColor: "#000000",
-        backgroundColor: "#ffffff",
-        fillStyle: "hachure",
-        strokeWidth: 1,
-        strokeStyle: "solid",
-        roughness: 1,
-        opacity: 100,
-        seed: 0,
-        groupIds: [],
-        roundness: null,
-        version: 0,
-        isDeleted: false,
-        versionNonce: 0,
-        boundElements: [],
-        updated: 0,
-        link: null,
-        locked: false,
-      },
-    ];
-    // @ts-expect-error - we don't really need to pass an app state here to test this
-    const appState: Parameters<typeof clipboard["copyToClipboard"]>[1] = {};
-    const files: Parameters<typeof clipboard["copyToClipboard"]>[2] = null;
-
-    const input: Parameters<typeof clipboard["copyToClipboard"]> = [
-      elements,
-      appState,
-      files,
-    ];
-
-    await clipboard.copyToClipboard(...input);
-
-    expect(copyTextToSystemClipboard).toHaveBeenCalledWith(expect.any(String));
-  });
-
-  it("should log an error if copyTextToSystemClipboard throws", async () => {
-    const error = new Error("some-error");
-    copyTextToSystemClipboard.mockRejectedValue(error);
-
-    const consoleError = jest.spyOn(console, "error").mockImplementation();
-
-    const elements: Parameters<typeof clipboard["copyToClipboard"]>[0] = [];
-    // @ts-expect-error - we don't really need to pass an app state here to test this
-    const appState: Parameters<typeof clipboard["copyToClipboard"]>[1] = {};
-    const files: Parameters<typeof clipboard["copyToClipboard"]>[2] = null;
-
-    const input: Parameters<typeof clipboard["copyToClipboard"]> = [
-      elements,
-      appState,
-      files,
-    ];
-
-    await clipboard.copyToClipboard(...input);
-
-    expect(consoleError).toHaveBeenNthCalledWith(2, error);
-  });
-});
-
-describe("transformClipboardElementsToText", () => {
-  it('should return a concatenated string of the text contents when all elements are of type "text"', () => {
-    const elements: Parameters<typeof transformClipboardElementsToText>[0] = [
-      // @ts-expect-error - we only care about the type and text properties in this test
-      {
-        type: "text",
-        text: "123\n456",
-      },
-      // @ts-expect-error - we only care about the type and text properties in this test
-      {
-        type: "text",
-        text: "789",
-      },
-    ];
-
-    const result = transformClipboardElementsToText(elements);
-
-    expect(result).toBe("123\n456\n789");
   });
 });
