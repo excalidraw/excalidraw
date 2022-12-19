f5a84f05c39b01b629ad729a5b22592887243676
Daniel J. Geiger <1852529+DanielJGeiger@users.noreply.github.com>
2022-12-19 14:48:21 -0600

    Implement #5765 with test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2084,6 +2084,15 @@ class App extends React.Component<AppProps, AppState> {
         this.setState({ isBindingEnabled: false });
       }
 
+      if (event.key === KEYS.PAGE_UP || event.key === KEYS.PAGE_DOWN) {
+        let offsetY = this.state.height / this.state.zoom.value;
+        if (event.key === KEYS.PAGE_DOWN) {
+          offsetY = -offsetY;
+        }
+        const scrollY = this.state.scrollY + offsetY;
+        this.setState({ scrollY });
+      }
+
       if (isArrowKey(event.key)) {
         const step =
           (this.state.gridSize &&
