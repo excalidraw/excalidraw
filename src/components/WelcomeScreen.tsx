3e837c2e32db5952521a83912fc4878c3eb5cd3f
dwelle <luzar.david@gmail.com>
2023-01-08 10:33:18 +0100

    don't mandate specific Center children and render anything

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -58,26 +58,18 @@ const WelcomeScreenMenuItem = ({
 };
 
 const Center = ({ children }: { children?: React.ReactNode }) => {
-  const childrenComponents =
-    ReactChildrenToObject<UIWelcomeScreenCenterComponents>(
-      children ||
-        (
-          <>
-            <Logo />
-            <Heading>{t("welcomeScreen.defaults.center_heading")}</Heading>
-            <Menu>
-              <MenuItemLoadScene />
-              <MenuItemHelp />
-            </Menu>
-          </>
-        ).props.children,
-    );
-
   return (
     <div className="WelcomeScreen-container">
-      {childrenComponents.Logo}
-      {childrenComponents.Heading}
-      {childrenComponents.Menu}
+      {children || (
+        <>
+          <Logo />
+          <Heading>{t("welcomeScreen.defaults.center_heading")}</Heading>
+          <Menu>
+            <MenuItemLoadScene />
+            <MenuItemHelp />
+          </Menu>
+        </>
+      )}
     </div>
   );
 };
