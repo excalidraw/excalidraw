e3cf236fe6e2c0b045d971aec493e04002c10b39
Aakansha Doshi <aakansha1216@gmail.com>
2023-01-25 17:16:20 +0530

    enable algolia with dummy creds

--- dev-docs/docusaurus.config.js
+++ dev-docs/docusaurus.config.js
@@ -123,6 +123,15 @@ const config = {
           hideable: true,
         },
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "YOUR_APP_ID",
+
+        // Public API key: it is safe to commit it
+        apiKey: "YOUR_SEARCH_API_KEY",
+
+        indexName: "YOUR_INDEX_NAME",
+      },
     }),
   themes: ["@docusaurus/theme-live-codeblock"],
   plugins: ["docusaurus-plugin-sass"],
