48e8a6e8e349fb02d7ae3a7d90941c8182459a4e
Aakansha Doshi <aakansha1216@gmail.com>
2023-02-01 19:57:54 +0530

    docs: migrating dev docs to docusaurus :) (#6073)
    * docs: migrating existing docs to docosaraus :)
    
    * log broken links
    
    * lint :p
    
    * fix
    
    * divide the doc into diff categories
    
    * fix
    
    * order sidebars and more
    
    * fix lint
    
    * point to installation
    
    * making docs better :)
    
    * fix
    
    * renaming git
    
    * renaming git
    
    * fix links
    
    * fix
    
    * update readme
    
    * fix
    
    * resolve duplicate url and make /docs as base url
    
    * fix
    
    * move main docs as well
    
    * making docs better
    
    * support mdx
    
    * update og
    
    * fix title
    
    * upgrade docusarus to stable version
    
    * use draculla theme
    
    * fix
    
    * make entire sidebar collapsable
    
    * live editor for footer wohoo
    
    * render excalidraw only on client to fix the prod build
    
    * migrate MainMenu to live editor too :)
    
    * lint :p
    
    * cleanup integration and use live editor and tabs
    
    * fix
    
    * Add welcome screen doc
    
    * Live Collaboration comp docs
    
    * Add collaborator example
    
    * Add example
    
    * add more
    
    * remove isCollaborating
    
    * Rewrite ref and move to sidebar
    
    * change color of links inside pre
    
    * add initial data
    
    * fix lint
    
    * Add styling
    
    * fix lint
    
    * Add example for customizing styles
    
    * fix lint
    
    * fix
    
    * fix lint
    
    * Add link to livecollabtrigger
    
    * fix
    
    * rewrite UIOptions to sidebar
    
    * move initialdata to sidebar
    
    * move render props to sidebar and rewrite renderTopRightUI and renderCustomStats
    
    * rewrite renderSidebar
    
    * update og
    
    * update url for testing
    
    * fix url
    
    * update readme
    
    * fix style
    
    * tweaks
    
    * Add highlight comp to highlight text
    
    * Add bash syntax highlight
    
    * fix
    
    * tweaks
    
    * fix
    
    * rewrite export utilities
    
    * fix restore
    
    * rewrite utils
    
    * move constants to sidebar
    
    * update readme
    
    * add copyright
    
    * fix links style
    
    * Add linkedin
    
    * tweaks
    
    * rename package to @excalidraw/excalidraw
    
    * enable algolia with dummy creds
    
    * tweaks to integration doc
    
    * tweak WelcomeScreen docs to reflect upcoming API changes
    
    * tweak components intro
    
    * tweak nomenclature
    
    * fix admonition
    
    * rename `components` sidebar item and change order of components list
    
    * uncollapse package section in sidebar
    
    * show level 4 haeadings in TOC
    
    * remove algolia
    
    * remove unused assets
    
    * capitalize C
    
    * tweak
    
    * rename components to App
    
    * rename components -> children-components in the routes
    
    * move notable used tools to intro
    
    * update MainMenu docs with `onSelect` preventDefault behavior
    
    * change sidebar label for children components
    
    * use code
    
    * tweak README & docs intro
    
    * tweak package development doc
    
    * make scrollbar gutter stable
    
    * tweak api intro
    
    * add admonition for export utils
    
    * use next
    
    * wip
    
    * wip
    
    * make excalidraw examples use current color theme & prefer system
    
    * fix welcomescreen docs
    
    * use latest temp release
    
    * fix component order
    
    * revert wip changes
    
    * use next
    
    * tweak
    
    * increase height to fix welcome screen hint
    
    * tweak editor height
    
    * update excal version
    
    * wrap Excal with forwardRef to fix refs
    
    * migrate contributing.md
    
    * fix broken links
    
    ---------
    
    Co-authored-by: dwelle <luzar.david@gmail.com>

---
+++ dev-docs/src/theme/ReactLiveScope/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import initialData from "@site/src/initialData";
+import { useColorMode } from "@docusaurus/theme-common";
+
+let ExcalidrawComp = {};
+if (ExecutionEnvironment.canUseDOM) {
+  ExcalidrawComp = require("@excalidraw/excalidraw");
+}
+const Excalidraw = React.forwardRef((props, ref) => {
+  const { colorMode } = useColorMode();
+  return <ExcalidrawComp.Excalidraw theme={colorMode} {...props} ref={ref} />;
+});
+// Add react-live imports you need here
+const ExcalidrawScope = {
+  React,
+  ...React,
+  Excalidraw,
+  Footer: ExcalidrawComp.Footer,
+  useDevice: ExcalidrawComp.useDevice,
+  MainMenu: ExcalidrawComp.MainMenu,
+  WelcomeScreen: ExcalidrawComp.WelcomeScreen,
+  LiveCollaborationTrigger: ExcalidrawComp.LiveCollaborationTrigger,
+  Sidebar: ExcalidrawComp.Sidebar,
+  exportToCanvas: ExcalidrawComp.exportToCanvas,
+  initialData,
+};
+
+export default ExcalidrawScope;
