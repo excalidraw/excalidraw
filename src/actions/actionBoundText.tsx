12770d021be59982cbfeb24217f3a4e3c9c6eeac
Aakansha Doshi <aakansha1216@gmail.com>
2023-03-03 18:02:43 +0530

    fix

--- src/actions/actionBoundText.tsx
+++ src/actions/actionBoundText.tsx
@@ -1,9 +1,4 @@
-import {
-  BOUND_TEXT_PADDING,
-  ROUNDNESS,
-  TEXT_ALIGN,
-  VERTICAL_ALIGN,
-} from "../constants";
+import { BOUND_TEXT_PADDING, ROUNDNESS, VERTICAL_ALIGN } from "../constants";
 import { getNonDeletedElements, isTextElement, newElement } from "../element";
 import { mutateElement } from "../element/mutateElement";
 import {
