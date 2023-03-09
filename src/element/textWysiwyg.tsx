a818cd606587fa348bc8bb742f0e4c13e9c0c36a
Aakansha Doshi <aakansha1216@gmail.com>
2023-03-09 13:07:36 +0530

    fix: add an offset of 0.5px for text editor in containers (#6328)
    * fix: add an offset of 0.5px for text editor in containers
    
    * fix specs and lint

--- src/element/textWysiwyg.tsx
+++ src/element/textWysiwyg.tsx
@@ -11,7 +11,7 @@ import {
   isBoundToContainer,
   isTextElement,
 } from "./typeChecks";
-import { CLASSES, isFirefox, isSafari, VERTICAL_ALIGN } from "../constants";
+import { CLASSES, VERTICAL_ALIGN } from "../constants";
 import {
   ExcalidrawElement,
   ExcalidrawLinearElement,
@@ -273,8 +273,7 @@ export const textWysiwyg = ({
       if (!container) {
         maxWidth = (appState.width - 8 - viewportX) / appState.zoom.value;
         textElementWidth = Math.min(textElementWidth, maxWidth);
-      } else if (isFirefox || isSafari) {
-        // As firefox, Safari needs little higher dimensions on DOM
+      } else {
         textElementWidth += 0.5;
       }
       // Make sure text editor height doesn't go beyond viewport
