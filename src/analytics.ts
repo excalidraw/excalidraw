fdb29ca05a93dcca9d99f566fe0d5f2fc73e5dee
Maielo <maielo.mv@gmail.com>
2023-03-28 15:36:13 +0200

    [fix] lint & notes

--- src/analytics.ts
+++ src/analytics.ts
@@ -4,30 +4,30 @@ export const trackEvent = (
   label?: string,
   value?: number,
 ) => {
-  if (
-    (typeof process !== "undefined" && process.env?.JEST_WORKER_ID) ||
-    !window
-  ) {
-    return;
-  }
+  try {
+    if (
+      (typeof process !== "undefined" && process.env?.JEST_WORKER_ID) ||
+      typeof window === "undefined"
+    ) {
+      return;
+    }
 
-  // Uncomment the next line to track locally
-  // console.log("Track Event", { category, action, label, value });
+    // Uncomment the next line to track locally
+    // console.log("Track Event", { category, action, label, value });
 
-  if (process.env?.REACT_APP_GOOGLE_ANALYTICS_ID && window.gtag) {
-    try {
+    if (process.env?.REACT_APP_GOOGLE_ANALYTICS_ID && window.gtag) {
       window.gtag("event", action, {
         event_category: category,
         event_label: label,
         value,
       });
-    } catch (error) {
-      console.error("error logging to ga", error);
     }
-  }
 
-  // MATOMO event tracking _paq must be same as the one in index.html
-  if (window._paq) {
-    window._paq.push(["trackEvent", category, action, label, value]);
+    // MATOMO event tracking _paq must be same as the one in index.html
+    if (window._paq) {
+      window._paq.push(["trackEvent", category, action, label, value]);
+    }
+  } catch (error) {
+    console.error("error logging to ga", error);
   }
 };
