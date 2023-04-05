8de65168238b8fb9a638e0c75f596f371983c2c7
Aakansha Doshi <aakansha1216@gmail.com>
2023-04-05 17:33:05 +0530

    floor line height and height when dom height increases than canvas height

--- src/element/textWysiwyg.tsx
+++ src/element/textWysiwyg.tsx
@@ -36,6 +36,7 @@ import {
   getMaxContainerWidth,
   computeContainerDimensionForBoundText,
   measureDOMHeight,
+  getLineHeightInPx,
   splitIntoLines,
 } from "./textElement";
 import {
@@ -278,11 +279,19 @@ export const textWysiwyg = ({
         getFontString(updatedTextElement),
         updatedTextElement.lineHeight,
       );
-
       let lineHeight = element.lineHeight;
-      if (isSafari && domHeight > textElementHeight) {
-        lineHeight = (Math.floor(element.lineHeight * element.fontSize) /
-          element.fontSize) as ExcalidrawTextElement["lineHeight"];
+      if (isSafari) {
+        const lineCount = splitIntoLines(element.text).length;
+        if (domHeight > textElementHeight) {
+          lineHeight = (Math.floor(element.lineHeight * element.fontSize) /
+            element.fontSize) as ExcalidrawTextElement["lineHeight"];
+          textElementHeight =
+            getLineHeightInPx(element.fontSize, lineHeight) * lineCount;
+          mutateElement(updatedTextElement, {
+            lineHeight,
+            height: textElementHeight,
+          });
+        }
       }
 
       // Make sure text editor height doesn't go beyond viewport
