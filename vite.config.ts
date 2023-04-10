dbe31230c7643e46488ecac5380e5114bd495ea4
Weslley Braga <weslley@bambee.com>
2023-04-09 23:39:59 -0500

    feat: add ejs plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import envCompatible from "vite-plugin-env-compatible";
+import {ViteEjsPlugin} from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin(), envCompatible()],
+  plugins: [react(), svgrPlugin(), envCompatible(), ViteEjsPlugin()],
   publicDir: "./public",
 });
