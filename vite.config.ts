dca0306f097dabfd76919dff7575d06b8786f8e1
Weslley Braga <weslley@bambee.com>
2023-04-09 23:34:22 -0500

    fix: href serving from public

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-import envCompatible from 'vite-plugin-env-compatible'
+import envCompatible from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin(), envCompatible()],
+  publicDir: "./public",
 });
