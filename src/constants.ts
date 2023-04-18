a84c1ed3a2c0d879bd3dd3269b9048a2fa9213d3
Karl Horky <karl.horky@gmail.com>
2023-04-18 13:53:22 +0200

    Fix linting warning

--- src/constants.ts
+++ src/constants.ts
@@ -107,7 +107,7 @@ export const THEME = {
 export const WINDOWS_EMOJI_FALLBACK_FONT = "Segoe UI Emoji";
 
 export const DEFAULT_FONT_SIZE = 20;
-export const DEFAULT_FONT_FAMILY: FontFamilyValues = FONT_FAMILY.Virgil["id"];
+export const DEFAULT_FONT_FAMILY: FontFamilyValues = FONT_FAMILY.Virgil.id;
 export const DEFAULT_TEXT_ALIGN = "left";
 export const DEFAULT_VERTICAL_ALIGN = "top";
 export const DEFAULT_VERSION = "{version}";
