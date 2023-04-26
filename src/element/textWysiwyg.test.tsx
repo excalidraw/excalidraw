6a6578f223ae8ade3dce98912b4e99a496f659c7
Aakansha Doshi <aakansha1216@gmail.com>
2023-04-26 12:38:03 +0530

    fix test

--- src/element/textWysiwyg.test.tsx
+++ src/element/textWysiwyg.test.tsx
@@ -1212,37 +1212,47 @@ describe("textWysiwyg", () => {
       expect(h.elements[1].isDeleted).toBe(true);
     });
 
-    it.skip("should restore original container height and clear cache once text is unbind", async () => {
-      const originalRectHeight = rectangle.height;
-      expect(rectangle.height).toBe(originalRectHeight);
-
-      Keyboard.keyPress(KEYS.ENTER);
-      const editor = document.querySelector(
-        ".excalidraw-textEditorContainer > textarea",
-      ) as HTMLTextAreaElement;
-      await new Promise((r) => setTimeout(r, 0));
-
-      fireEvent.change(editor, {
-        target: { value: "Online whiteboard collaboration made easy" },
+    it("should restore original container height and clear cache once text is unbind", async () => {
+      const container = API.createElement({
+        type: "rectangle",
+        height: 75,
+        width: 90,
       });
+      const originalRectHeight = container.height;
+      expect(container.height).toBe(originalRectHeight);
 
-      editor.blur();
-      expect((h.elements[1] as ExcalidrawTextElementWithContainer).text).toBe(
-        "Online \nwhitebo\nard \ncollabo\nration \nmade \neasy",
-      );
-      expect(h.elements[0].height).toBe(185);
-      mouse.select(rectangle);
+      const text = API.createElement({
+        type: "text",
+        text: "Online whiteboard collaboration made easy",
+      });
+      h.elements = [container, text];
+      API.setSelectedElements([container, text]);
+
       fireEvent.contextMenu(GlobalTestState.canvas, {
         button: 2,
         clientX: 20,
         clientY: 30,
       });
-      const contextMenu = document.querySelector(".context-menu");
+      let contextMenu = document.querySelector(".context-menu");
+
+      fireEvent.click(
+        queryByText(contextMenu as HTMLElement, "Bind text to the container")!,
+      );
+
+      expect((h.elements[1] as ExcalidrawTextElementWithContainer).text).toBe(
+        "Online \nwhitebo\nard \ncollabo\nration \nmade \neasy",
+      );
+      fireEvent.contextMenu(GlobalTestState.canvas, {
+        button: 2,
+        clientX: 20,
+        clientY: 30,
+      });
+      contextMenu = document.querySelector(".context-menu");
       fireEvent.click(queryByText(contextMenu as HTMLElement, "Unbind text")!);
       expect(h.elements[0].boundElements).toEqual([]);
-      expect(getOriginalContainerHeightFromCache(rectangle.id)).toBe(null);
+      expect(getOriginalContainerHeightFromCache(container.id)).toBe(null);
 
-      expect(rectangle.height).toBe(originalRectHeight);
+      expect(container.height).toBe(originalRectHeight);
     });
 
     it("should reset the container height cache when resizing", async () => {
