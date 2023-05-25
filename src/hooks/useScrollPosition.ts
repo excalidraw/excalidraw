2494ce1013ad5000d4c36ab084fe62432d2bbed9
Arnošt Pleskot <arnostpleskot@gmail.com>
2023-05-25 18:43:29 +0200

    fix: remove event handler from same element as is set on

--- src/hooks/useScrollPosition.ts
+++ src/hooks/useScrollPosition.ts
@@ -8,25 +8,26 @@ export const useScrollPosition = <T extends HTMLElement>(
   elementRef: React.RefObject<T>,
 ) => {
   const [scrollPosition, setScrollPosition] = useAtom(scrollPositionAtom);
+  const { current: element } = elementRef;
 
   useEffect(() => {
     const handleScroll = throttle(() => {
-      if (elementRef.current) {
-        const { scrollTop } = elementRef.current;
+      if (element) {
+        const { scrollTop } = element;
         setScrollPosition(scrollTop);
       }
     }, 200);
 
-    if (elementRef.current) {
-      elementRef.current.addEventListener("scroll", handleScroll);
+    if (element) {
+      element.addEventListener("scroll", handleScroll);
     }
 
     return () => {
-      if (elementRef.current) {
-        elementRef.current.removeEventListener("scroll", handleScroll);
+      if (element) {
+        element.removeEventListener("scroll", handleScroll);
       }
     };
-  }, [elementRef, setScrollPosition]);
+  }, [element, setScrollPosition]);
 
   return scrollPosition;
 };
