d65b3331dd5e4b93e36eb643fb4dcc64aee9b6cd
dwelle <luzar.david@gmail.com>
2023-05-26 10:45:33 +0200

    read from ref inside effect to fix stale ref issues

--- src/hooks/useScrollPosition.ts
+++ src/hooks/useScrollPosition.ts
@@ -8,26 +8,25 @@ export const useScrollPosition = <T extends HTMLElement>(
   elementRef: React.RefObject<T>,
 ) => {
   const [scrollPosition, setScrollPosition] = useAtom(scrollPositionAtom);
-  const { current: element } = elementRef;
 
   useEffect(() => {
-    const handleScroll = throttle(() => {
-      if (element) {
-        const { scrollTop } = element;
-        setScrollPosition(scrollTop);
-      }
-    }, 200);
-
-    if (element) {
-      element.addEventListener("scroll", handleScroll);
+    const { current: element } = elementRef;
+    if (!element) {
+      return;
     }
 
+    // console.log("2", { element });
+    const handleScroll = throttle(() => {
+      const { scrollTop } = element;
+      setScrollPosition(scrollTop);
+    }, 200);
+
+    element.addEventListener("scroll", handleScroll);
+
     return () => {
-      if (element) {
-        element.removeEventListener("scroll", handleScroll);
-      }
+      element.removeEventListener("scroll", handleScroll);
     };
-  }, [element, setScrollPosition]);
+  }, [elementRef, setScrollPosition]);
 
   return scrollPosition;
 };
