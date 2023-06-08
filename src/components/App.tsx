a39640ead1d90e7c4b5f4d30fdf07735bf5f3f3e
WBbug <1056342711@qq.com>
2023-06-08 17:41:22 +0800

    fix: delete setCursor when resize (#6660)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4155,12 +4155,6 @@ class App extends React.Component<AppProps, AppState> {
         );
       }
       if (pointerDownState.resize.handleType) {
-        setCursor(
-          this.canvas,
-          getCursorForResizingElement({
-            transformHandleType: pointerDownState.resize.handleType,
-          }),
-        );
         pointerDownState.resize.isResizing = true;
         pointerDownState.resize.offset = tupleToCoors(
           getResizeOffsetXY(
