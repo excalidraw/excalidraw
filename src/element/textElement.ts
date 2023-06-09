c9b3cc5de6590a484cbb51d74d14d486e4d33d1e
吴佳杰 <827121759@qq.com>
2023-06-09 11:26:58 +0800

    chinese

--- src/element/textElement.ts
+++ src/element/textElement.ts
@@ -957,7 +957,7 @@ export const isMeasureTextSupported = () => {
 const DEFAULT_LINE_HEIGHT = {
   // ~1.25 is the average for Virgil in WebKit and Blink.
   // Gecko (FF) uses ~1.28.
-  [FONT_FAMILY.Virgil]: 1.25 as ExcalidrawTextElement["lineHeight"],
+  [FONT_FAMILY['Virgil, HanziPen SC, KaiTi']]: 1.25 as ExcalidrawTextElement["lineHeight"],
   // ~1.15 is the average for Virgil in WebKit and Blink.
   // Gecko if all over the place.
   [FONT_FAMILY.Helvetica]: 1.15 as ExcalidrawTextElement["lineHeight"],
