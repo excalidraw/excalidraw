1fe5027090941145e9a1c7abda6f4b26953388cd
Sudharsan Aravind <63002244+vsaravind01@users.noreply.github.com>
2023-06-15 14:58:11 +0530

    fix: updated link for documentation page under help section (#6654)
    * fix: updated link for documentation page under help section
    
    * Update docs link
    
    ---------
    
    Co-authored-by: Aakansha Doshi <aakansha1216@gmail.com>

--- src/components/HelpDialog.tsx
+++ src/components/HelpDialog.tsx
@@ -12,7 +12,7 @@ const Header = () => (
   <div className="HelpDialog__header">
     <a
       className="HelpDialog__btn"
-      href="https://github.com/excalidraw/excalidraw#documentation"
+      href="https://docs.excalidraw.com"
       target="_blank"
       rel="noopener noreferrer"
     >
