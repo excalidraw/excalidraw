0ef60ce7d887bc90e442bb48539a36a4f4125202
zsviczian <viczian.zsolt@gmail.com>
2023-06-28 19:07:19 +0200

    update iframes.ts to match main PR

--- src/element/iframe.ts
+++ src/element/iframe.ts
@@ -8,35 +8,51 @@ import { getContainerElement } from "./textElement";
 import { isIFrameElement } from "./typeChecks";
 import { ExcalidrawElement, NonDeletedExcalidrawElement } from "./types";
 
-const YOUTUBE_REG =
-  /^(?:http(?:s)?:\/\/)?(?:(?:w){3}.)?youtu(?:be|.be)?(?:\.com)?\/(?:embed\/|watch\?v=|shorts\/)?([a-zA-Z0-9_-]+)(?:\?t=|&t=)?([a-zA-Z0-9_-]+)?[^\s]*$/;
-const VIMEO_REG =
-  /^(?:http(?:s)?:\/\/)?(?:(?:w){3}.)?(?:player\.)?vimeo\.com\/(?:video\/)?([^?\s]+)(?:\?.*)?$/;
-const TWITTER_REG = /^(?:http(?:s)?:\/\/)?(?:(?:w){3}.)?twitter.com/;
-const FIGMA_REG = /^https:\/\/www\.figma\.com/;
-const EXCALIDRAW_REG = /^https:\/\/excalidraw.com/;
-
-export const getEmbedLink = (
-  link?: string | null,
-): {
+type EmbeddedLink = {
   link: string;
   aspectRatio: { w: number; h: number };
   type: "video" | "generic";
-} | null => {
+} | null;
+
+const embeddedLinkCache = new Map<string, EmbeddedLink>();
+
+const YOUTUBE_REG =
+  /^(?:http(?:s)?:\/\/)?(?:(?:w){3}.)?youtu(?:be|.be)?(?:\.com)?\/(embed\/|watch\?v=|shorts\/|playlist\?list=|embed\/videoseries\?list=)?([a-zA-Z0-9_-]+)(?:\?t=|&t=)?([a-zA-Z0-9_-]+)?[^\s]*$/;
+const VIMEO_REG =
+  /^(?:http(?:s)?:\/\/)?(?:(?:w){3}.)?(?:player\.)?vimeo\.com\/(?:video\/)?([^?\s]+)(?:\?.*)?$/;
+//const TWITTER_REG = /^(?:http(?:s)?:\/\/)?(?:(?:w){3}.)?twitter.com/;
+const FIGMA_REG = /^https:\/\/www\.figma\.com/;
+//const EXCALIDRAW_REG = /^https:\/\/excalidraw.com/;
+
+export const getEmbedLink = (link?: string | null): EmbeddedLink => {
   if (!link) {
     return null;
+  }
+
+  if (embeddedLinkCache.has(link)) {
+    return embeddedLinkCache.get(link)!;
   }
 
   let type: "video" | "generic" = "generic";
   let aspectRatio = { w: 560, h: 840 };
   const ytLink = link.match(YOUTUBE_REG);
-  if (ytLink?.[1]) {
-    const time = ytLink[2] ? `&t=${ytLink[2]}` : ``;
-    const target = `${ytLink[1]}?enablejsapi=1${time}`;
+  if (ytLink?.[2]) {
+    const time = ytLink[3] ? `&t=${ytLink[3]}` : ``;
     const isPortrait = link.includes("shorts");
     type = "video";
-    link = `https://www.youtube.com/embed/${target}`;
+    switch (ytLink[1]) {
+      case "embed/":
+      case "watch?v=":
+      case "shorts/":
+        link = `https://www.youtube.com/embed/${ytLink[2]}?enablejsapi=1${time}`;
+        break;
+      case "playlist?list=":
+      case "embed/videoseries?list=":
+        link = `https://www.youtube.com/embed/videoseries?list=${ytLink[2]}&enablejsapi=1${time}`;
+        break;
+    }
     aspectRatio = isPortrait ? { w: 315, h: 560 } : { w: 560, h: 315 };
+    embeddedLinkCache.set(link, { link, aspectRatio, type });
     return { link, aspectRatio, type };
   }
 
@@ -46,16 +62,18 @@ export const getEmbedLink = (
     type = "video";
     link = `https://player.vimeo.com/video/${target}?api=1`;
     aspectRatio = { w: 560, h: 315 };
+    embeddedLinkCache.set(link, { link, aspectRatio, type });
     return { link, aspectRatio, type };
   }
 
-  const twitterLink = link.match(TWITTER_REG);
+  /*const twitterLink = link.match(TWITTER_REG);
   if (twitterLink) {
     type = "generic";
     link = `https://twitframe.com/show?url=${encodeURIComponent(link)}`;
     aspectRatio = { w: 550, h: 550 };
+    embeddedLinkCache.set(link, { link, aspectRatio, type });
     return { link, aspectRatio, type };
-  }
+  }*/
 
   const figmaLink = link.match(FIGMA_REG);
   if (figmaLink) {
@@ -64,9 +82,11 @@ export const getEmbedLink = (
       link,
     )}`;
     aspectRatio = { w: 550, h: 550 };
+    embeddedLinkCache.set(link, { link, aspectRatio, type });
     return { link, aspectRatio, type };
   }
 
+  embeddedLinkCache.set(link, { link, aspectRatio, type });
   return { link, aspectRatio, type };
 };
 
@@ -170,9 +190,9 @@ export class IFrameURLValidator {
     return Boolean(
       url.match(YOUTUBE_REG) ||
         url.match(VIMEO_REG) ||
-        url.match(TWITTER_REG) ||
-        url.match(FIGMA_REG) ||
-        url.match(EXCALIDRAW_REG),
+        //url.match(TWITTER_REG) ||
+        url.match(FIGMA_REG),
+      //|| url.match(EXCALIDRAW_REG),
     );
   }
 }
