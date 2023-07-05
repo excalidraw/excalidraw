8fb64e203c8cfd7a16197183b09f074bd10ff43d
Aakansha Doshi <aakansha1216@gmail.com>
2023-07-05 12:31:19 +0530

    fix lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
