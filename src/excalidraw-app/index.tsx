ad0ab8f21ecececc81faca106c4d5f9d9b75a744
dwelle <luzar.david@gmail.com>
2023-07-18 12:45:56 +0200

    [debug]

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -696,6 +696,7 @@ const ExcalidrawWrapper = () => {
             />
           );
         }}
+        validateEmbeddable={true}
       >
         <AppMainMenu
           setCollabDialogShown={setCollabDialogShown}
