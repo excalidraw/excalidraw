b3740f1cf5a458919a1ab4e7b2194fc29ee7458f
dwelle <luzar.david@gmail.com>
2023-07-19 00:05:07 +0200

    add `iframe.sandbox` attrs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -934,6 +934,7 @@ class App extends React.Component<AppProps, AppState> {
                       title="Excalidraw Embedded Content"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen={true}
+                      sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox allow-presentation"
                     />
                   )}
                 </div>
