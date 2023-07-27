5ba3c94fc3f92860593c19e9b939251f2a75346d
Aakansha Doshi <aakansha1216@gmail.com>
2023-07-27 16:01:23 +0530

    integrate vite-plugin-checker for ts errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { VitePWA } from "vite-plugin-pwa";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,6 +31,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    checker({ typescript: true }),
     svgrPlugin(),
     ViteEjsPlugin(),
     VitePWA({
