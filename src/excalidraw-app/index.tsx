803e14ada1d048acd7410f0fa7afe5f4e75f178f
dwelle <luzar.david@gmail.com>
2023-08-02 17:41:32 +0200

    Revert "[debug]"
    This reverts commit 71eb3023b2fd267db44fc5d11c4fb6814b4206a6.

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -731,14 +731,6 @@ const ExcalidrawWrapper = () => {
             />
           );
         }}
-        scrollConstraints={{
-          x: 0,
-          y: 0,
-          width: 2560,
-          height: 1300,
-          lockZoom: true,
-          viewportZoomFactor: 0.1,
-        }}
       >
         <AppMainMenu
           setCollabDialogShown={setCollabDialogShown}
