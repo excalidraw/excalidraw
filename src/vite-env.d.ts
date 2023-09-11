6abf1942814ec002f916b8a17c52dd124531bc42
David Luzar <luzar.david@gmail.com>
2023-09-11 23:13:16 +0200

    fix: export to plus url (#6980)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -47,6 +47,9 @@ interface ImportMetaEnv {
   VITE_PKG_VERSION: string;
   VITE_IS_EXCALIDRAW_NPM_PACKAGE: string;
 
+  VITE_APP_PLUS_LP: string;
+  VITE_APP_PLUS_APP: string;
+
   VITE_WORKER_ID: string;
   MODE: string;
   DEV: string;
