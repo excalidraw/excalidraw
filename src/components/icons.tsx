070b635f99780468f4479fbff3e1cb3985ef87b2
dwelle <luzar.david@gmail.com>
2023-09-28 22:41:03 +0200

    update icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1655,12 +1655,10 @@ export const frameToolIcon = createIcon(
 );
 
 export const laserPointerToolIcon = createIcon(
-  <>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <path d="M3 12h18"></path>
-    <path d="M12 21v-18"></path>
-    <path d="M7.5 7.5l9 9"></path>
-    <path d="M7.5 16.5l9 -9"></path>
-  </>,
-  tablerIconProps,
+  <g fill="none" stroke-width="1.25" stroke="currentColor">
+    <path d="m9.644 13.69 7.774-7.773a2.357 2.357 0 0 0-3.334-3.334l-7.773 7.774L8 12l1.643 1.69Z" />
+    <path d="m13.25 3.417 3.333 3.333M10 10l2-2M5 15l3-3M2.156 17.894l1-1M5.453 19.029l-.144-1.407M2.377 11.887l.866 1.118M8.354 17.273l-1.194-.758M.953 14.652l1.408.13" />
+  </g>,
+
+  20,
 );
