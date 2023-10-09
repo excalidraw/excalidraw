876977e2bf5b9ccb12159a39f20d83340f4a7446
mazijian <1245113225@qq.com>
2023-10-09 16:01:26 +0800

    chore: prettier fix

--- src/element/textWysiwyg.tsx
+++ src/element/textWysiwyg.tsx
@@ -584,7 +584,7 @@ export const textWysiwyg = ({
     window.removeEventListener("pointerdown", onPointerDown);
     window.removeEventListener("pointerup", bindBlurEvent);
     window.removeEventListener("blur", handleSubmit);
-    window.removeEventListener("beforeunload",handleSubmit);
+    window.removeEventListener("beforeunload", handleSubmit);
     unbindUpdate();
 
     editable.remove();
@@ -701,7 +701,7 @@ export const textWysiwyg = ({
     passive: false,
     capture: true,
   });
-  window.addEventListener("beforeunload",handleSubmit);
+  window.addEventListener("beforeunload", handleSubmit);
   excalidrawContainer
     ?.querySelector(".excalidraw-textEditorContainer")!
     .appendChild(editable);
