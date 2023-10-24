9f7124f9e5363d7a41b7ad3a2fad53a14560767f
Viczián András <vandras2003@gmail.com>
2023-10-24 20:36:13 +0200

    fix: Actions panel ux improvement (#6850)
    Co-authored-by: dwelle <luzar.david@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -695,3 +695,12 @@ export type KeyboardModifiersObject = {
   altKey: boolean;
   metaKey: boolean;
 };
+
+export type Primitive =
+  | number
+  | string
+  | boolean
+  | bigint
+  | symbol
+  | null
+  | undefined;
