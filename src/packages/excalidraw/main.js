b963098eeede12892e353aee60093953bf18842a
Aakansha Doshi <aakansha1216@gmail.com>
2023-11-08 21:41:38 +0530

    add log

--- src/packages/excalidraw/main.js
+++ src/packages/excalidraw/main.js
@@ -1,3 +1,4 @@
+console.log(window.IS_PREACT, "PREACT");
 if (window.IS_PREACT) {
   module.exports = require("./dist/excalidraw-with-preact.production.min.js");
 } else if (process.env.NODE_ENV === "production") {
