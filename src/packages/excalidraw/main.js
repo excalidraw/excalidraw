5e200e34ccb824a5cf6c879f1d8a9eb85c66b5b6
Aakansha Doshi <aakansha1216@gmail.com>
2023-11-08 19:11:44 +0530

    build: support preact

--- src/packages/excalidraw/main.js
+++ src/packages/excalidraw/main.js
@@ -1,4 +1,6 @@
-if (process.env.NODE_ENV === "production") {
+if (window.IS_PREACT) {
+  module.exports = require("./dist/excalidraw-with-preact.production.min.js");
+} else if (process.env.NODE_ENV === "production") {
   module.exports = require("./dist/excalidraw.production.min.js");
 } else {
   module.exports = require("./dist/excalidraw.development.js");
