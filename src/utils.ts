30a55a6e2d42b0b7525bd6103c216bd7ed6c6aea
dwelle <5153846+dwelle@users.noreply.github.com>
2023-11-08 17:33:33 +0100

    make `isExcalidrawElement` exhaustive

--- src/utils.ts
+++ src/utils.ts
@@ -834,11 +834,18 @@ export const isOnlyExportingSingleFrame = (
   );
 };
 
+/**
+ * supply `null` as message if non-never value is valid, you just need to
+ * typecheck against it
+ */
 export const assertNever = (
   value: never,
-  message: string,
+  message: string | null,
   softAssert?: boolean,
 ): never => {
+  if (!message) {
+    return value;
+  }
   if (softAssert) {
     console.error(message);
     return value;
