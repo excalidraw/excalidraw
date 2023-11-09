f3c05748b0a30467790cab2eed97be91acc5d2aa
Aakansha Doshi <aakansha1216@gmail.com>
2023-11-09 12:54:57 +0530

    Simplify the config and generate prod and dev builds for preact

--- src/packages/excalidraw/main.js
+++ src/packages/excalidraw/main.js
@@ -1,6 +1,11 @@
 console.log(window.IS_PREACT, "PREACT");
-if (window.IS_PREACT) {
-  module.exports = require("./dist/excalidraw-with-preact.production.min.js");
+
+if (window.IS_PREACT === "true") {
+  if (process.env.NODE_ENV === "production") {
+    module.exports = require("./dist/excalidraw-with-preact.production.min.js");
+  } else {
+    module.exports = require("./dist/excalidraw-with-preact.development.js");
+  }
 } else if (process.env.NODE_ENV === "production") {
   module.exports = require("./dist/excalidraw.production.min.js");
 } else {
