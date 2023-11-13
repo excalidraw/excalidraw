9855e1955a4117e1040307257e63c4a0daccbe3a
Aakansha Doshi <aakansha1216@gmail.com>
2023-11-13 16:18:36 +0530

    build: support preact 🥳 (#7255)
    * build: support preact
    
    * add log
    
    * Simplify the config and generate prod and dev builds for preact
    
    * update changelog
    
    * remove logs
    
    * use env variable so its available during build time
    
    * update cl
    
    * fix

--- src/packages/excalidraw/main.js
+++ src/packages/excalidraw/main.js
@@ -1,4 +1,10 @@
-if (process.env.NODE_ENV === "production") {
+if (process.env.IS_PREACT === "true") {
+  if (process.env.NODE_ENV === "production") {
+    module.exports = require("./dist/excalidraw-with-preact.production.min.js");
+  } else {
+    module.exports = require("./dist/excalidraw-with-preact.development.js");
+  }
+} else if (process.env.NODE_ENV === "production") {
   module.exports = require("./dist/excalidraw.production.min.js");
 } else {
   module.exports = require("./dist/excalidraw.development.js");
