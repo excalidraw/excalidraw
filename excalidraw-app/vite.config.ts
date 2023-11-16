7af3f9d6622c00bcd75b7b5f6044e3f48df6cbde
Aakansha Doshi <aakansha1216@gmail.com>
2023-11-16 13:24:54 +0530

    add favicons to manifest

--- excalidraw-app/vite.config.ts
+++ excalidraw-app/vite.config.ts
@@ -116,6 +116,16 @@ export default defineConfig({
             type: "image/png",
             sizes: "180x180",
           },
+          {
+            src: "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
         ],
         start_url: "/",
         display: "standalone",
