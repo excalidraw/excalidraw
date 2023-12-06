d2008d504801da61bc44e7a170dd6706590ea4ba
fdemian <fdemian@gmail.com>
2023-12-05 23:27:23 -0300

    Add checks for vite compatibility.

--- src/packages/excalidraw/main.js
+++ src/packages/excalidraw/main.js
@@ -1,10 +1,10 @@
-if (process.env.IS_PREACT === "true") {
-  if (process.env.NODE_ENV === "production") {
+if ((process && process.env.IS_PREACT === "true") || import.meta.env.IS_PREACT === "true") {
+  if (process.env.NODE_ENV === "production" || import.meta.env.MODE === "production") {
     module.exports = require("./dist/excalidraw-with-preact.production.min.js");
   } else {
     module.exports = require("./dist/excalidraw-with-preact.development.js");
   }
-} else if (process.env.NODE_ENV === "production") {
+} else if ((process && process.env.NODE_ENV === "production") || import.meta.env.MODE === "production") {
   module.exports = require("./dist/excalidraw.production.min.js");
 } else {
   module.exports = require("./dist/excalidraw.development.js");
