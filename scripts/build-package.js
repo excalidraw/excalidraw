080b3238ead1be3b1aa7703b504809755ca1a187
Aakansha Doshi <aakansha1216@gmail.com>
2023-12-18 13:05:17 +0530

    remove entry.js, publicPath and yarn install:deps script

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -2,43 +2,6 @@ const { build } = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
 const { externalGlobalPlugin } = require("esbuild-plugin-external-global");
 
-const fs = require("fs");
-const path = require("path");
-
-function getFiles(dir, files = []) {
-  const fileList = fs.readdirSync(dir);
-  for (const file of fileList) {
-    const name = `${dir}/${file}`;
-    if (
-      name.includes("node_modules") ||
-      name.includes("config") ||
-      name.includes("package.json") ||
-      name.includes("main.js") ||
-      name.includes("index-node.ts") ||
-      name.endsWith(".d.ts")
-    ) {
-      continue;
-    }
-
-    if (fs.statSync(name).isDirectory()) {
-      getFiles(name, files);
-    } else if (
-      !(
-        name.endsWith(".js") ||
-        name.endsWith(".ts") ||
-        name.endsWith(".tsx") ||
-        name.endsWith(".scss") ||
-        name.endsWith(".scss")
-      )
-    ) {
-      continue;
-    } else {
-      files.push(name);
-    }
-  }
-  return files;
-}
-
 const createESMBrowserBuild = async () => {
   await build({
     entryPoints: ["index.tsx"],
@@ -73,26 +36,12 @@ const createESMRawBuild = async () => {
     define: {
       "import.meta.env": "{}",
     },
-    chunkNames: "excalidraw-assets/[name]-[hash]",
     loader: {
       ".woff2": "dataurl",
     },
     packages: "external",
   });
 };
-
-const filesinExcalidrawPackage = getFiles(
-  `${path.resolve(`${__dirname}/..`)}/packages/excalidraw`,
-);
-
-const filesToTransform = filesinExcalidrawPackage.filter((file) => {
-  return !(
-    file.includes("/__tests__/") ||
-    file.includes(".test.") ||
-    file.includes("/tests/") ||
-    file.includes("example")
-  );
-});
 
 createESMRawBuild();
 createESMBrowserBuild();
