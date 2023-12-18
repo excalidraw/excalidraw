93dfede513bfff479e4895d38c0b8d23987218f4
dwelle <5153846+dwelle@users.noreply.github.com>
2023-12-18 13:31:47 +0100

    feat: prevent unnecessary state updates

--- packages/excalidraw/utils.ts
+++ packages/excalidraw/utils.ts
@@ -769,6 +769,24 @@ export const queryFocusableElements = (container: HTMLElement | null) => {
     : [];
 };
 
+/** use as a fallback after identity check (for perf reasons) */
+const _defaultIsShallowComparatorFallback = (a: any, b: any): boolean => {
+  // consider two empty arrays equal
+  if (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === 0 &&
+    b.length === 0
+  ) {
+    return true;
+  }
+  return a === b;
+};
+
+/**
+ * Returns whether object/array is shallow equal.
+ * Considers empty object/arrays as equal (whether top-level or second-level).
+ */
 export const isShallowEqual = <
   T extends Record<string, any>,
   K extends readonly unknown[],
@@ -796,10 +814,12 @@ export const isShallowEqual = <
 
   if (comparators && Array.isArray(comparators)) {
     for (const key of comparators) {
-      const ret = objA[key] === objB[key];
+      const ret =
+        objA[key] === objB[key] ||
+        _defaultIsShallowComparatorFallback(objA[key], objB[key]);
       if (!ret) {
         if (debug) {
-          console.info(
+          console.warn(
             `%cisShallowEqual: ${key} not equal ->`,
             "color: #8B4000",
             objA[key],
@@ -816,20 +836,13 @@ export const isShallowEqual = <
     const comparator = (
       comparators as { [key in keyof T]?: (a: T[key], b: T[key]) => boolean }
     )?.[key as keyof T];
-    let ret = comparator
+    const ret = comparator
       ? comparator(objA[key], objB[key])
-      : objA[key] === objB[key];
-    // two empty arrays can be equal
-    if (
-      Array.isArray(objA[key]) &&
-      objA[key].length === 0 &&
-      Array.isArray(objB[key]) &&
-      objB[key].length === 0
-    ) {
-      ret = true;
-    }
+      : objA[key] === objB[key] ||
+        _defaultIsShallowComparatorFallback(objA[key], objB[key]);
+
     if (!ret && debug) {
-      console.info(
+      console.warn(
         `%cisShallowEqual: ${key} not equal ->`,
         "color: #8B4000",
         objA[key],
@@ -968,4 +981,14 @@ export const cloneJSON = <T>(obj: T): T => JSON.parse(JSON.stringify(obj));
 
 export const isFiniteNumber = (value: any): value is number => {
   return typeof value === "number" && Number.isFinite(value);
+};
+
+export const updateStable = <T extends any[] | Record<string, any>>(
+  prevValue: T,
+  nextValue: T,
+) => {
+  if (isShallowEqual(prevValue, nextValue)) {
+    return prevValue;
+  }
+  return nextValue;
 };
