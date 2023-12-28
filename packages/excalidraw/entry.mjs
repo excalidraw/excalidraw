5375b27850679425a0e20da0a6313e4bfedb6c10
Aakansha Doshi <aakansha1216@gmail.com>
2023-12-28 17:55:17 +0530

    update script

--- packages/excalidraw/entry.mjs
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV !== "development") {
-  await import("./dist/dev/index.js");
-} else {
-  await import("./dist/prod/index.js");
-}
--- packages/excalidraw/main.js
+++ packages/excalidraw/main.js
@@ -1,11 +1,5 @@
-if (process.env.IS_PREACT === "true") {
-  if (process.env.NODE_ENV === "production") {
-    module.exports = require("./dist/excalidraw-with-preact.production.min.js");
-  } else {
-    module.exports = require("./dist/excalidraw-with-preact.development.js");
-  }
-} else if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dist/excalidraw.production.min.js");
+if (process.env.NODE_ENV !== "development") {
+  await import("./dist/dev/index.js");
 } else {
-  module.exports = require("./dist/excalidraw.development.js");
+  await import("./dist/prod/index.js");
 }
