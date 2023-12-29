647e9c668b789c039d87bc39f91c5315eb2419a6
VasiliyMac <cartario@yandex.ru>
2023-12-30 01:25:01 +0400

    clean

--- excalidraw-app/index.tsx
+++ excalidraw-app/index.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ExcalidrawApp from "./App";
 import { registerSW } from "virtual:pwa-register";
-//hello from github
 import "../excalidraw-app/sentry";
 window.__EXCALIDRAW_SHA__ = import.meta.env.VITE_APP_GIT_SHA;
 const rootElement = document.getElementById("root")!;
