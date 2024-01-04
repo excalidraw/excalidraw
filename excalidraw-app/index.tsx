a6e845f5e3e9a58559190f3ab0b51d58d0602b61
Sebastian Amara <sebastian.amara@karat.com>
2024-01-04 13:10:10 -0800

    remove unusable export options, release 0.0.19

--- excalidraw-app/index.tsx
+++ excalidraw-app/index.tsx
@@ -689,30 +689,6 @@ const ExcalidrawWrapper = () => {
         UIOptions={{
           canvasActions: {
             toggleTheme: true,
-            export: {
-              onExportToBackend,
-              renderCustomUI: (elements, appState, files) => {
-                return (
-                  <ExportToExcalidrawPlus
-                    elements={elements}
-                    appState={appState}
-                    files={files}
-                    onError={(error) => {
-                      excalidrawAPI?.updateScene({
-                        appState: {
-                          errorMessage: error.message,
-                        },
-                      });
-                    }}
-                    onSuccess={() => {
-                      excalidrawAPI?.updateScene({
-                        appState: { openDialog: null },
-                      });
-                    }}
-                  />
-                );
-              },
-            },
           },
         }}
         langCode={langCode}
