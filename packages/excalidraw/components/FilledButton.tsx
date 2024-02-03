5ad35fe982809e0370d033193db603ac37be7ebd
dwelle <5153846+dwelle@users.noreply.github.com>
2024-02-03 14:40:52 +0100

    refactor: handle spinner layout via CSS instead of JS

--- packages/excalidraw/components/FilledButton.tsx
+++ packages/excalidraw/components/FilledButton.tsx
@@ -1,14 +1,10 @@
-import React, {
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-  useState,
-} from "react";
+import React, { forwardRef, useState } from "react";
 import clsx from "clsx";
 
 import "./FilledButton.scss";
 import { AbortError } from "../errors";
 import Spinner from "./Spinner";
+import { isPromiseLike } from "../utils";
 
 export type ButtonVariant = "filled" | "outlined" | "icon";
 export type ButtonColor = "primary" | "danger" | "warning" | "muted";
@@ -46,23 +42,11 @@ export const FilledButton = forwardRef<HTMLButtonElement, FilledButtonProps>(
   ) => {
     const [isLoading, setIsLoading] = useState(false);
 
-    const internalRef = useRef<HTMLButtonElement>(null);
-    const widthRef = useRef<number | undefined>(undefined);
-
-    useImperativeHandle(ref, () => internalRef.current!, []);
-
     const _onClick = async (event: React.MouseEvent) => {
       const ret = onClick?.(event);
 
-      if (ret && "then" in ret) {
+      if (isPromiseLike(ret)) {
         try {
-          if (internalRef.current) {
-            try {
-              widthRef.current =
-                parseFloat(getComputedStyle(internalRef.current).width) ||
-                undefined;
-            } catch {}
-          }
           setIsLoading(true);
           await ret;
         } catch (error: any) {
@@ -72,7 +56,6 @@ export const FilledButton = forwardRef<HTMLButtonElement, FilledButtonProps>(
             console.warn(error);
           }
         } finally {
-          widthRef.current = undefined;
           setIsLoading(false);
         }
       }
@@ -91,26 +74,18 @@ export const FilledButton = forwardRef<HTMLButtonElement, FilledButtonProps>(
         onClick={_onClick}
         type="button"
         aria-label={label}
-        ref={internalRef}
+        ref={ref}
         disabled={isLoading}
-        style={{
-          // too keep the same width when replacing the button content
-          // with a spinner
-          width: widthRef.current,
-        }}
       >
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <>
-            {icon && (
-              <div className="ExcButton__icon" aria-hidden>
-                {icon}
-              </div>
-            )}
-            {variant !== "icon" && (children ?? label)}
-          </>
-        )}
+        <div className="ExcButton__contents">
+          {isLoading && <Spinner />}
+          {icon && (
+            <div className="ExcButton__icon" aria-hidden>
+              {icon}
+            </div>
+          )}
+          {variant !== "icon" && (children ?? label)}
+        </div>
       </button>
     );
   },
