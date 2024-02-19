ee3b0c3a22bc2efbd335c129abd63b5b3130d274
Aakansha Doshi <aakansha1216@gmail.com>
2024-02-19 11:49:01 +0530

    fix: make LinearElementEditor independent of scene (#7670)
    * fix: make LinearElementEditor independent of scene
    
    * more fixes
    
    * pass elements and elementsMap to maybeBindBindableElement,getHoveredElementForBinding,bindingBorderTest,getElligibleElementsForBindableElementAndWhere,isLinearElementEligibleForNewBindingByBindable
    
    * replace `ElementsMap` with `NonDeletedSceneElementsMap` & remove unused params
    
    * fix lint
    
    ---------
    
    Co-authored-by: dwelle <5153846+dwelle@users.noreply.github.com>

--- packages/excalidraw/actions/actionFinalize.tsx
+++ packages/excalidraw/actions/actionFinalize.tsx
@@ -26,12 +26,12 @@ export const actionFinalize = register({
     _,
     { interactiveCanvas, focusContainer, scene },
   ) => {
-    const elementsMap = arrayToMap(elements);
+    const elementsMap = scene.getNonDeletedElementsMap();
 
     if (appState.editingLinearElement) {
       const { elementId, startBindingElement, endBindingElement } =
         appState.editingLinearElement;
-      const element = LinearElementEditor.getElement(elementId);
+      const element = LinearElementEditor.getElement(elementId, elementsMap);
 
       if (element) {
         if (isBindingElement(element)) {
@@ -191,7 +191,7 @@ export const actionFinalize = register({
         // To select the linear element when user has finished mutipoint editing
         selectedLinearElement:
           multiPointElement && isLinearElement(multiPointElement)
-            ? new LinearElementEditor(multiPointElement, scene)
+            ? new LinearElementEditor(multiPointElement)
             : appState.selectedLinearElement,
         pendingImageElementId: null,
       },
