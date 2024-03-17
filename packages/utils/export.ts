652ee2dbef83c5ac7de296b8cdde97a2c4bacdde
zsviczian <viczian.zsolt@gmail.com>
2024-03-17 19:31:33 +0100

    cleanup utils export after merge

--- packages/utils/export.ts
+++ packages/utils/export.ts
@@ -205,3 +205,16 @@ export const exportToClipboard = async (
     throw new Error("Invalid export type");
   }
 };
+
+export { getCommonBoundingBox } from "../excalidraw/element/bounds"; //zsviczian
+export { getMaximumGroups } from "../excalidraw/groups"; //zsviczian
+export { intersectElementWithLine } from "../excalidraw/element/collision"; //zsviczian
+export { determineFocusDistance } from "../excalidraw/element/collision"; //zsviczian
+export {
+  measureText,
+  wrapText,
+  getDefaultLineHeight,
+} from "../excalidraw/element/textElement"; //zsviczian
+export { getFontString } from "../excalidraw/utils"; //zsviczian
+export { getBoundTextMaxWidth } from "../excalidraw/element/textElement"; //zsviczian
+export { mermaidToExcalidraw } from "../excalidraw/components/TTDDialog/MermaidToExcalidraw"; //zsviczian
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,15 +1,3 @@
 export * from "./export";
 export * from "./withinBounds";
 export * from "./bbox";
-export { getCommonBoundingBox } from "../excalidraw/element/bounds"; //zsviczian
-export { getMaximumGroups } from "../excalidraw/groups"; //zsviczian
-export { intersectElementWithLine } from "../excalidraw/element/collision"; //zsviczian
-export { determineFocusDistance } from "../excalidraw/element/collision"; //zsviczian
-export {
-  measureText,
-  wrapText,
-  getDefaultLineHeight,
-} from "../excalidraw/element/textElement"; //zsviczian
-export { getFontString } from "../excalidraw/utils"; //zsviczian
-export { getBoundTextMaxWidth } from "../excalidraw/element/textElement"; //zsviczian
-export { mermaidToExcalidraw } from "../excalidraw/components/TTDDialog/MermaidToExcalidraw"; //zsviczian
