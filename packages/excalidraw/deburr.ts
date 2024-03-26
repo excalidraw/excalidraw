f47d7d2e28539004389dd01e085ed96b6c4ea334
dwelle <5153846+dwelle@users.noreply.github.com>
2024-03-26 23:21:27 +0100

    always deburr to single letters

--- packages/excalidraw/deburr.ts
+++ packages/excalidraw/deburr.ts
@@ -12,9 +12,17 @@ const reComboMark = RegExp(rsCombo, "g");
 
 const reLatin = /[\xc0-\xd6\xd8-\xf6\xf8-\xff\u0100-\u017f]/g;
 
+// NOTE below letter replacements are modified from lodash to always convert
+// to single-letter form by phonetic similarity to keep indexing identical.
+// Doing this is only useful for search highlighting, and only insofar
+// we use a library that can highlight the original source string using
+// the matching indices. As such, we'll likely need to write our own highlighter
+// anyway. Ultimately, we'll want to write our own matcher altogether
+// so we don't have to do any deburring, which will be the most correct
+// solution.
+//
 // prettier-ignore
 const deburredLetters = {
-    // Latin-1 Supplement block.
     '\xc0': 'A',  '\xc1': 'A', '\xc2': 'A', '\xc3': 'A', '\xc4': 'A', '\xc5': 'A',
     '\xe0': 'a',  '\xe1': 'a', '\xe2': 'a', '\xe3': 'a', '\xe4': 'a', '\xe5': 'a',
     '\xc7': 'C',  '\xe7': 'c',
@@ -29,10 +37,12 @@ const deburredLetters = {
     '\xd9': 'U',  '\xda': 'U', '\xdb': 'U', '\xdc': 'U',
     '\xf9': 'u',  '\xfa': 'u', '\xfb': 'u', '\xfc': 'u',
     '\xdd': 'Y',  '\xfd': 'y', '\xff': 'y',
-    '\xc6': 'Ae', '\xe6': 'ae',
-    '\xde': 'Th', '\xfe': 'th',
-    '\xdf': 'ss',
-    // Latin Extended-A block.
+    // normaly Ae/ae
+    '\xc6': 'E', '\xe6': 'e',
+    // normally Th/th
+    '\xde': 'T', '\xfe': 't',
+    // normally ss
+    '\xdf': 's',
     '\u0100': 'A',  '\u0102': 'A', '\u0104': 'A',
     '\u0101': 'a',  '\u0103': 'a', '\u0105': 'a',
     '\u0106': 'C',  '\u0108': 'C', '\u010a': 'C', '\u010c': 'C',
@@ -65,9 +75,13 @@ const deburredLetters = {
     '\u0176': 'Y',  '\u0177': 'y', '\u0178': 'Y',
     '\u0179': 'Z',  '\u017b': 'Z', '\u017d': 'Z',
     '\u017a': 'z',  '\u017c': 'z', '\u017e': 'z',
-    '\u0132': 'IJ', '\u0133': 'ij',
-    '\u0152': 'Oe', '\u0153': 'oe',
-    '\u0149': "'n", '\u017f': 's'
+    // normally IJ/ij
+    '\u0132': 'I', '\u0133': 'i',
+    // normally OE/oe
+    '\u0152': 'E', '\u0153': 'e',
+    // normally "'n"
+    '\u0149': "n",
+    '\u017f': 's'
   };
 
 export const deburr = (str: string) => {
