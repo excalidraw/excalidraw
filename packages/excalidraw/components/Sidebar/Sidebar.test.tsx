1f809a0070f4f5cbcc5498e51ba9c58b7f5a18df
dwelle <5153846+dwelle@users.noreply.github.com>
2024-03-28 14:36:27 +0100

    fix lint

--- packages/excalidraw/components/Sidebar/Sidebar.test.tsx
+++ packages/excalidraw/components/Sidebar/Sidebar.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { DEFAULT_SIDEBAR } from "../../constants";
-import { DefaultSidebar, Excalidraw, Sidebar } from "../../index";
+import { Excalidraw, Sidebar } from "../../index";
 import {
   fireEvent,
   GlobalTestState,
