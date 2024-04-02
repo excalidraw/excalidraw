d6d31cc530a281eefb43b166e489ef71247566a9
dliang <humga@icloud.com>
2024-04-03 00:11:51 +0800

    add tauri package app

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,8 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
