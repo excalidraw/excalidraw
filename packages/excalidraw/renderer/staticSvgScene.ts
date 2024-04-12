fc5bddc17e6268f0587bcf008f710154e9c34ba0
David Luzar <5153846+dwelle@users.noreply.github.com>
2024-04-12 21:50:02 +0200

    fix: always make sure we render bound text above containers (#7880)

--- packages/excalidraw/renderer/staticSvgScene.ts
+++ packages/excalidraw/renderer/staticSvgScene.ts
@@ -618,6 +618,15 @@ export const renderSceneToSvg = (
     .filter((el) => !isIframeLikeElement(el))
     .forEach((element) => {
       if (!element.isDeleted) {
+        if (
+          isTextElement(element) &&
+          element.containerId &&
+          elementsMap.has(element.containerId)
+        ) {
+          // will be rendered with the container
+          return;
+        }
+
         try {
           renderElementToSvg(
             element,
@@ -629,6 +638,20 @@ export const renderSceneToSvg = (
             element.y + renderConfig.offsetY,
             renderConfig,
           );
+
+          const boundTextElement = getBoundTextElement(element, elementsMap);
+          if (boundTextElement) {
+            renderElementToSvg(
+              boundTextElement,
+              elementsMap,
+              rsvg,
+              svgRoot,
+              files,
+              boundTextElement.x + renderConfig.offsetX,
+              boundTextElement.y + renderConfig.offsetY,
+              renderConfig,
+            );
+          }
         } catch (error: any) {
           console.error(error);
         }
