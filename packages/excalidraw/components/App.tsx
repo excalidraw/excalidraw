4689a6b300d8756b54fdbaa7a3f1f8db60b9cc78
Ryan Di <ryan.weihao.di@gmail.com>
2024-04-12 18:58:51 +0800

    fix: hit test for closed sharp curves (#7881)

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -4383,6 +4383,7 @@ class App extends React.Component<AppProps, AppState> {
 
         return shouldTestInside(element)
           ? getClosedCurveShape(
+              element,
               roughShape,
               [element.x, element.y],
               element.angle,
