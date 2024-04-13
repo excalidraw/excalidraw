03ec3f629234cac04d76760bd3ed38991481028b
David Luzar <5153846+dwelle@users.noreply.github.com>
2024-04-13 19:12:29 +0200

    feat: add "toggle grid" to command palette (#7887)

--- packages/excalidraw/components/icons.tsx
+++ packages/excalidraw/components/icons.tsx
@@ -2116,3 +2116,16 @@ export const youtubeIcon = createIcon(
   </g>,
   tablerIconProps,
 );
+
+export const gridIcon = createIcon(
+  <g strokeWidth={1.5}>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 6h18" />
+    <path d="M3 12h18" />
+    <path d="M3 18h18" />
+    <path d="M6 3v18" />
+    <path d="M12 3v18" />
+    <path d="M18 3v18" />
+  </g>,
+  tablerIconProps,
+);
