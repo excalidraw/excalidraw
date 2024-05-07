10e2b005c7c03dbfeb89de0a7982bf327a600583
dwelle <5153846+dwelle@users.noreply.github.com>
2024-05-07 13:02:46 +0200

    feat: change signin/signup icon

--- packages/excalidraw/components/icons.tsx
+++ packages/excalidraw/components/icons.tsx
@@ -2096,14 +2096,13 @@ export const DeviceDesktopIcon = createIcon(
   { ...tablerIconProps, strokeWidth: 1.5 },
 );
 
-// arrow-bar-to-left
-export const arrowBarToLeftIcon = createIcon(
-  <g>
+// login
+export const loginIcon = createIcon(
+  <g strokeWidth={1.5}>
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M10 12l10 0" />
-    <path d="M10 12l4 4" />
-    <path d="M10 12l4 -4" />
-    <path d="M4 4l0 16" />
+    <path d="M15 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+    <path d="M21 12h-13l3 -3" />
+    <path d="M11 15l-3 -3" />
   </g>,
   tablerIconProps,
 );
