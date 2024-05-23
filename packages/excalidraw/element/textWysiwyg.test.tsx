df2c9d8017299d0080c7402985379245b3f49205
Marcel Mraz <marcel@excalidraw.com>
2024-05-23 10:51:01 +0100

    fix: flaky snapshot tests with floating point precision issues (#8049)

--- packages/excalidraw/element/textWysiwyg.test.tsx
+++ packages/excalidraw/element/textWysiwyg.test.tsx
@@ -1064,7 +1064,7 @@ describe("textWysiwyg", () => {
       expect([h.elements[1].x, h.elements[1].y]).toMatchInlineSnapshot(`
         [
           85,
-          4.999999999999986,
+          "5.00000",
         ]
       `);
 
@@ -1109,8 +1109,8 @@ describe("textWysiwyg", () => {
       UI.resize(rectangle, "ne", [rectangle.x + 100, rectangle.y - 100]);
       expect([h.elements[1].x, h.elements[1].y]).toMatchInlineSnapshot(`
         [
-          374.99999999999994,
-          -535.0000000000001,
+          "375.00000",
+          "-535.00000",
         ]
       `);
     });
