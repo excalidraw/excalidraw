8f0b87bea8ad125224cce036b788155cb38051cd
dliang <humga@icloud.com>
2024-06-12 02:37:44 +0800

    load file directly

--- src/components/main-menu/DefaultItems.tsx
+++ src/components/main-menu/DefaultItems.tsx
@@ -3,7 +3,7 @@ import { useI18n } from "../../i18n";
 import {
   useExcalidrawSetAppState,
   useExcalidrawActionManager,
-  useExcalidrawElements,
+  // useExcalidrawElements,
   useAppProps,
 } from "../App";
 import {
@@ -34,36 +34,36 @@ import { useSetAtom } from "jotai";
 import { activeConfirmDialogAtom } from "../ActiveConfirmDialog";
 import { jotaiScope } from "../../jotai";
 import { useUIAppState } from "../../context/ui-appState";
-import { openConfirmModal } from "../OverwriteConfirm/OverwriteConfirmState";
-import Trans from "../Trans";
+// import { openConfirmModal } from "../OverwriteConfirm/OverwriteConfirmState";
+// import Trans from "../Trans";
 
 export const LoadScene = () => {
   const { t } = useI18n();
   const actionManager = useExcalidrawActionManager();
-  const elements = useExcalidrawElements();
+  // const elements = useExcalidrawElements();
 
   if (!actionManager.isActionEnabled(actionLoadScene)) {
     return null;
   }
 
   const handleSelect = async () => {
-    if (
-      !elements.length ||
-      (await openConfirmModal({
-        title: t("overwriteConfirm.modal.loadFromFile.title"),
-        actionLabel: t("overwriteConfirm.modal.loadFromFile.button"),
-        color: "warning",
-        description: (
-          <Trans
-            i18nKey="overwriteConfirm.modal.loadFromFile.description"
-            bold={(text) => <strong>{text}</strong>}
-            br={() => <br />}
-          />
-        ),
-      }))
-    ) {
-      actionManager.executeAction(actionLoadScene);
-    }
+    // if (
+    //   !elements.length ||
+    //   (await openConfirmModal({
+    //     title: t("overwriteConfirm.modal.loadFromFile.title"),
+    //     actionLabel: t("overwriteConfirm.modal.loadFromFile.button"),
+    //     color: "warning",
+    //     description: (
+    //       <Trans
+    //         i18nKey="overwriteConfirm.modal.loadFromFile.description"
+    //         bold={(text) => <strong>{text}</strong>}
+    //         br={() => <br />}
+    //       />
+    //     ),
+    //   }))
+    // ) {
+    actionManager.executeAction(actionLoadScene);
+    // }
   };
 
   return (
