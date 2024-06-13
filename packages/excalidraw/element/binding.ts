16c0417f5b69ba91a46682510ee7acabe663033d
zsviczian <viczian.zsolt@gmail.com>
2024-06-13 18:50:51 +0200

    Fix - binding to nonexistent element

--- packages/excalidraw/element/binding.ts
+++ packages/excalidraw/element/binding.ts
@@ -828,6 +828,9 @@ const bindingBorderTest = (
   { x, y }: { x: number; y: number },
   app: AppClassProperties,
 ): boolean => {
+  if(!element || !element.width || !element.height) {
+    return false;
+  }
   const threshold = maxBindingGap(element, element.width, element.height);
   const shape = app.getElementShape(element);
   return isPointOnShape([x, y], shape, threshold);
