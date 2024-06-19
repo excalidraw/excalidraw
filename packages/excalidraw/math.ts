a75e2572f8132c5d8852972705ea9748bd9ffd09
RussianMoroccan <y.baassine@student.vu.nl>
2024-06-19 13:14:55 +0200

    Wrote coverage tool for orientation and intersect

--- packages/excalidraw/math.ts
+++ packages/excalidraw/math.ts
@@ -13,6 +13,7 @@ import type {
 import { getCurvePathOps } from "./element/bounds";
 import type { Mutable } from "./utility-types";
 import { ShapeCache } from "./scene/ShapeCache";
+import { logCoverage } from './tests/coverageTool';
 
 export const rotate = (
   // target point to rotate
@@ -221,45 +222,51 @@ export const isPointWithinBounds = (p: Point, q: Point, r: Point) => {
 // 0 if p, q, r are colinear
 // 1 if Clockwise
 // 2 if counterclickwise
-const orderedColinearOrientation = (p: Point, q: Point, r: Point) => {
+export const orderedColinearOrientation = (p: Point, q: Point, r: Point) => {
   const val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]);
+
   if (val === 0) {
+    logCoverage('orderedColinearOrientation', 'colinearBranch')
     return 0;
   }
-  return val > 0 ? 1 : 2;
+  return val > 0 ? 
+  (logCoverage('orderedColinearOrientation', 'clockwiseBranch'), 1) :
+  (logCoverage('orderedColinearOrientation', 'counterclockwiseBranch'), 2);
 };
 
 // Check is p1q1 intersects with p2q2
-const doSegmentsIntersect = (p1: Point, q1: Point, p2: Point, q2: Point) => {
+export const doSegmentsIntersect = (p1: Point, q1: Point, p2: Point, q2: Point) => {
   const o1 = orderedColinearOrientation(p1, q1, p2);
   const o2 = orderedColinearOrientation(p1, q1, q2);
   const o3 = orderedColinearOrientation(p2, q2, p1);
   const o4 = orderedColinearOrientation(p2, q2, q1);
 
   if (o1 !== o2 && o3 !== o4) {
+    logCoverage('doSegmentsIntersect', 'generalCaseBranch');
     return true;
   }
 
-  // p1, q1 and p2 are colinear and p2 lies on segment p1q1
   if (o1 === 0 && isPointWithinBounds(p1, p2, q1)) {
+    logCoverage('doSegmentsIntersect', 'specCase1Branch');
     return true;
   }
 
-  // p1, q1 and p2 are colinear and q2 lies on segment p1q1
   if (o2 === 0 && isPointWithinBounds(p1, q2, q1)) {
+    logCoverage('doSegmentsIntersect', 'specCase2Branch');
     return true;
   }
 
-  // p2, q2 and p1 are colinear and p1 lies on segment p2q2
   if (o3 === 0 && isPointWithinBounds(p2, p1, q2)) {
+    logCoverage('doSegmentsIntersect', 'specCase3Branch');
     return true;
   }
 
-  // p2, q2 and q1 are colinear and q1 lies on segment p2q2
   if (o4 === 0 && isPointWithinBounds(p2, q1, q2)) {
+    logCoverage('doSegmentsIntersect', 'specCase4Branch');
     return true;
   }
 
+  logCoverage('doSegmentsIntersect', 'notIntersectingBranch');
   return false;
 };
 
