611d8a68c7b84eccba029a3787b129fe792d476b
Marcel Mraz <marcel@excalidraw.com>
2024-07-19 19:06:31 +0200

    Allow shift + f shortcut on text tool selection

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -4126,6 +4126,7 @@ class App extends React.Component<AppProps, AppState> {
         }
 
         if (
+          this.state.activeTool.type === "text" ||
           selectedElements.find(
             (element) =>
               isTextElement(element) ||
