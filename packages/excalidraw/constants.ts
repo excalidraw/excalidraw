e76191f87ed37eedf8206b2a88f50d5572bbe2cc
Marcel Mraz <marcel@excalidraw.com>
2024-07-24 16:10:27 +0200

    Update comment about leaving 4 unset

--- packages/excalidraw/constants.ts
+++ packages/excalidraw/constants.ts
@@ -126,7 +126,7 @@ export const FONT_FAMILY = {
   Virgil: 1,
   Helvetica: 2,
   Cascadia: 3,
-  // leave 4 unused for now
+  // leave 4 unused as it was historically used for Assistant (which we don't use anymore) or custom font (Obsidian)
   Excalifont: 5,
   Nunito: 6,
   "Lilita One": 7,
