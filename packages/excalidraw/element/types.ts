504dbbf75e85763126a8fa1cd2ade5ebeb0b475c
Mark Tolmacs <mark@lazycat.hu>
2024-07-31 18:33:11 +0200

    fixedPoint documentation
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- packages/excalidraw/element/types.ts
+++ packages/excalidraw/element/types.ts
@@ -232,6 +232,11 @@ export type PointBinding = {
   elementId: ExcalidrawBindableElement["id"];
   focus: number;
   gap: number;
+  // Represents the fixed point binding information in form of a vertical and
+  // horizontal ratio (i.e. a percentage value in the 0.0-1.0 range). This ratio
+  // gives the user selected fixed point by multiplying the bound element width
+  // with fixedPoint[0] and the bound element height with fixedPoint[1] to get the
+  // bound element-local point coordinate.
   fixedPoint: [number, number];
 };
 
