57ea1ec277c424bafed6167474e45df1afe19ebf
lzl0304 <develop0304@outlook.com>
2024-08-06 17:50:22 +0800

    fix: record history when pasting image

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -3006,6 +3006,7 @@ class App extends React.Component<AppProps, AppState> {
         const imageElement = this.createImageElement({ sceneX, sceneY });
         this.insertImageElement(imageElement, file);
         this.initializeImageDimensions(imageElement);
+        this.store.shouldCaptureIncrement();
         this.setState({
           selectedElementIds: makeNextSelectedElementIds(
             {
