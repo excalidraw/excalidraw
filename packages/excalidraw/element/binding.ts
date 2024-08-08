25c023ef8d1dead014bc47c6b8e68a592391a70d
dwelle <5153846+dwelle@users.noreply.github.com>
2024-08-08 14:33:23 +0200

    perf: remove unnecessary alloc on hot path

--- packages/excalidraw/element/binding.ts
+++ packages/excalidraw/element/binding.ts
@@ -2233,13 +2233,12 @@ export const getArrowLocalFixedPoints = (
 export const normalizeFixedPoint = <T extends FixedPoint | null>(
   fixedPoint: T,
 ): T extends null ? null : FixedPoint => {
-  return (
-    fixedPoint
-      ? fixedPoint.map((ratio) =>
-          // Do not allow a precise 0.5 for fixed point ratio
-          // to avoid jumping arrow heading due to floating point imprecision
-          ratio === 0.5 ? 0.5001 : ratio,
-        )
-      : null
-  ) as T extends null ? null : FixedPoint;
+  // Do not allow a precise 0.5 for fixed point ratio
+  // to avoid jumping arrow heading due to floating point imprecision
+  if (fixedPoint && (fixedPoint[0] === 0.5 || fixedPoint[1] === 0.5)) {
+    return fixedPoint.map((ratio) =>
+      ratio === 0.5 ? 0.5001 : ratio,
+    ) as T extends null ? null : FixedPoint;
+  }
+  return fixedPoint as any as T extends null ? null : FixedPoint;
 };
