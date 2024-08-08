dd1370381df80fa00a612bcacdaada9c15ab7ecf
Márk Tolmács <mark@lazycat.hu>
2024-08-08 14:06:26 +0200

    chore: Refactor and remove scene from elbow arrow generation (#8342)
    * Refactor and remove scene from elbow arrow generation

--- packages/excalidraw/actions/actionHistory.tsx
+++ packages/excalidraw/actions/actionHistory.tsx
@@ -58,7 +58,6 @@ export const createUndoAction: ActionCreator = (history, store) => ({
         arrayToMap(elements) as SceneElementsMap, // TODO: #7348 refactor action manager to already include `SceneElementsMap`
         appState,
         store.snapshot,
-        app.scene,
       ),
     ),
   keyTest: (event) =>
@@ -100,7 +99,6 @@ export const createRedoAction: ActionCreator = (history, store) => ({
         arrayToMap(elements) as SceneElementsMap, // TODO: #7348 refactor action manager to already include `SceneElementsMap`
         appState,
         store.snapshot,
-        app.scene,
       ),
     ),
   keyTest: (event) =>
