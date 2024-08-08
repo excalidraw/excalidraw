0c8fa91ee90e5d40fc3c12c0a59d46f85554b9e0
Márk Tolmács <mark@lazycat.hu>
2024-08-08 14:06:26 +0200

    chore: Refactor and remove scene from elbow arrow generation (#8342)
    * Refactor and remove scene from elbow arrow generation

--- packages/excalidraw/actions/actionFlip.ts
+++ packages/excalidraw/actions/actionFlip.ts
@@ -120,7 +120,6 @@ const flipElements = (
     true,
     flipDirection === "horizontal" ? maxX : minX,
     flipDirection === "horizontal" ? minY : maxY,
-    app.scene,
   );
 
   bindOrUnbindLinearElements(
