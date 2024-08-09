91dbbfd677875379d219d20c95b2deead7ce139e
Clarence Chan <69307556+clarencechaan@users.noreply.github.com>
2024-08-09 16:20:36 -0400

    fix: text content with tab characters act different in view/edit (#8336)
    Co-authored-by: dwelle <5153846+dwelle@users.noreply.github.com>

--- packages/excalidraw/element/textWysiwyg.tsx
+++ packages/excalidraw/element/textWysiwyg.tsx
@@ -357,7 +357,16 @@ export const textWysiwyg = ({
     };
 
     editable.oninput = () => {
-      onChange(normalizeText(editable.value));
+      const normalized = normalizeText(editable.value);
+      if (editable.value !== normalized) {
+        const selectionStart = editable.selectionStart;
+        editable.value = normalized;
+        // put the cursor at some position close to where it was before
+        // normalization (otherwise it'll end up at the end of the text)
+        editable.selectionStart = selectionStart;
+        editable.selectionEnd = selectionStart;
+      }
+      onChange(editable.value);
     };
   }
 
