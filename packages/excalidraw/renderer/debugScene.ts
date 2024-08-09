9cbfa54f876cf2d1246854f7e5998a54a2f8c340
Mark Tolmacs <mark@lazycat.hu>
2024-08-09 15:21:28 +0200

    Basic shapes and line debug, transient mode

--- packages/excalidraw/renderer/debugScene.ts
+++ packages/excalidraw/renderer/debugScene.ts
@@ -1,4 +1,5 @@
 import type { LineSegment } from "../../utils";
+import type { BoundingBox, Bounds } from "../element/bounds";
 import type { AppState, Point } from "../types";
 import { throttleRAF } from "../utils";
 import { bootstrapCanvas, getNormalizedCanvasDimensions } from "./helpers";
@@ -8,29 +9,125 @@ let debugData: DebugElement[][] = [];
 type DebugElement = {
   color: string;
   data: LineSegment;
+  permanent: boolean;
 };
 
-export const debugDrawLine = (segment: LineSegment, color: string = "red") => {
-  addToCurrentFrame({
-    color,
-    data: segment,
-  });
+export const debugDrawLine = (
+  segment: LineSegment | LineSegment[],
+  opts?: {
+    color?: string;
+    permanent?: boolean;
+  },
+) => {
+  (isLineSegment(segment) ? [segment] : segment).forEach((data) =>
+    addToCurrentFrame({
+      color: opts?.color ?? "red",
+      data,
+      permanent: !!opts?.permanent,
+    }),
+  );
 };
 
-export const debugDrawPoint = (point: Point, color: string = "cyan") => {
+export const debugDrawPoint = (
+  point: Point,
+  opts?: {
+    color?: string;
+    permanent?: boolean;
+    fuzzy?: boolean;
+  },
+) => {
+  const xOffset = opts?.fuzzy ? Math.random() * 3 : 0;
+  const yOffset = opts?.fuzzy ? Math.random() * 3 : 0;
+
   debugDrawLine(
     [
-      [point[0] - 10, point[1] - 10],
-      [point[0] + 10, point[1] + 10],
+      [point[0] + xOffset - 10, point[1] + yOffset - 10],
+      [point[0] + xOffset + 10, point[1] + yOffset + 10],
     ],
-    color,
+    {
+      color: opts?.color ?? "cyan",
+      permanent: opts?.permanent,
+    },
   );
   debugDrawLine(
     [
-      [point[0] - 10, point[1] + 10],
-      [point[0] + 10, point[1] - 10],
+      [point[0] + xOffset - 10, point[1] + yOffset + 10],
+      [point[0] + xOffset + 10, point[1] + yOffset - 10],
     ],
-    color,
+    {
+      color: opts?.color ?? "cyan",
+      permanent: opts?.permanent,
+    },
+  );
+};
+
+export const debugDrawBoundingBox = (
+  box: BoundingBox | BoundingBox[],
+  opts?: {
+    color?: string;
+    permanent?: boolean;
+  },
+) => {
+  (Array.isArray(box) ? box : [box]).forEach((bbox) =>
+    debugDrawLine(
+      [
+        [
+          [bbox.minX, bbox.minY],
+          [bbox.maxX, bbox.minY],
+        ],
+        [
+          [bbox.maxX, bbox.minY],
+          [bbox.maxX, bbox.maxY],
+        ],
+        [
+          [bbox.maxX, bbox.maxY],
+          [bbox.minX, bbox.maxY],
+        ],
+        [
+          [bbox.minX, bbox.maxY],
+          [bbox.minX, bbox.minY],
+        ],
+      ],
+      {
+        color: opts?.color ?? "cyan",
+        permanent: opts?.permanent,
+      },
+    ),
+  );
+};
+
+export const debugDrawBounds = (
+  box: Bounds | Bounds[],
+  opts?: {
+    color: string;
+    permanent: boolean;
+  },
+) => {
+  (isBounds(box) ? [box] : box).forEach((bbox) =>
+    debugDrawLine(
+      [
+        [
+          [bbox[0], bbox[1]],
+          [bbox[2], bbox[1]],
+        ],
+        [
+          [bbox[2], bbox[1]],
+          [bbox[2], bbox[3]],
+        ],
+        [
+          [bbox[2], bbox[3]],
+          [bbox[0], bbox[3]],
+        ],
+        [
+          [bbox[0], bbox[3]],
+          [bbox[0], bbox[1]],
+        ],
+      ],
+      {
+        color: opts?.color ?? "green",
+        permanent: opts?.permanent,
+      },
+    ),
   );
 };
 
@@ -58,6 +155,14 @@ const isLineSegment = (segment: any): segment is LineSegment =>
   isPoint(segment[0]) &&
   isPoint(segment[0]);
 
+const isBounds = (box: any): box is Bounds =>
+  Array.isArray(box) &&
+  box.length === 4 &&
+  typeof box[0] === "number" &&
+  typeof box[1] === "number" &&
+  typeof box[2] === "number" &&
+  typeof box[3] === "number";
+
 const addToCurrentFrame = (element: DebugElement) => {
   if (debugData.length === 0) {
     debugData[0] = [];
@@ -71,21 +176,25 @@ const renderLine = (
   segment: LineSegment,
   color: string,
 ) => {
+  context.save();
   context.strokeStyle = color;
   context.beginPath();
   context.moveTo(segment[0][0], segment[0][1]);
-  context.lineTo(segment[1][0], segment[1][0]);
+  context.lineTo(segment[1][0], segment[1][1]);
   context.stroke();
+  context.restore();
 };
 
 const renderOrigin = (context: CanvasRenderingContext2D, zoom: number) => {
   context.strokeStyle = "#888";
+  context.save();
   context.beginPath();
   context.moveTo(-10 * zoom, -10 * zoom);
   context.lineTo(10 * zoom, 10 * zoom);
   context.moveTo(10 * zoom, -10 * zoom);
   context.lineTo(-10 * zoom, 10 * zoom);
   context.stroke();
+  context.save();
 };
 
 const _debugRenderer = (
@@ -124,4 +233,6 @@ const _debugRenderer = (
       }
     });
   });
+
+  debugData = debugData.map((frame) => frame.filter((el) => el.permanent));
 };
