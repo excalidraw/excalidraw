3ddb7efb192e0594d8ab89c57e357fcbacc68026
David Luzar <5153846+dwelle@users.noreply.github.com>
2024-08-20 18:06:22 +0200

    feat: rewrite d2c to not require token (#8269)

--- packages/excalidraw/components/dropdownMenu/DropdownMenuItemLink.tsx
+++ packages/excalidraw/components/dropdownMenu/DropdownMenuItemLink.tsx
@@ -13,6 +13,7 @@ const DropdownMenuItemLink = ({
   onSelect,
   className = "",
   selected,
+  rel = "noreferrer",
   ...rest
 }: {
   href: string;
@@ -22,6 +23,7 @@ const DropdownMenuItemLink = ({
   className?: string;
   selected?: boolean;
   onSelect?: (event: Event) => void;
+  rel?: string;
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
   const handleClick = useHandleDropdownMenuItemClick(rest.onClick, onSelect);
 
