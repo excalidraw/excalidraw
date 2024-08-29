4cd46c033960cbd7d8dcace9648550f89d9d04d3
Marcel Mraz <marcel@excalidraw.com>
2024-08-29 20:04:26 +0200

    Explicitly reject async promise executors

--- packages/excalidraw/fonts/wasm/hb-subset.loader.ts
+++ packages/excalidraw/fonts/wasm/hb-subset.loader.ts
@@ -10,30 +10,37 @@ const load = (): Promise<{
     codePoints: ReadonlySet<number>,
   ) => Uint8Array;
 }> => {
-  return new Promise(async (resolve) => {
-    const [binary, bindings] = await Promise.all([
-      import("./hb-subset.wasm"),
-      import("./hb-subset.bindings"),
-    ]);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const [binary, bindings] = await Promise.all([
+        import("./hb-subset.wasm"),
+        import("./hb-subset.bindings"),
+      ]);
 
-    WebAssembly.instantiate(binary.default).then((module) => {
-      const harfbuzzJsWasm = module.instance.exports;
-      // @ts-expect-error since `.buffer` is custom prop
-      const heapu8 = new Uint8Array(harfbuzzJsWasm.memory.buffer);
+      WebAssembly.instantiate(binary.default).then((module) => {
+        const harfbuzzJsWasm = module.instance.exports;
+        // @ts-expect-error since `.buffer` is custom prop
+        const heapu8 = new Uint8Array(harfbuzzJsWasm.memory.buffer);
 
-      const hbSubset = {
-        subset: (fontBuffer: ArrayBuffer, codePoints: ReadonlySet<number>) => {
-          return bindings.default.subset(
-            harfbuzzJsWasm,
-            heapu8,
-            fontBuffer,
-            codePoints,
-          );
-        },
-      };
+        const hbSubset = {
+          subset: (
+            fontBuffer: ArrayBuffer,
+            codePoints: ReadonlySet<number>,
+          ) => {
+            return bindings.default.subset(
+              harfbuzzJsWasm,
+              heapu8,
+              fontBuffer,
+              codePoints,
+            );
+          },
+        };
 
-      resolve(hbSubset);
-    });
+        resolve(hbSubset);
+      });
+    } catch (error) {
+      reject(error);
+    }
   });
 };
 
--- packages/excalidraw/fonts/wasm/woff2.loader.ts
+++ packages/excalidraw/fonts/wasm/woff2.loader.ts
@@ -10,45 +10,49 @@ const load = (): Promise<{
   compress: (buffer: ArrayBuffer) => Uint8Array;
   decompress: (buffer: ArrayBuffer) => Uint8Array;
 }> => {
-  return new Promise(async (resolve) => {
-    const [binary, bindings] = await Promise.all([
-      import("./woff2.wasm"),
-      import("./woff2.bindings"),
-    ]);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const [binary, bindings] = await Promise.all([
+        import("./woff2.wasm"),
+        import("./woff2.bindings"),
+      ]);
 
-    // initializing the module manually, so that we could pass in the wasm binary
-    bindings
-      .default({ wasmBinary: binary.default })
-      .then(
-        (module: {
-          woff2Enc: (buffer: ArrayBuffer, byteLength: number) => Vector;
-          woff2Dec: (buffer: ArrayBuffer, byteLength: number) => Vector;
-        }) => {
-          // re-map from internal vector into byte array
-          function convertFromVecToUint8Array(vector: Vector): Uint8Array {
-            const arr = [];
-            for (let i = 0, l = vector.size(); i < l; i++) {
-              arr.push(vector.get(i));
+      // initializing the module manually, so that we could pass in the wasm binary
+      bindings
+        .default({ wasmBinary: binary.default })
+        .then(
+          (module: {
+            woff2Enc: (buffer: ArrayBuffer, byteLength: number) => Vector;
+            woff2Dec: (buffer: ArrayBuffer, byteLength: number) => Vector;
+          }) => {
+            // re-map from internal vector into byte array
+            function convertFromVecToUint8Array(vector: Vector): Uint8Array {
+              const arr = [];
+              for (let i = 0, l = vector.size(); i < l; i++) {
+                arr.push(vector.get(i));
+              }
+
+              return new Uint8Array(arr);
             }
 
-            return new Uint8Array(arr);
-          }
+            // re-exporting only compress and decompress functions (also avoids infinite loop inside emscripten bindings)
+            const woff2 = {
+              compress: (buffer: ArrayBuffer) =>
+                convertFromVecToUint8Array(
+                  module.woff2Enc(buffer, buffer.byteLength),
+                ),
+              decompress: (buffer: ArrayBuffer) =>
+                convertFromVecToUint8Array(
+                  module.woff2Dec(buffer, buffer.byteLength),
+                ),
+            };
 
-          // re-exporting only compress and decompress functions (also avoids infinite loop inside emscripten bindings)
-          const woff2 = {
-            compress: (buffer: ArrayBuffer) =>
-              convertFromVecToUint8Array(
-                module.woff2Enc(buffer, buffer.byteLength),
-              ),
-            decompress: (buffer: ArrayBuffer) =>
-              convertFromVecToUint8Array(
-                module.woff2Dec(buffer, buffer.byteLength),
-              ),
-          };
-
-          resolve(woff2);
-        },
-      );
+            resolve(woff2);
+          },
+        );
+    } catch (e) {
+      reject(e);
+    }
   });
 };
 
