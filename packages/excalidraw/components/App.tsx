daa787d03c8bd53cae038bf9230010627118efac
zsviczian <viczian.zsolt@gmail.com>
2024-08-31 09:00:19 +0200

    Update App.tsx

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -2515,7 +2515,7 @@ class App extends React.Component<AppProps, AppState> {
       addEventListener(
         this.excalidrawContainerRef.current,
         EVENT.WHEEL,
-        this.onWheel,
+        this.handleWheel,
         { passive: false },
       ),
       addEventListener(window, EVENT.MESSAGE, this.onWindowMessage, false),
@@ -4322,13 +4322,6 @@ class App extends React.Component<AppProps, AppState> {
       // -----------------------------------------------------------------------
     },
   );
-
-  private onWheel = withBatchedUpdates((event: WheelEvent) => {
-    // prevent browser pinch zoom on DOM elements
-    if (!(event.target instanceof HTMLCanvasElement) && event.ctrlKey) {
-      event.preventDefault();
-    }
-  });
 
   private onKeyUp = withBatchedUpdates((event: KeyboardEvent) => {
     if (event.key === KEYS.SPACE) {
