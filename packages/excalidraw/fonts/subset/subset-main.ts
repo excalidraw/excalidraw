485b5b2747289e072bae2c1e9a75f427002e10db
Marcel Mraz <marcel@excalidraw.com>
2024-09-25 16:43:04 +0200

    Add diagram link

--- packages/excalidraw/fonts/subset/subset-main.ts
+++ packages/excalidraw/fonts/subset/subset-main.ts
@@ -12,6 +12,8 @@ let shouldUseWorkers = typeof Worker !== "undefined";
  * Tries to subset glyphs in a font based on the used codepoints, returning the font as dataurl.
  * Under the hood utilizes worker threads (Web Workers, if available), otherwise fallbacks to the main thread.
  *
+ * Check the following diagram for details: link.excalidraw.com/readonly/MbbnWPSWXgadXdtmzgeO
+ *
  * @param arrayBuffer font data buffer in the woff2 format
  * @param codePoints codepoints used to subset the glyphs
  *
