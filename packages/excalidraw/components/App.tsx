f4db72acdbb80b94abaf523182c96a3e8d2af6f9
ritk20 <ritobrotokalita@gmail.com>
2024-10-04 04:29:26 +0530

    correct lint

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -7889,7 +7889,11 @@ class App extends React.Component<AppProps, AppState> {
           }
 
           // We duplicate the selected element if alt is pressed on pointer move
-          if (event.altKey && !pointerDownState.hit.hasBeenDuplicated && !this.state.selectedElementsAreBeingDragged) {
+          if (
+            event.altKey && 
+            !pointerDownState.hit.hasBeenDuplicated && 
+            !this.state.selectedElementsAreBeingDragged
+          ) {
             // Move the currently selected elements to the top of the z index stack, and
             // put the duplicates where the selected elements used to be.
             // (the origin point where the dragging started)
