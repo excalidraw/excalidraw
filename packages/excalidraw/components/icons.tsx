c742b4847f16ea4cfea43ab1a0e811bf2a93467d
Ryan Di <ryan.weihao.di@gmail.com>
2024-10-09 17:46:08 +0800

    center icon

--- packages/excalidraw/components/icons.tsx
+++ packages/excalidraw/components/icons.tsx
@@ -2154,5 +2154,5 @@ export const cropIcon = createIcon(
     <path d="M8 5v10a1 1 0 0 0 1 1h10" />
     <path d="M5 8h10a1 1 0 0 1 1 1v10" />
   </g>,
-  modifiedTablerIconProps,
+  tablerIconProps,
 );
