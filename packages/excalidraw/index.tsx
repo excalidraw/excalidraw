0565bf3a9c69e91522996f2e2d8b98a1e4c52850
gimenes <tlgimenes@gmail.com>
2024-10-10 00:59:10 -0300

    export duplicateElements function to excalidraw API

--- packages/excalidraw/index.tsx
+++ packages/excalidraw/index.tsx
@@ -214,6 +214,7 @@ export {
   isInvisiblySmallElement,
   getNonDeletedElements,
   getTextFromElements,
+  duplicateElements,
 } from "./element";
 export { defaultLang, useI18n, languages } from "./i18n";
 export {
