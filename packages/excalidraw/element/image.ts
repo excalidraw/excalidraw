ab0d122156935d0bb726cc82cb7ff63841b84e5a
Ryan Di <ryan.weihao.di@gmail.com>
2024-11-27 18:42:53 +0800

    revert to 1:1 default

--- packages/excalidraw/element/image.ts
+++ packages/excalidraw/element/image.ts
@@ -115,9 +115,8 @@ export const normalizeSVG = (SVGString: string) => {
 
     if (!svg.hasAttribute("width") || !svg.hasAttribute("height")) {
       const viewBox = svg.getAttribute("viewBox");
-      // use 300 & 150 as default to match with the SVG speficiation default
-      let width = svg.getAttribute("width") || "300";
-      let height = svg.getAttribute("height") || "150";
+      let width = svg.getAttribute("width") || "50";
+      let height = svg.getAttribute("height") || "50";
 
       if (viewBox) {
         const match = viewBox.match(/\d+ +\d+ +(\d+) +(\d+)/);
