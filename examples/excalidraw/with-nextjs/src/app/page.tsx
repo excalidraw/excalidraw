d4053ca6ecb00645de9095da2534f3c8d6692007
Devanshi Gupta <160590340+DevanshiGupta15@users.noreply.github.com>
2024-12-20 06:06:24 +0530

    Update page.tsx

--- examples/excalidraw/with-nextjs/src/app/page.tsx
+++ examples/excalidraw/with-nextjs/src/app/page.tsx
@@ -2,8 +2,6 @@ import dynamic from "next/dynamic";
 import Script from "next/script";
 import "../common.scss";
 
-// Since client components get prerenderd on server as well hence importing the excalidraw stuff dynamically
-// with ssr false
 const ExcalidrawWithClientOnly = dynamic(
   async () => (await import("../excalidrawWrapper")).default,
   {
@@ -14,8 +12,7 @@ const ExcalidrawWithClientOnly = dynamic(
 export default function Page() {
   return (
     <>
-      <a href="/excalidraw-in-pages">Switch to Pages router</a>
-      <h1 className="page-title">App Router</h1>
+      <h1 className="page-title">WELCOME</h1>
       <Script id="load-env-variables" strategy="beforeInteractive">
         {`window["EXCALIDRAW_ASSET_PATH"] = window.origin;`}
       </Script>
