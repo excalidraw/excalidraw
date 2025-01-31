53bffd5e0ff7a8ac8d62c4c0a121d2d84d849bc8
David Luzar <5153846+dwelle@users.noreply.github.com>
2025-01-31 16:52:50 +0100

    feat: tweak slider colors to be more muted (#9076)

--- packages/excalidraw/components/Range.tsx
+++ packages/excalidraw/components/Range.tsx
@@ -34,7 +34,7 @@ export const Range = ({
       const position =
         (value / 100) * (inputWidth - thumbWidth) + thumbWidth / 2;
       valueElement.style.left = `${position}px`;
-      rangeElement.style.background = `linear-gradient(to right, var(--color-primary) 0%, var(--color-primary) ${value}%, var(--button-bg) ${value}%, var(--button-bg) 100%)`;
+      rangeElement.style.background = `linear-gradient(to right, var(--color-slider-track) 0%, var(--color-slider-track) ${value}%, var(--button-bg) ${value}%, var(--button-bg) 100%)`;
     }
   }, [value]);
 
