e5da6b08b2fe29678777cc68e33ea7d866dafa2d
dwelle <5153846+dwelle@users.noreply.github.com>
2025-02-21 13:06:48 +0100

    refactor: explicitly export `elementsOverlappingBBox` from utils package

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,4 +1,5 @@
 export * from "./export";
 export * from "./withinBounds";
 export * from "./bbox";
+export { elementsOverlappingBBox } from "./withinBounds";
 export { getCommonBounds } from "../excalidraw/element/bounds";
