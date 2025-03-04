d8a5bb6b05a72ceac7b49b43e44c470010d64534
David Luzar <5153846+dwelle@users.noreply.github.com>
2025-03-04 21:45:48 +0100

    fix: package env vars (#9221)

--- scripts/buildPackage.js
+++ scripts/buildPackage.js
@@ -1,6 +1,18 @@
 const path = require("path");
 const { build } = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
+const { parseEnvVariables } = require("../packages/excalidraw/env.cjs");
+
+const ENV_VARS = {
+  development: {
+    ...parseEnvVariables(`${__dirname}/../.env.development`),
+    DEV: true,
+  },
+  production: {
+    ...parseEnvVariables(`${__dirname}/../.env.production`),
+    PROD: true,
+  },
+};
 
 // excludes all external dependencies and bundles only the source code
 const getConfig = (outdir) => ({
@@ -28,7 +40,7 @@ function buildDev(config) {
     ...config,
     sourcemap: true,
     define: {
-      "import.meta.env": JSON.stringify({ DEV: true }),
+      "import.meta.env": JSON.stringify(ENV_VARS.development),
     },
   });
 }
@@ -38,7 +50,7 @@ function buildProd(config) {
     ...config,
     minify: true,
     define: {
-      "import.meta.env": JSON.stringify({ PROD: true }),
+      "import.meta.env": JSON.stringify(ENV_VARS.production),
     },
   });
 }
