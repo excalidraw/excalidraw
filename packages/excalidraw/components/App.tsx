618199cc5c2cb15442ce5feb998532f0dfc73b66
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-04-21 18:33:23 -0500

    Fix: groupIds update for boundText element

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -9230,6 +9230,22 @@ class App extends React.Component<AppProps, AppState> {
                   },
                   false,
                 );
+
+                const boundText = getBoundTextElement(element, this.scene.getNonDeletedElementsMap())
+
+                if (boundText) {
+                  const boundTextIndex = boundText.groupIds.indexOf(
+                    this.state.editingGroupId!,
+                  );
+
+                  mutateElement(
+                    boundText,
+                    {
+                      groupIds: boundText.groupIds.slice(0, boundTextIndex),
+                    },
+                    false,
+                  );
+                }
               }
 
               nextElements.forEach((element) => {
