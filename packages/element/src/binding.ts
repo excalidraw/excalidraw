e1fcfb98dec6a05dd86a27e162692e1af888315c
Mark Tolmacs <mark@lazycat.hu>
2025-04-22 19:20:20 +0200

    Fix snapping at diamond edges

--- packages/element/src/binding.ts
+++ packages/element/src/binding.ts
@@ -44,7 +44,6 @@ import { intersectElementWithLineSegment } from "./collision";
 import { distanceToBindableElement } from "./distance";
 import {
   headingForPointFromElement,
-  headingIsHorizontal,
   vectorToHeading,
   type Heading,
 } from "./heading";
@@ -923,23 +922,16 @@ export const bindPointToSnapToElementOutline = (
 
   let intersection: GlobalPoint | null = null;
   if (elbowed) {
-    const isHorizontal = headingIsHorizontal(
-      headingForPointFromElement(bindableElement, aabb, globalP),
-    );
-    const otherPoint = pointFrom<GlobalPoint>(
-      isHorizontal ? center[0] : edgePoint[0],
-      !isHorizontal ? center[1] : edgePoint[1],
-    );
     intersection = intersectElementWithLineSegment(
       bindableElement,
       lineSegment(
-        otherPoint,
+        center,
         pointFromVector(
           vectorScale(
-            vectorNormalize(vectorFromPoint(edgePoint, otherPoint)),
+            vectorNormalize(vectorFromPoint(edgePoint, center)),
             Math.max(bindableElement.width, bindableElement.height) * 2,
           ),
-          otherPoint,
+          center,
         ),
       ),
     )[0];
