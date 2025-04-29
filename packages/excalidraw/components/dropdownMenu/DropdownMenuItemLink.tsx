c8036948ba62684feacffc01164f7429b6857369
David Luzar <5153846+dwelle@users.noreply.github.com>
2025-04-29 18:45:17 +0200

    fix: remove `noreferrer` on internal links (#9452)
    * fix: remove `noreferrer` on internal links
    
    * fix snaps
    
    * fix lint

--- packages/excalidraw/components/dropdownMenu/DropdownMenuItemLink.tsx
+++ packages/excalidraw/components/dropdownMenu/DropdownMenuItemLink.tsx
@@ -16,7 +16,7 @@ const DropdownMenuItemLink = ({
   onSelect,
   className = "",
   selected,
-  rel = "noreferrer",
+  rel = "noopener",
   ...rest
 }: {
   href: string;
@@ -31,11 +31,12 @@ const DropdownMenuItemLink = ({
   const handleClick = useHandleDropdownMenuItemClick(rest.onClick, onSelect);
 
   return (
+    // eslint-disable-next-line react/jsx-no-target-blank
     <a
       {...rest}
       href={href}
       target="_blank"
-      rel="noreferrer"
+      rel={rel || "noopener"}
       className={getDropdownMenuItemClassName(className, selected)}
       title={rest.title ?? rest["aria-label"]}
       onClick={handleClick}
