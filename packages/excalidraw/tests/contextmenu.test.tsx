5d90670a71dc9cf10a3606413e0b0a5a3b3664ba
Mark Tolmacs <mark@lazycat.hu>
2025-05-08 12:48:36 +0200

    Fixing tests

--- packages/excalidraw/tests/contextmenu.test.tsx
+++ packages/excalidraw/tests/contextmenu.test.tsx
@@ -304,12 +304,12 @@ describe("contextMenu element", () => {
 
   it("selecting 'Copy styles' in context menu copies styles", () => {
     UI.clickTool("rectangle");
-    mouse.down(10, 10);
+    mouse.down(13, 10);
     mouse.up(20, 20);
 
     fireEvent.contextMenu(GlobalTestState.interactiveCanvas, {
       button: 2,
-      clientX: 3,
+      clientX: 13,
       clientY: 3,
     });
     const contextMenu = UI.queryContextMenu();
@@ -389,12 +389,12 @@ describe("contextMenu element", () => {
 
   it("selecting 'Delete' in context menu deletes element", () => {
     UI.clickTool("rectangle");
-    mouse.down(10, 10);
+    mouse.down(13, 10);
     mouse.up(20, 20);
 
     fireEvent.contextMenu(GlobalTestState.interactiveCanvas, {
       button: 2,
-      clientX: 3,
+      clientX: 13,
       clientY: 3,
     });
     const contextMenu = UI.queryContextMenu();
@@ -405,12 +405,12 @@ describe("contextMenu element", () => {
 
   it("selecting 'Add to library' in context menu adds element to library", async () => {
     UI.clickTool("rectangle");
-    mouse.down(10, 10);
+    mouse.down(13, 10);
     mouse.up(20, 20);
 
     fireEvent.contextMenu(GlobalTestState.interactiveCanvas, {
       button: 2,
-      clientX: 3,
+      clientX: 13,
       clientY: 3,
     });
     const contextMenu = UI.queryContextMenu();
@@ -424,12 +424,12 @@ describe("contextMenu element", () => {
 
   it("selecting 'Duplicate' in context menu duplicates element", () => {
     UI.clickTool("rectangle");
-    mouse.down(10, 10);
+    mouse.down(13, 10);
     mouse.up(20, 20);
 
     fireEvent.contextMenu(GlobalTestState.interactiveCanvas, {
       button: 2,
-      clientX: 3,
+      clientX: 13,
       clientY: 3,
     });
     const contextMenu = UI.queryContextMenu();
