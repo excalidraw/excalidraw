69d7da0d1fb016bb207a648b8aa97a7e46623eec
dwelle <5153846+dwelle@users.noreply.github.com>
2025-05-09 15:46:47 +0200

    rename `serverSide` -> `private`

--- packages/common/src/font-metadata.ts
+++ packages/common/src/font-metadata.ts
@@ -22,8 +22,10 @@ export interface FontMetadata {
   };
   /** flag to indicate a deprecated font */
   deprecated?: true;
-  /** flag to indicate a server-side only font */
-  serverSide?: true;
+  /**
+   * whether this is a font that users can use (= shown in font picker)
+   */
+  private?: true;
   /** flag to indiccate a local-only font */
   local?: true;
   /** flag to indicate a fallback font */
@@ -98,7 +100,7 @@ export const FONT_METADATA: Record<number, FontMetadata> = {
       descender: -434,
       lineHeight: 1.15,
     },
-    serverSide: true,
+    private: true,
   },
   [FONT_FAMILY.Assistant]: {
     metrics: {
@@ -107,7 +109,7 @@ export const FONT_METADATA: Record<number, FontMetadata> = {
       descender: -287,
       lineHeight: 1.25,
     },
-    serverSide: true,
+    private: true,
   },
   [FONT_FAMILY_FALLBACKS.Xiaolai]: {
     metrics: {
