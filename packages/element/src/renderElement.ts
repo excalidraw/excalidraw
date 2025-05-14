f7dcc893ea0f2d1061db614d3c2c7b43e256e7bc
zsviczian <viczian.zsolt@gmail.com>
2025-05-14 13:38:18 +0200

    feat: transparent link background, scale link icon when zooming to below 100% (#9520)
    * Do not set link background color, dynamically scale down link icon size with zoom.
    
    * removed unnecessary change
    
    * use canvas bg color & reduce size and stroke width
    
    ---------
    
    Co-authored-by: dwelle <5153846+dwelle@users.noreply.github.com>

--- packages/element/src/renderElement.ts
+++ packages/element/src/renderElement.ts
@@ -349,8 +349,6 @@ const generateElementCanvas = (
   };
 };
 
-export const DEFAULT_LINK_SIZE = 14;
-
 const IMAGE_PLACEHOLDER_IMG = document.createElement("img");
 IMAGE_PLACEHOLDER_IMG.src = `data:${MIME_TYPES.svg},${encodeURIComponent(
   `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="image" class="svg-inline--fa fa-image fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#888" d="M464 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM112 120c-30.928 0-56 25.072-56 56s25.072 56 56 56 56-25.072 56-56-25.072-56-56-56zM64 384h384V272l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L208 320l-55.515-55.515c-4.686-4.686-12.284-4.686-16.971 0L64 336v48z"></path></svg>`,
