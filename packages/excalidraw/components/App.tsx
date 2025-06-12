fdcf5424c2f246b28151e261a3c6da317cc07c3c
zsviczian <viczian.zsolt@gmail.com>
2025-06-12 13:16:24 +0000

    0.18.0-21

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -7764,17 +7764,21 @@ class App extends React.Component<AppProps, AppState> {
           });
         }
 
+        const isUnlockedHitElSelected = unlockedHitElements.some(
+          (el) => this.state.selectedElementIds[el.id],
+        ); //zsviczian
         // Prioritize unlocked elements over locked ones
-        if (unlockedHitElements.length > 0) { //zsviczian https://github.com/excalidraw/excalidraw/pull/9582
+        if (
+          !isUnlockedHitElSelected &&
+          unlockedHitElements.length > 0
+        ) { //zsviczian https://github.com/excalidraw/excalidraw/pull/9582
           // If there are unlocked elements, use the topmost one
           pointerDownState.hit.element =
             unlockedHitElements[unlockedHitElements.length - 1];
         } else if (
           hitElementMightBeLocked &&
           hitElementMightBeLocked.locked &&
-          !unlockedHitElements.some(
-            (el) => this.state.selectedElementIds[el.id],
-          )
+          !isUnlockedHitElSelected //zsviczian
         ) {
           pointerDownState.hit.element = null;
         } else {
