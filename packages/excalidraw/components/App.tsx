2b9a53e51cb62450a5415043a4057c0d73ee9997
Omar Brikaa <brikaaomar@gmail.com>
2025-06-20 20:01:54 +0300

    Events in props -> api

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -621,6 +621,9 @@ class App extends React.Component<AppProps, AppState> {
     ]
   >();
 
+  onLoadEmitter = new Emitter();
+  onResetEmitter = new Emitter();
+
   onPointerDownEmitter = new Emitter<
     [
       activeTool: AppState["activeTool"],
@@ -720,6 +723,8 @@ class App extends React.Component<AppProps, AppState> {
         updateFrameRendering: this.updateFrameRendering,
         toggleSidebar: this.toggleSidebar,
         onChange: (cb) => this.onChangeEmitter.on(cb),
+        onLoad: (cb) => this.onLoadEmitter.on(cb),
+        onReset: (cb) => this.onResetEmitter.on(cb),
         onIncrement: (cb) => this.store.onStoreIncrementEmitter.on(cb),
         onPointerDown: (cb) => this.onPointerDownEmitter.on(cb),
         onPointerUp: (cb) => this.onPointerUpEmitter.on(cb),
@@ -2280,7 +2285,7 @@ class App extends React.Component<AppProps, AppState> {
       }));
       this.resetStore();
       this.resetHistory();
-      this.props.onReset?.();
+      this.onResetEmitter.trigger();
     },
   );
 
@@ -2562,6 +2567,8 @@ class App extends React.Component<AppProps, AppState> {
     this.laserTrails.stop();
     this.eraserTrail.stop();
     this.onChangeEmitter.clear();
+    this.onLoadEmitter.clear();
+    this.onResetEmitter.clear();
     this.store.onStoreIncrementEmitter.clear();
     this.store.onDurableIncrementEmitter.clear();
     ShapeCache.destroy();
@@ -10425,7 +10432,7 @@ class App extends React.Component<AppProps, AppState> {
           },
           replaceFiles: true,
           captureUpdate: CaptureUpdateAction.IMMEDIATELY,
-          callback: this.props.onLoad,
+          callback: this.onLoadEmitter.trigger,
         });
       } else if (ret.type === MIME_TYPES.excalidrawlib) {
         await this.library
