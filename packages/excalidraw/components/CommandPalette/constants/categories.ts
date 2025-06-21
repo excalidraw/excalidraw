d46ed6b7a6a6ed7aa21b569251817fcd4c6462d2
bartek <bartzagr@gmail.com>
2025-06-21 15:13:25 +0200

    refaactor: extract constants

---
+++ packages/excalidraw/components/CommandPalette/constants/categories.ts
@@ -0,0 +1,17 @@
+export const DEFAULT_CATEGORIES = {
+  app: "App",
+  export: "Export",
+  tools: "Tools",
+  editor: "Editor",
+  elements: "Elements",
+  links: "Links",
+};
+
+export const CATEGORIES_ORDER = {
+  [DEFAULT_CATEGORIES.app]: 1,
+  [DEFAULT_CATEGORIES.export]: 2,
+  [DEFAULT_CATEGORIES.editor]: 3,
+  [DEFAULT_CATEGORIES.tools]: 4,
+  [DEFAULT_CATEGORIES.elements]: 5,
+  [DEFAULT_CATEGORIES.links]: 6,
+};
