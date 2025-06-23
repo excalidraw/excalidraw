95d4d23d73e858969f8cf1bbcf800a07d671ea4b
Omar Brikaa <brikaaomar@gmail.com>
2025-06-23 18:38:29 +0300

    Test

--- excalidraw-app/tests/SaveReminder.test.tsx
+++ excalidraw-app/tests/SaveReminder.test.tsx
@@ -7,7 +7,6 @@ import { vi } from "vitest";
 import { resolvablePromise } from "@excalidraw/common";
 import { Excalidraw } from "@excalidraw/excalidraw";
 import { t } from "@excalidraw/excalidraw/i18n";
-import { Pointer } from "@excalidraw/excalidraw/tests/helpers/ui";
 
 import { API } from "@excalidraw/excalidraw/tests/helpers/api";
 
@@ -17,9 +16,10 @@ import type { ExcalidrawElement } from "@excalidraw/element/types";
 import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
 
 const { h } = window;
-const mouse = new Pointer("mouse");
 
 describe("Save reminder", () => {
+  let excalidrawAPI: ExcalidrawImperativeAPI;
+
   beforeAll(() => {
     vi.useFakeTimers();
     vi.setSystemTime("2020-01-01T00:00:00Z");
@@ -33,7 +33,7 @@ describe("Save reminder", () => {
       />,
     );
 
-    const excalidrawAPI = await excalidrawAPIPromise;
+    excalidrawAPI = await excalidrawAPIPromise;
     rerender(
       <Excalidraw>
         <SaveReminder excalidrawAPI={excalidrawAPI} />
@@ -50,7 +50,7 @@ describe("Save reminder", () => {
       currentTime.setTime(currentTime.getTime() + tier.time);
       vi.setSystemTime(currentTime);
       act(() => {
-        mouse.click();
+        h.scene.triggerUpdate();
       });
       expect(h.state.toast).toBe(null);
       const elements: ExcalidrawElement[] = [];
@@ -72,8 +72,46 @@ describe("Save reminder", () => {
       API.updateScene({ elements: [] });
     }
   });
-});
 
-afterAll(() => {
-  vi.useRealTimers();
+  it("Should only fire after time threshold is exceeded if count threshold is already exceeded", async () => {
+    for (const tier of REMINDER_TIERS) {
+      const elements: ExcalidrawElement[] = [];
+      for (let i = 0; i < tier.elementsCount; ++i) {
+        elements.push(
+          API.createElement({
+            id: i.toString(),
+            type: "rectangle",
+            height: 200,
+            width: 100,
+            x: i,
+            y: i,
+          }),
+        );
+      }
+      API.updateScene({ elements: syncInvalidIndices(elements) });
+      expect(h.state.toast).toBe(null);
+      const currentTime = vi.getMockedSystemTime();
+      if (!currentTime) {
+        throw new Error("No mocked date");
+      }
+      currentTime.setTime(currentTime.getTime() + tier.time);
+      vi.setSystemTime(currentTime);
+      act(() => {
+        h.scene.triggerUpdate();
+      });
+      expect(h.state.toast?.message).toBe(t("toast.rememberToSave"));
+      API.setAppState({ toast: null });
+      API.updateScene({ elements: [] });
+    }
+  });
+
+  afterEach(() => {
+    act(() => {
+      excalidrawAPI.resetScene();
+    });
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
 });
