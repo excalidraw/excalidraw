c8de17df3392f165e14cbd06443d7866ebd8666a
root <root@Gameify.ndy.onl>
2025-07-20 14:16:00 +0200

    feat: Implement and integrate GamifyToolbar

--- excalidraw-app/components/GamifyToolbar.tsx
+++ excalidraw-app/components/GamifyToolbar.tsx
@@ -1,67 +1,40 @@
 import React from "react";
-import { useExcalidrawAPI } from "@excalidraw/excalidraw";
-import { newElementWith } from "@excalidraw/element";
+import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 import { nanoid } from "nanoid";
 
-export const GamifyToolbar = () => {
-  const excalidrawAPI = useExcalidrawAPI();
+interface GamifyToolbarProps {
+  excalidrawAPI: ExcalidrawImperativeAPI;
+}
 
+export const GamifyToolbar: React.FC<GamifyToolbarProps> = ({ excalidrawAPI }) => {
   const createGameSet = () => {
-    const cardId = nanoid();
-    const zoneId = nanoid();
+    const cardId = `card_${nanoid()}`;
+    const zoneId = `zone_${nanoid()}`;
 
-    const card = newElementWith({
+    const cardElement = { 
       type: "rectangle",
       x: 100,
       y: 100,
       width: 100,
-      height: 50,
-      strokeColor: "#000000",
-      backgroundColor: "#FFC0CB", // Pink
-      fillStyle: "solid",
-      strokeWidth: 2,
-      roughness: 1,
-      opacity: 100,
-      angle: 0,
-      groupIds: [],
-      frameId: null,
-      roundness: null,
-      boundElements: null,
-      updated: Date.now(),
-      link: null,
-      locked: false,
+      height: 100,
       customData: { isCard: true, id: cardId },
-    });
-
-    const zone = newElementWith({
+    };
+    const zoneElement = { 
       type: "rectangle",
       x: 300,
       y: 100,
-      width: 120,
-      height: 70,
-      strokeColor: "#000000",
-      backgroundColor: "#ADD8E6", // Light Blue
-      fillStyle: "solid",
-      strokeWidth: 2,
-      roughness: 1,
-      opacity: 100,
-      angle: 0,
-      groupIds: [],
-      frameId: null,
-      roundness: null,
-      boundElements: null,
-      updated: Date.now(),
-      link: null,
-      locked: false,
-      customData: { isZone: true, accepts: cardId },
-    });
+      width: 150,
+      height: 150,
+      customData: { isZone: true, accepts: cardId, id: zoneId },
+    };
 
-    excalidrawAPI.updateScene({ elements: [card, zone] });
+    excalidrawAPI.addElements([cardElement, zoneElement]);
   };
 
   return (
     <div style={{ position: "absolute", top: "10px", left: "10px", zIndex: 10 }}>
-      <button onClick={createGameSet}>Create Game Set</button>
+      <h3>Gamify-Tools</h3>
+      <button onClick={createGameSet}>Neues Spiel-Set erstellen</button>
     </div>
   );
 };
