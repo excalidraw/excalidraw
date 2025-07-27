e6137bc8467d396ef40bc7a7744bf29003ccff92
Omar Brikaa <brikaaomar@gmail.com>
2025-07-27 20:37:38 +0300

    More reliable width and height change detection

--- packages/excalidraw/components/canvases/StaticCanvas.tsx
+++ packages/excalidraw/components/canvases/StaticCanvas.tsx
@@ -49,26 +49,6 @@ const StaticCanvas = (props: StaticCanvasProps) => {
       canvas.classList.add("excalidraw__canvas", "static");
     }
 
-    const widthString = `${props.appState.width}px`;
-    const heightString = `${props.appState.height}px`;
-    if (canvas.style.width !== widthString) {
-      canvas.style.width = widthString;
-    }
-    if (canvas.style.height !== heightString) {
-      canvas.style.height = heightString;
-    }
-
-    const scaledWidth = props.appState.width * props.scale;
-    const scaledHeight = props.appState.height * props.scale;
-    // setting width/height resets the canvas even if dimensions not changed,
-    // which would cause flicker when we skip frame (due to throttling)
-    if (canvas.width !== scaledWidth) {
-      canvas.width = scaledWidth;
-    }
-    if (canvas.height !== scaledHeight) {
-      canvas.height = scaledHeight;
-    }
-
     renderStaticScene(
       {
         canvas,
@@ -83,6 +63,13 @@ const StaticCanvas = (props: StaticCanvasProps) => {
       isRenderThrottlingEnabled(),
     );
   });
+
+  useEffect(() => {
+    props.canvas.style.width = `${props.appState.width}px`;
+    props.canvas.style.height = `${props.appState.height}px`;
+    props.canvas.width = props.appState.width * props.scale;
+    props.canvas.height = props.appState.height * props.scale;
+  }, [props.appState.height, props.appState.width, props.canvas, props.scale]);
 
   return <div className="excalidraw__canvas-wrapper" ref={wrapperRef} />;
 };
