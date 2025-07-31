750fe2f9a5f6bd3a01e3e6222e43cecb225c8fa1
Ryan Di <ryan.weihao.di@gmail.com>
2025-07-31 22:10:59 +1000

    fix: add frame clipping to new element canvas (#9794)
    * fix: add frame clipping to new element canvas
    
    * cleanup save/restore
    
    ---------
    
    Co-authored-by: dwelle <5153846+dwelle@users.noreply.github.com>

--- packages/excalidraw/renderer/staticScene.ts
+++ packages/excalidraw/renderer/staticScene.ts
@@ -113,7 +113,7 @@ const strokeGrid = (
   context.restore();
 };
 
-const frameClip = (
+export const frameClip = (
   frame: ExcalidrawFrameLikeElement,
   context: CanvasRenderingContext2D,
   renderConfig: StaticCanvasRenderConfig,
