44d9be19b2a115a33f6321a06ccc0e325f633d8c
Andras Viczian <vandras2003@gmail.com>
2025-08-01 18:08:05 +0200

    Don't render if the position is 0,0

--- packages/excalidraw/components/ConvertElementTypePopup.tsx
+++ packages/excalidraw/components/ConvertElementTypePopup.tsx
@@ -313,6 +313,7 @@ const Panel = ({
       ref={panelRef}
       tabIndex={-1}
       style={{
+        visibility: `${positionRef.current === "" ? "hidden" : "visible"}`,
         position: "absolute",
         top: `${
           panelPosition.y +
