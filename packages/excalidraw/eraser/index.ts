de827c336bb89495b5a09ece6a8beea1fba6f30e
Mark Tolmacs <mark@lazycat.hu>
2025-09-14 11:27:30 +0200

    fix:Remove extra in-poly check

--- packages/excalidraw/eraser/index.ts
+++ packages/excalidraw/eraser/index.ts
@@ -257,10 +257,10 @@ const eraserTest = (
       ) as GlobalPoint[]),
     );
 
-    if (
-      polygonIncludesPoint(pathSegment[0], poly) ||
-      polygonIncludesPoint(pathSegment[1], poly)
-    ) {
+    // PERF: Check only one point of the eraser segment. If the eraser segment
+    // start is inside the closed freedraw shape, the other point is either also
+    // inside or the eraser segment will intersect the shape outline anyway
+    if (polygonIncludesPoint(pathSegment[0], poly)) {
       return true;
     }
 
