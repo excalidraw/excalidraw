727921d1e53437e0dc53ed990fb8ffc877344de9
drick-infinity <harshimalviya47487@gmail.com>
2025-09-16 01:46:23 -0700

    add feat export-selection to context-menu

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -305,7 +305,7 @@ import {
 } from "../actions";
 import { actionWrapTextInContainer } from "../actions/actionBoundText";
 import { actionToggleHandTool, zoomToFit } from "../actions/actionCanvas";
-import { actionPaste } from "../actions/actionClipboard";
+import { actionPaste, imageExport } from "../actions/actionClipboard";
 import { actionCopyElementLink } from "../actions/actionElementLink";
 import { actionUnlockAllElements } from "../actions/actionElementLock";
 import {
@@ -11172,6 +11172,8 @@ class App extends React.Component<AppProps, AppState> {
       CONTEXT_MENU_SEPARATOR,
       actionDuplicateSelection,
       actionToggleElementLock,
+      CONTEXT_MENU_SEPARATOR,
+      imageExport,
       CONTEXT_MENU_SEPARATOR,
       actionDeleteSelected,
     ];
