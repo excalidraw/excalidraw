9b35f89d74acd09401314dad061c471d2c9aa5fd
Yasir761 <myasiransari16@gmail.com>
2025-10-12 12:39:50 +0530

    Initialized false for storage alert

--- excalidraw-app/data/LocalData.ts
+++ excalidraw-app/data/LocalData.ts
@@ -47,7 +47,7 @@ import { updateBrowserStateVersion } from "./tabSync";
 
 const filesStore = createStore("files-db", "files-store");
 
-export const localStorageQuotaExceededAtom = atom(true);
+export const localStorageQuotaExceededAtom = atom(false);
 
 class LocalFileManager extends FileManager {
   clearObsoleteFiles = async (opts: { currentFileIds: FileId[] }) => {
