4d416c6542b008abfb21457cdaf4e4e537f6cdd1
Omar Brikaa <brikaaomar@gmail.com>
2025-10-12 22:07:47 +0300

    More normalized shortcuts

--- packages/excalidraw/shortcut.ts
+++ packages/excalidraw/shortcut.ts
@@ -4,10 +4,13 @@ import { t } from "./i18n";
 
 export const getShortcutKey = (shortcut: string): string =>
   shortcut
-    .replace(/\bAlt\b/i, isDarwin ? t("keys.option") : t("keys.alt"))
+    .replace(/\b(Option|Alt)\b/i, isDarwin ? t("keys.option") : t("keys.alt"))
     .replace(/\bShift\b/i, t("keys.shift"))
     .replace(/\b(Enter|Return)\b/i, t("keys.enter"))
-    .replace(/\bCtrlOrCmd\b/gi, isDarwin ? t("keys.cmd") : t("keys.ctrl"))
-    .replace(/\bEscape\b/i, t("keys.escape"))
+    .replace(
+      /\b(Ctrl|Cmd|CtrlOrCmd)\b/gi,
+      isDarwin ? t("keys.cmd") : t("keys.ctrl"),
+    )
+    .replace(/\b(Esc|Escape)\b/i, t("keys.escape"))
     .replace(/\b(Space|Spacebar)\b/i, t("keys.spacebar"))
     .replace(/\b(Del|Delete)\b/i, t("keys.delete"));
