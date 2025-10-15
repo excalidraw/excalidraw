a83c70ec9bbf97be3ea0af199179e69ec90ab7ec
rigozsolt <zsolt.rigo@datatronic.hu>
2025-10-15 23:50:44 +0200

    refactor: make the TTDDialogSubmitShortcut more intuitive

--- packages/excalidraw/components/TTDDialog/TTDDialogSubmitShortcut.tsx
+++ packages/excalidraw/components/TTDDialog/TTDDialogSubmitShortcut.tsx
@@ -3,6 +3,7 @@ import { getShortcutKey } from "@excalidraw/common";
 export const TTDDialogSubmitShortcut = () => {
   return (
     <div className="ttd-dialog-submit-shortcut">
+      <span className="ttd-dialog-submit-shortcut__label">or</span>
       <div className="ttd-dialog-submit-shortcut__key">
         {getShortcutKey("CtrlOrCmd")}
       </div>
