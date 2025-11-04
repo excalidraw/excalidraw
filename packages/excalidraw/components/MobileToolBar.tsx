a391ab7e40dda37a4091f9c8c9534d819a2ef251
Ryan Di <ryan.weihao.di@gmail.com>
2025-11-04 14:20:05 +1100

    fix lint

--- packages/excalidraw/components/MobileToolBar.tsx
+++ packages/excalidraw/components/MobileToolBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import clsx from "clsx";
 
 import { KEYS, capitalizeString } from "@excalidraw/common";
