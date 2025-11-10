420ff0571f5856953406d0f7f1c8bdf871b9443b
Márk Tolmács <mark@lazycat.hu>
2025-11-10 22:31:08 +0100

    fix: Alt-duplication copied elements placement (#10152)

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -8846,6 +8846,15 @@ class App extends React.Component<AppProps, AppState> {
               }));
 
               this.scene.replaceAllElements(elementsWithIndices);
+              selectedElements.forEach((element) => {
+                if (
+                  isBindableElement(element) &&
+                  element.boundElements?.some((other) => other.type === "arrow")
+                ) {
+                  updateBoundElements(element, this.scene);
+                }
+              });
+
               this.maybeCacheVisibleGaps(event, selectedElements, true);
               this.maybeCacheReferenceSnapPoints(event, selectedElements, true);
             });
