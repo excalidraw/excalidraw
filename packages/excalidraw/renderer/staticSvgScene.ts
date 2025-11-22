14026db966e2423e1a4e5e150619136cf15fec6b
zsviczian <viczian.zsolt@gmail.com>
2025-11-22 14:52:33 +0100

    fixes #10386 svg export

--- packages/excalidraw/renderer/staticSvgScene.ts
+++ packages/excalidraw/renderer/staticSvgScene.ts
@@ -560,7 +560,7 @@ const renderElementToSvg = (
             "clipPath",
           );
           clipPath.id = `image-clipPath-${element.id}`;
-
+          clipPath.setAttribute("clipPathUnits", "userSpaceOnUse"); //zsviczian #10387
           const clipRect = svgRoot.ownerDocument!.createElementNS(
             SVG_NS,
             "rect",
@@ -569,6 +569,10 @@ const renderElementToSvg = (
             Math.min(element.width, element.height),
             element,
           );
+          const clipOffsetX = element.crop ? normalizedCropX : 0; //zsviczian #10387
+          const clipOffsetY = element.crop ? normalizedCropY : 0; //zsviczian #10387
+          clipRect.setAttribute("x", `${clipOffsetX}`); //zsviczian #10387
+          clipRect.setAttribute("y", `${clipOffsetY}`); //zsviczian #10387
           clipRect.setAttribute("width", `${element.width}`);
           clipRect.setAttribute("height", `${element.height}`);
           clipRect.setAttribute("rx", `${radius}`);
