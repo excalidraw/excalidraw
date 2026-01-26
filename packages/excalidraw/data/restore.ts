29c3af30b0fb2c57e5613cf293fa17867a480afd
Mark Tolmacs <mark@lazycat.hu>
2026-01-26 20:35:55 +0000

    chore: Restore restore.ts
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- packages/excalidraw/data/restore.ts
+++ packages/excalidraw/data/restore.ts
@@ -19,13 +19,7 @@ import {
   normalizeLink,
   getLineHeight,
 } from "@excalidraw/common";
-import {
-  calculateFixedPointForNonElbowArrowBinding,
-  getNonDeletedElements,
-  isPointInElement,
-  isValidPolygon,
-  projectFixedPointOntoDiagonal,
-} from "@excalidraw/element";
+import { getNonDeletedElements, isValidPolygon } from "@excalidraw/element";
 import { normalizeFixedPoint } from "@excalidraw/element";
 import {
   updateElbowArrowPoints,
@@ -59,7 +53,6 @@ import type {
   ElementsMap,
   ElementsMapOrArray,
   ExcalidrawArrowElement,
-  ExcalidrawBindableElement,
   ExcalidrawElbowArrowElement,
   ExcalidrawElement,
   ExcalidrawLinearElement,
@@ -82,12 +75,7 @@ import {
   getNormalizedZoom,
 } from "../scene";
 
-import type {
-  AppState,
-  BinaryFiles,
-  LibraryItem,
-  NormalizedZoomValue,
-} from "../types";
+import type { AppState, BinaryFiles, LibraryItem } from "../types";
 import type { ImportedDataState, LegacyAppState } from "./types";
 
 type RestoredAppState = Omit<
@@ -145,153 +133,41 @@ const repairBinding = <T extends ExcalidrawArrowElement>(
     return null;
   }
 
-  try {
-    // ---------------------------------------------------------------------------
-    // elbow arrows
-    // ---------------------------------------------------------------------------
-
-    if (isElbowArrow(element)) {
-      const fixedPointBinding:
-        | ExcalidrawElbowArrowElement["startBinding"]
-        | ExcalidrawElbowArrowElement["endBinding"] = {
-        ...binding,
-        fixedPoint: normalizeFixedPoint(binding.fixedPoint ?? [0, 0]),
-        mode: binding.mode || "orbit",
-      };
-
-      return fixedPointBinding;
-    }
-
-    // ---------------------------------------------------------------------------
-    // simple arrows
-    // ---------------------------------------------------------------------------
-
-    // binding schema v2
-    // ---------------------------------------------------------------------------
-
-    if (binding.mode) {
-      // if latest binding schema, don't check if binding.elementId exists
-      // (it's done in a separate pass)
-      if (binding.elementId) {
-        return {
-          elementId: binding.elementId,
-          mode: binding.mode,
-          fixedPoint: normalizeFixedPoint(binding.fixedPoint || [0.5, 0.5]),
-        } as FixedPointBinding | null;
-      }
-      return null;
-    }
-
-    // binding schema v1 (legacy) -> attempt to migrate to v2
-    // ---------------------------------------------------------------------------
-
-    const targetBoundElement =
-      (targetElementsMap.get(binding.elementId) as ExcalidrawBindableElement) ||
-      undefined;
-    const boundElement =
-      targetBoundElement ||
-      (existingElementsMap?.get(
-        binding.elementId,
-      ) as ExcalidrawBindableElement) ||
-      undefined;
-    const elementsMap = targetBoundElement
-      ? targetElementsMap
-      : existingElementsMap;
-
-    // migrating legacy focus point bindings
-    if (boundElement && elementsMap) {
-      const p = LinearElementEditor.getPointAtIndexGlobalCoordinates(
-        element,
-        startOrEnd === "start" ? 0 : element.points.length - 1,
-        elementsMap,
-      );
-      const mode = isPointInElement(p, boundElement, elementsMap)
-        ? "inside"
-        : "orbit";
-      const focusPoint =
-        mode === "inside"
-          ? p
-          : projectFixedPointOntoDiagonal(
-              element,
-              p,
-              boundElement,
-              startOrEnd,
-              elementsMap,
-              { value: 1 as NormalizedZoomValue },
-            ) || p;
-      const { fixedPoint } = calculateFixedPointForNonElbowArrowBinding(
-        element,
-        boundElement,
-        startOrEnd,
-        elementsMap,
-        focusPoint,
-      );
-
-      return {
-        mode,
-        elementId: binding.elementId,
-        fixedPoint,
-      };
-    }
-
-    console.error(`could not repair binding for element`);
-  } catch (error) {
-    console.error("Error repairing binding:", error);
-  }
-
-  // binding schema v1 (legacy) -> attempt to migrate to v2
+  // ---------------------------------------------------------------------------
+  // elbow arrows
   // ---------------------------------------------------------------------------
 
-  const targetBoundElement =
-    (targetElementsMap.get(binding.elementId) as ExcalidrawBindableElement) ||
-    undefined;
-  const boundElement =
-    targetBoundElement ||
-    (existingElementsMap?.get(
-      binding.elementId,
-    ) as ExcalidrawBindableElement) ||
-    undefined;
-  const elementsMap = targetBoundElement
-    ? targetElementsMap
-    : existingElementsMap;
-
-  // migrating legacy focus point bindings
-  if (boundElement && elementsMap) {
-    const p = LinearElementEditor.getPointAtIndexGlobalCoordinates(
-      element,
-      startOrEnd === "start" ? 0 : element.points.length - 1,
-      elementsMap,
-    );
-    const mode = isPointInElement(p, boundElement, elementsMap)
-      ? "inside"
-      : "orbit";
-    const focusPoint =
-      mode === "inside"
-        ? p
-        : projectFixedPointOntoDiagonal(
-            element,
-            p,
-            boundElement,
-            startOrEnd,
-            elementsMap,
-            { value: 1 as NormalizedZoomValue },
-          ) || p;
-    const { fixedPoint } = calculateFixedPointForNonElbowArrowBinding(
-      element,
-      boundElement,
-      startOrEnd,
-      elementsMap,
-      focusPoint,
-    );
-
-    return {
-      mode,
-      elementId: binding.elementId,
-      fixedPoint,
+  if (isElbowArrow(element)) {
+    const fixedPointBinding:
+      | ExcalidrawElbowArrowElement["startBinding"]
+      | ExcalidrawElbowArrowElement["endBinding"] = {
+      ...binding,
+      fixedPoint: normalizeFixedPoint(binding.fixedPoint ?? [0, 0]),
+      mode: binding.mode || "orbit",
     };
+
+    return fixedPointBinding;
   }
 
-  console.error(`could not repair binding for element`);
+  // ---------------------------------------------------------------------------
+  // simple arrows
+  // ---------------------------------------------------------------------------
+
+  // binding schema v2
+  // ---------------------------------------------------------------------------
+
+  if (binding.mode) {
+    // if latest binding schema, don't check if binding.elementId exists
+    // (it's done in a separate pass)
+    if (binding.elementId) {
+      return {
+        elementId: binding.elementId,
+        mode: binding.mode,
+        fixedPoint: normalizeFixedPoint(binding.fixedPoint || [0.5, 0.5]),
+      } as FixedPointBinding | null;
+    }
+    return null;
+  }
 
   return null;
 };
