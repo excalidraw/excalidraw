43b48b4ef0bcf06d8c10301f817a9a90c5e797b5
Mark Tolmacs <mark@lazycat.hu>
2026-02-21 17:53:49 +0100

    fix: Missing state definition
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- packages/excalidraw/appState.ts
+++ packages/excalidraw/appState.ts
@@ -71,6 +71,7 @@ export const getDefaultAppState = (): Omit<
     gridStep: DEFAULT_GRID_STEP,
     gridModeEnabled: false,
     isBindingEnabled: true,
+    startBoundElement: null,
     defaultSidebarDockedPreference: false,
     isLoading: false,
     isResizing: false,
@@ -193,6 +194,7 @@ const APP_STATE_STORAGE_CONF = (<
   gridModeEnabled: { browser: true, export: true, server: true },
   height: { browser: false, export: false, server: false },
   isBindingEnabled: { browser: false, export: false, server: false },
+  startBoundElement: { browser: false, export: false, server: false },
   defaultSidebarDockedPreference: {
     browser: true,
     export: false,
