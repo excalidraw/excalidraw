c6221c1e3928d12b45002eb3e121f2073101eb4b
Mark Tolmacs <mark@lazycat.hu>
2026-02-21 21:37:28 +0100

    fix: Remove the rest of startBoundElement
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -8882,7 +8882,6 @@ class App extends React.Component<AppProps, AppState> {
             ...prevState,
             bindMode: "orbit",
             newElement: element,
-            startBoundElement: boundElement,
             suggestedBinding:
               boundElement && isBindingElement(element)
                 ? {
