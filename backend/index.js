62bb1cb48d2dc46947e5b26d7b818a0553f1747a
tushar sariya <tushar.97@hotmail.com>
2026-02-23 13:50:11 -0330

    added new edges to nodes

--- backend/index.js
+++ backend/index.js
@@ -33,12 +33,87 @@ function get_adjacency_list_from_dot(input) {
       if (!(source in adjlist)) {
         adjlist[source] = []
       }
-      adjlist[source].push(target)
+      if (!adjlist[source].includes(target)) {
+        adjlist[source].push(target)
+      }
     }
   }
 
   collect_edges(input)
+
+  writeNodesDictToTemp(adjlist,'adjlist.json')
+
   return adjlist
+}
+
+function load_plan_and_nodes(plan) {
+  const resource_changes = plan.resource_changes || [];
+  const nodes = {};
+
+  for (const resource_change of resource_changes) {
+    const address = resource_change.address;
+    const res_path = address.replace(/\[\d+\]/g, '');
+    if (!(res_path in nodes)) {
+      nodes[res_path] = { resources: {} };
+    }
+    nodes[res_path].resources[address] = resource_change;
+  }
+
+  writeNodesDictToTemp(nodes,'nodes_dict.json')
+
+  return nodes;
+}
+
+function build_new_edges_nx(nodes,adjlist) {
+  for( const address in nodes) {
+    nodes[address]["edges_new"] = []
+    var visited = new Set();
+    visited.add(address)
+    var queue = [address]
+    while (queue.length != 0) {
+      current = queue.shift();
+      if (!(current in adjlist)) {
+        continue
+      }
+      for (const neibour of adjlist[current]) {
+        if (visited.has(neibour)) {
+          continue
+        }
+        visited.add(neibour)
+        if (neibour in nodes && !nodes[address]["edges_new"].includes(neibour)) {
+          nodes[address]["edges_new"].push(neibour)
+        } else if (neibour.startsWith("provider")) {
+          continue
+        } else {
+          queue.push(neibour)
+        }
+      }
+
+    }
+  }
+
+  //make edges bi directional
+  for ( const node in nodes) {
+    for (const target of nodes[node]["edges_new"]) {
+      if ( target in nodes) {
+        const target_edges = nodes[target]["edges_new"];
+        if (!target_edges.includes(node)) {
+          nodes[target]["edges_new"].push(node)
+        }
+      }
+    }
+  }
+  writeNodesDictToTemp(nodes,'nodes_new_edges.json')
+  return nodes
+}
+
+function writeNodesDictToTemp(nodes,filename) {
+  if (!fs.existsSync(TEMP_DIR)) {
+    fs.mkdirSync(TEMP_DIR, { recursive: true });
+  }
+  const filePath = path.join(TEMP_DIR, filename);
+  fs.writeFileSync(filePath, JSON.stringify(nodes, null, 2), 'utf-8');
+  return filePath;
 }
 
 app.post("/terraform/upload", upload.fields([
@@ -63,13 +138,15 @@ app.post("/terraform/upload", upload.fields([
   //console.log(graph);
   //console.log(graph.nodes());
   //console.log(graph.edges());
-  var adjlist = get_adjacency_list_from_dot(graph)
+  var adjlist = get_adjacency_list_from_dot(graph);
+  var nodesDict = load_plan_and_nodes(JSON.parse(planContent));
+  var nodesDict = build_new_edges_nx(nodesDict,adjlist);
 
   res.json({
     success: true,
     plan: { name: planFile.originalname, size: planContent.length },
     dot: { name: dotFile.originalname, size: dotContent.length },
-    temp: { adjlist, nodeCount: adjlist.length},
+    temp: { adjlist, nodeCount: Object.keys(adjlist).length },
   });
 })
 
