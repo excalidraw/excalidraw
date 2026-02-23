2bd8f1515c68652f143b0eedf56733f6afc937d3
tushar sariya <tushar.97@hotmail.com>
2026-02-23 11:08:54 -0330

    got dot file parsing working

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,43 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });  // saves files to uploads/ folder
+const upload = multer({ dest: "uploads/" });
+var fs = require('fs');
+var path = require('path');
+var dot = require('graphlib-dot');
+
+const TEMP_DIR = path.join(__dirname, 'temp');
 
 
 app.use(cors());
 app.use(express.json());
 
-// Mock endpoint
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, world!', success: true });
-});
+function get_adjacency_list_from_dot(input) {
+  const adjlist = {};
+
+  function extract_resource_name(node_id) {
+    const parts = node_id.trim().split(" ");
+    if (parts.length >= 2) {
+      return parts[1].replace('"','').replace('\\','')
+    }
+    return parts.replace('"','').replace('\\','')
+  }
+
+  function collect_edges(g) {
+    //yeah js for loops dont return the element, they return the index, funny stuff
+    for (const edge in g.edges()) {
+      source = extract_resource_name(g.edges()[edge].v)
+      target = extract_resource_name(g.edges()[edge].w)
+      if (!(source in adjlist)) {
+        adjlist[source] = []
+      }
+      adjlist[source].push(target)
+    }
+  }
+
+  collect_edges(input)
+  return adjlist
+}
 
 app.post("/terraform/upload", upload.fields([
   { name: "planFile", maxCount: 1 },
@@ -32,10 +59,17 @@ app.post("/terraform/upload", upload.fields([
   console.log("Plan file:", planFile.originalname, planContent.length, "chars");
   console.log("Dot file:", dotFile.originalname, dotContent.length, "chars");
 
+  var graph = dot.read(dotContent);
+  //console.log(graph);
+  //console.log(graph.nodes());
+  //console.log(graph.edges());
+  var adjlist = get_adjacency_list_from_dot(graph)
+
   res.json({
     success: true,
     plan: { name: planFile.originalname, size: planContent.length },
     dot: { name: dotFile.originalname, size: dotContent.length },
+    temp: { adjlist, nodeCount: adjlist.length},
   });
 })
 
