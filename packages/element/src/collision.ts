a66d8a3ee06a3f87de573a1c67c27e69abfdc21b
Noa Levi <275430404+lphuc2250gma@users.noreply.github.com>
2026-05-14 04:38:31 +0000

    fix: duplicated words in element/{groups,collision,duplicate} comments

--- packages/element/src/collision.ts
+++ packages/element/src/collision.ts
@@ -309,7 +309,7 @@ export const getAllHoveredElementAtPoint = (
   tolerance?: number,
 ): NonDeleted<ExcalidrawBindableElement>[] => {
   const candidateElements: NonDeleted<ExcalidrawBindableElement>[] = [];
-  // We need to to hit testing from front (end of the array) to back (beginning of the array)
+  // We need to hit testing from front (end of the array) to back (beginning of the array)
   // because array is ordered from lower z-index to highest and we want element z-index
   // with higher z-index
   for (let index = elements.length - 1; index >= 0; --index) {
@@ -375,7 +375,7 @@ export const getHoveredElementForFocusPoint = (
   tolerance?: number,
 ): ExcalidrawBindableElement | null => {
   const candidateElements: NonDeleted<ExcalidrawBindableElement>[] = [];
-  // We need to to hit testing from front (end of the array) to back (beginning of the array)
+  // We need to hit testing from front (end of the array) to back (beginning of the array)
   // because array is ordered from lower z-index to highest and we want element z-index
   // with higher z-index
   for (let index = elements.length - 1; index >= 0; --index) {
