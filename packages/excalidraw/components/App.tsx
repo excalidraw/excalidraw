acb48c3f454f050353c32819d7a5deded201e9db
David Luzar <5153846+dwelle@users.noreply.github.com>
2026-07-14 18:18:21 +0200

    fix(editor): make embeddable ignore higher-z-index non-framelike elements (#11662)

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -6617,7 +6617,7 @@ class App extends React.Component<AppProps, AppState> {
       includeLockedElements?: boolean;
     },
   ): NonDeleted<ExcalidrawElement>[] {
-    const iframeLikes: Ordered<NonDeleted<ExcalidrawIframeElement>>[] = [];
+    const iframeLikes: Ordered<NonDeleted<ExcalidrawIframeLikeElement>>[] = [];
 
     const elementsMap = this.scene.getNonDeletedElementsMap();
 
@@ -6642,7 +6642,10 @@ class App extends React.Component<AppProps, AppState> {
         }
         return containingFrame &&
           this.state.frameRendering.enabled &&
-          this.state.frameRendering.clip
+          this.state.frameRendering.clip &&
+          // iframe-like elements are rendered as DOM overlays and are not
+          // visually clipped by their containing frames
+          !isIframeLikeElement(element)
           ? isCursorInFrame(
               { x, y },
               containingFrame as NonDeleted<ExcalidrawFrameLikeElement>,
@@ -6655,7 +6658,7 @@ class App extends React.Component<AppProps, AppState> {
         // We want to preserve that order on the returned array.
         // Exception being embeddables which should be on top of everything else in
         // terms of hit testing.
-        if (isIframeElement(el)) {
+        if (isIframeLikeElement(el)) {
           iframeLikes.push(el);
           return false;
         }
