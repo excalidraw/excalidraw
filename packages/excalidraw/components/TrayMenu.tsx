183b53c8e615358b95e150b00bfd0a9e4c473a0b
zsviczian <viczian.zsolt@gmail.com>
2026-07-31 14:31:12 +0200

    TrayMenu moved to Toolbar.tsx

--- packages/excalidraw/components/TrayMenu.tsx
+++ packages/excalidraw/components/TrayMenu.tsx
@@ -1,17 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { showSelectedShapeActions } from "@excalidraw/element";
 
 import clsx from "clsx";
 import { getNonDeletedElements } from "@excalidraw/element";
 
-import { KEYS } from "@excalidraw/common";
-
 import type { NonDeletedExcalidrawElement } from "@excalidraw/element/types";
 
 import type { EditorInterface } from "@excalidraw/common";
 
-import { isHandToolActive } from "../appState";
 import { useTunnels } from "../context/tunnels";
 import { t } from "../i18n";
 import { getScrollToContentState, isSomeElementSelected } from "../scene";
@@ -20,43 +17,12 @@ import { SCROLLBAR_WIDTH, SCROLLBAR_MARGIN } from "../scene/scrollbars";
 import { actionToggleStats } from "../actions";
 
 import { SelectedShapeActions, ZoomActions } from "./Actions";
-import { useAppProps, useStylesPanelMode } from "./App";
 import { FixedSideContainer } from "./FixedSideContainer";
-import { HintViewer } from "./HintViewer";
-import { ImageMenu } from "./ImageMenu";
 import { Island } from "./Island";
-import { LockButton } from "./LockButton";
-import { PenModeButton } from "./PenModeButton";
 import { Section } from "./Section";
 import Stack from "./Stack";
-import DropdownMenu from "./dropdownMenu/DropdownMenu";
-import {
-  drawShapeToolIcon, //zsviczian autoshape in tray menu
-  EmbedIcon,
-  DotsIcon,
-  frameToolIcon,
-  LassoIcon,
-  laserPointerToolIcon,
-  MagicIcon,
-  mermaidLogoIcon,
-} from "./icons";
-import {
-  ArrowToolButton,
-  DiamondToolButton,
-  EllipseToolButton,
-  EraserToolButton,
-  FreedrawToolButton,
-  getToolShortcut, //zsviczian autoshape in tray menu
-  HandToolButton,
-  isToolButtonDisabled,
-  LineToolButton,
-  RectangleToolButton,
-  SelectionToolButton,
-  TextToolButton,
-  LassoToolButton,
-} from "./Tools";
-
 import { Stats } from "./Stats";
+import { Toolbar } from "./Toolbar";
 
 import type { ActionManager } from "../actions/manager";
 import type {
@@ -88,191 +54,6 @@ type TrayMenuProps = {
   renderWelcomeScreen: boolean;
   UIOptions: AppProps["UIOptions"];
   app: AppClassProperties;
-};
-
-const TrayToolbar = ({
-  app,
-  appState,
-  setAppState,
-  UIOptions,
-}: {
-  app: AppClassProperties;
-  appState: UIAppState;
-  setAppState: React.Component<any, AppState>["setState"];
-  UIOptions: AppProps["UIOptions"];
-}) => {
-  const { TTDDialogTriggerTunnel } = useTunnels();
-  const { renderMermaid } = useAppProps(); //zsviczian
-  const stylesPanelMode = useStylesPanelMode(); //zsviczian
-  const [isExtraToolsMenuOpen, setIsExtraToolsMenuOpen] = useState(false);
-  const [isImageMenuOpen, setIsImageMenuOpen] = useState(false); //zsviczian
-
-  const activeTool = appState.activeTool;
-  const toolProps = { app, activeTool };
-
-  const isFullStylesPanel = stylesPanelMode === "full";
-  const isTrayModePanel = stylesPanelMode === "tray"; //zsviczian
-
-  const frameToolSelected = activeTool.type === "frame";
-  const drawShapeToolSelected = activeTool.type === "autoshape"; //zsviczian autoshape in tray menu
-  const laserToolSelected = activeTool.type === "laser";
-  const embeddableToolSelected = activeTool.type === "embeddable";
-  const lassoToolSelected =
-    (isFullStylesPanel || isTrayModePanel) &&
-    activeTool.type === "lasso" &&
-    app.state.preferredSelectionTool.type !== "lasso";
-
-  const showLassoTool = isFullStylesPanel || isTrayModePanel; //zsviczian
-  const showImageTool = UIOptions.tools?.image !== false;
-
-  return (
-    <>
-      <HandToolButton {...toolProps} hideKeyBinding />
-      {appState.preferredSelectionTool.type === "lasso" ? (
-        <LassoToolButton {...toolProps} />
-      ) : (
-        <SelectionToolButton {...toolProps} />
-      )}
-      <RectangleToolButton {...toolProps} />
-      <DiamondToolButton {...toolProps} />
-      <EllipseToolButton {...toolProps} />
-      <ArrowToolButton {...toolProps} />
-      <LineToolButton {...toolProps} />
-      <FreedrawToolButton {...toolProps} />
-      <TextToolButton {...toolProps} />
-      {showImageTool && (
-        <ImageMenu
-          app={app}
-          open={isImageMenuOpen}
-          onToggle={() => {
-            if (!isImageMenuOpen) {
-              setIsExtraToolsMenuOpen(false);
-            }
-            setIsImageMenuOpen(!isImageMenuOpen);
-          }}
-          onClose={() => setIsImageMenuOpen(false)}
-        />
-      )}
-      <EraserToolButton {...toolProps} />
-
-      <div className="App-toolbar__divider" />
-
-      <DropdownMenu open={isExtraToolsMenuOpen}>
-        <DropdownMenu.Trigger
-          className={clsx("App-toolbar__extra-tools-trigger", {
-            "App-toolbar__extra-tools-trigger--selected":
-              frameToolSelected ||
-              embeddableToolSelected ||
-              drawShapeToolSelected || //zsviczian autoshape in tray menu
-              lassoToolSelected ||
-              (laserToolSelected && !app.props.isCollaborating),
-          })}
-          onToggle={() => {
-            if (!isExtraToolsMenuOpen) {
-              setIsImageMenuOpen(false); //zsviczian
-            }
-            setIsExtraToolsMenuOpen(!isExtraToolsMenuOpen);
-            setAppState({ openMenu: null, openPopup: null });
-          }}
-          title={t("toolBar.extraTools")}
-        >
-          {frameToolSelected
-            ? frameToolIcon
-            : embeddableToolSelected
-            ? EmbedIcon
-            : drawShapeToolSelected //zsviczian autoshape in tray menu
-            ? drawShapeToolIcon //zsviczian autoshape in tray menu
-            : laserToolSelected && !app.props.isCollaborating
-            ? laserPointerToolIcon
-            : lassoToolSelected
-            ? LassoIcon
-            : DotsIcon}
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content
-          onClickOutside={() => setIsExtraToolsMenuOpen(false)}
-          onSelect={() => setIsExtraToolsMenuOpen(false)}
-          className="App-toolbar__extra-tools-dropdown"
-        >
-          <DropdownMenu.Item
-            onSelect={() => app.setActiveTool({ type: "frame" })}
-            icon={frameToolIcon}
-            shortcut={KEYS.F.toLocaleUpperCase()}
-            data-testid="toolbar-frame"
-            selected={frameToolSelected}
-            disabled={isToolButtonDisabled(app, "frame")}
-          >
-            {t("toolBar.frame")}
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            onSelect={() => app.setActiveTool({ type: "embeddable" })}
-            icon={EmbedIcon}
-            data-testid="toolbar-embeddable"
-            selected={embeddableToolSelected}
-            disabled={isToolButtonDisabled(app, "embeddable")}
-          >
-            {t("toolBar.embeddable")}
-          </DropdownMenu.Item>
-          {/*zsviczian START autoshape in tray menu*/}
-          <DropdownMenu.Item
-            onSelect={() => app.setActiveTool({ type: "autoshape" })}
-            icon={drawShapeToolIcon}
-            shortcut={getToolShortcut("autoshape")}
-            data-testid="toolbar-autoshape"
-            selected={drawShapeToolSelected}
-            disabled={isToolButtonDisabled(app, "autoshape")}
-          >
-            {t("toolBar.autoshape")}
-          </DropdownMenu.Item>
-          {/*zsviczian END*/}
-          <DropdownMenu.Item
-            onSelect={() => app.setActiveTool({ type: "laser" })}
-            icon={laserPointerToolIcon}
-            data-testid="toolbar-laser"
-            selected={laserToolSelected}
-            shortcut={KEYS.K.toLocaleUpperCase()}
-            disabled={isToolButtonDisabled(app, "laser")}
-          >
-            {t("toolBar.laser")}
-          </DropdownMenu.Item>
-          {showLassoTool && (
-            <DropdownMenu.Item
-              onSelect={() => app.setActiveTool({ type: "lasso" })}
-              icon={LassoIcon}
-              data-testid="toolbar-lasso"
-              selected={lassoToolSelected}
-              disabled={isToolButtonDisabled(app, "lasso")}
-            >
-              {t("toolBar.lasso")}
-            </DropdownMenu.Item>
-          )}
-          <div style={{ margin: "6px 0", fontSize: 14, fontWeight: 600 }}>
-            Generate
-          </div>
-          {app.props.aiEnabled !== false && <TTDDialogTriggerTunnel.Out />}
-          {renderMermaid && ( //zsviczian
-            <DropdownMenu.Item
-              onSelect={() => app.setOpenDialog({ name: "ttd", tab: "mermaid" })}
-              icon={mermaidLogoIcon}
-              data-testid="toolbar-embeddable"
-            >
-              {t("toolBar.mermaidToExcalidraw")}
-            </DropdownMenu.Item>
-          )}
-          {app.props.aiEnabled !== false && app.plugins.diagramToCode && (
-            <DropdownMenu.Item
-              onSelect={() => app.onMagicframeToolSelect()}
-              icon={MagicIcon}
-              data-testid="toolbar-magicframe"
-              badge={<DropdownMenu.Item.Badge>AI</DropdownMenu.Item.Badge>}
-              disabled={isToolButtonDisabled(app, "magicframe")}
-            >
-              {t("toolBar.magicframe")}
-            </DropdownMenu.Item>
-          )}
-        </DropdownMenu.Content>
-      </DropdownMenu>
-    </>
-  );
 };
 
 export const TrayMenu = ({
@@ -312,17 +93,15 @@ export const TrayMenu = ({
           {(heading: React.ReactNode) => (
             <Stack.Col gap={4} align="center">
               <Stack.Row gap={1} className="App-toolbar-container">
-                <Island padding={1} className="App-toolbar">
-                  {heading}
-                  <Stack.Row gap={1}>
-                    <TrayToolbar
-                      appState={appState}
-                      setAppState={setAppState}
-                      UIOptions={UIOptions}
-                      app={app}
-                    />
-                  </Stack.Row>
-                </Island>
+                <Toolbar
+                  app={app}
+                  appState={appState}
+                  setAppState={setAppState}
+                  UIOptions={UIOptions}
+                  onPenModeToggle={onPenModeToggle}
+                  onLockToggle={onLockToggle}
+                  heading={heading}
+                />
                 <div
                   className="tray-misc-tools-container"
                   style={
@@ -336,35 +115,19 @@ export const TrayMenu = ({
                   }
                 >
                   {!appState.viewModeEnabled && //zsviczian
-                    renderTopRightUI?.(editorInterface.formFactor === "phone", appState)}
+                    renderTopRightUI?.(
+                      editorInterface.formFactor === "phone",
+                      appState,
+                    )}
                   {!appState.viewModeEnabled &&
                     appState.openDialog?.name !== "elementLinkSelector" && (
                       <DefaultSidebarTriggerTunnel.Out />
                     )}
-                  <PenModeButton
-                    checked={appState.penMode}
-                    onChange={() => onPenModeToggle(null)}
-                    title={t("toolBar.penMode")}
-                    isMobile
-                    penDetected={appState.penDetected}
-                  />
-                  <LockButton
-                    checked={appState.activeTool.locked}
-                    onChange={onLockToggle}
-                    title={t("toolBar.lock")}
-                    isMobile
-                  />
                 </div>
               </Stack.Row>
             </Stack.Col>
           )}
         </Section>
-        <HintViewer
-          appState={appState}
-          isMobile={true}
-          editorInterface={editorInterface}
-          app={app}
-        />
         <div //zsviczian
           className={clsx("layer-ui__wrapper__top-right zen-mode-transition", {
             "transition-right": appState.zenModeEnabled,
