75f33f5766e8119ec199a6bab2b70d5a5dbd190f
alexsaf1987-ui <alexsaf1987-ui@users.noreply.github.com>
2026-08-27 12:42:43 +0000

    fix: scope RTL to Excalidraw container instead of document
    When using @excalidraw/excalidraw as a package, setLanguage() was
    mutating document.documentElement, which broke the host page layout.
    
    Added applyLanguageToDocument prop (default: true) to control this
    behavior. When false, RTL/LTR is scoped to the Excalidraw container
    and portal containers instead of the global document.
    
    Fixes #11963

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -2345,6 +2345,16 @@ class App extends React.Component<AppProps, AppState> {
     return (
       <div
         translate="no"
+        dir={
+          this.props.applyLanguageToDocument
+            ? undefined
+            : getLanguage().rtl
+            ? "rtl"
+            : "ltr"
+        }
+        lang={
+          this.props.applyLanguageToDocument ? undefined : getLanguage().code
+        }
         className={clsx(
           "excalidraw excalidraw-container notranslate",
           this.props.className,
@@ -13975,7 +13985,7 @@ class App extends React.Component<AppProps, AppState> {
     const currentLang =
       languages.find((lang) => lang.code === this.props.langCode) ||
       defaultLang;
-    await setLanguage(currentLang);
+    await setLanguage(currentLang, this.props.applyLanguageToDocument);
     this.setAppState({});
   }
 }
